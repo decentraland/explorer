d788d5e2b9209ff5f42f7c1ccbdcd5a6db7cbdc4
Brian Amadori <brian@decentraland.org>
2019-10-27 18:40:27 -0300

    add AssetPromiseKeeper_AssetBundle and related placeholder classes

---
+++ Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/AssetBundle/AssetPromiseKeeper_AssetBundle.cs
@@ -0,0 +1,11 @@
+﻿namespace DCL
+{
+    public class AssetPromiseKeeper_AssetBundle : AssetPromiseKeeper<Asset_AssetBundle, AssetLibrary_AssetBundle, AssetPromise_AssetBundle>
+    {
+        public AssetPromiseKeeper_AssetBundle(AssetLibrary_AssetBundle library) : base(library)
+        {
+            library = new AssetLibrary_AssetBundle();
+        }
+    }
+
+}
---
+++ Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/AssetBundle/Asset_AssetBundle.cs
@@ -0,0 +1,6 @@
+﻿namespace DCL
+{
+    public class Asset_AssetBundle : Asset
+    {
+    }
+}
