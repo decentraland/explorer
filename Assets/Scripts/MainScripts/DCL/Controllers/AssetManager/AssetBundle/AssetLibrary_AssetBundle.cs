5015dcb98258f5b370bde9d7b6e6a4e4d950facb
Brian Amadori <brian@decentraland.org>
2019-10-27 18:40:27 -0300

    add AssetPromiseKeeper_AssetBundle and related placeholder classes

---
+++ Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/AssetBundle/AssetLibrary_AssetBundle.cs
@@ -0,0 +1,35 @@
+﻿namespace DCL
+{
+    public class AssetLibrary_AssetBundle : AssetLibrary<Asset_AssetBundle>
+    {
+        public override void Add(Asset_AssetBundle asset)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Cleanup()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool Contains(object id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool Contains(Asset_AssetBundle asset)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override Asset_AssetBundle Get(object id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Release(Asset_AssetBundle asset)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/AssetBundle/AssetPromise_AssetBundle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DCL
+{
+    public class AssetPromise_AssetBundle : AssetPromise<Asset_AssetBundle>
+    {
+        protected override void ApplySettings_LoadFinished()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void ApplySettings_LoadStart()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnCancelLoading()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnLoad(Action OnSuccess, Action OnFail)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override object GetId()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
