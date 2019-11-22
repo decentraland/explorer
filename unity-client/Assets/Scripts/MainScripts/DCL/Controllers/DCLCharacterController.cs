0a23a7846731969f90369804556b8766a92b3c4f
Alejandro Jimenez <ajimenez@decentraland.org>
2019-11-22 17:42:08 +0100

    Free Mode WIP

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/DCLCharacterController.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/DCLCharacterController.cs
@@ -178,14 +178,26 @@ public class DCLCharacterController : MonoBehaviour
 
     public void SetEulerRotation(Vector3 eulerRotation)
     {
-        cameraTransform.localRotation = Quaternion.Euler(eulerRotation.x, 0f, 0f);
         if (!Input.GetMouseButton(1))
         {
+            cameraTransform.localRotation = Quaternion.Euler(eulerRotation.x, 0f, 0f);
             transform.rotation = Quaternion.Euler(0f, eulerRotation.y, 0f);
         }
         else
         {
-            transform.Find("CameraPivot").transform.localEulerAngles += Vector3.up * -aimingHorizontalDeltaAngle ;
+            var current = transform.Find("CameraPivot").transform.localEulerAngles;
+            var xValue = current.x - aimingVerticalDeltaAngle;
+            while (xValue < 0) xValue += 360;
+            var lowerClamp = 45;
+            var higherClamp = 360 - lowerClamp;
+            if (xValue > lowerClamp)
+            {
+                if (xValue < higherClamp)
+                {
+                    xValue = (xValue - lowerClamp) < (higherClamp - xValue) ? lowerClamp : higherClamp;
+                }
+            }
+            transform.Find("CameraPivot").transform.localRotation = Quaternion.Euler( xValue, current.y - aimingHorizontalDeltaAngle, 0f);
         }
     }
 
