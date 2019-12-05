403352c662f9766193ceca1e660ba19dd555d925
Esteban Ordano <esteban@decentraland.org>
2019-12-05 14:22:37 -0300

    Update unity-client/Assets/Scripts/MainScripts/DCL/Controllers/Scene/SceneBoundariesController/Tests/SceneBoundariesCheckerTests.cs
    Co-Authored-By: Brian Amadori <brian@decentraland.org>

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/Scene/SceneBoundariesController/Tests/SceneBoundariesCheckerTests.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/Scene/SceneBoundariesController/Tests/SceneBoundariesCheckerTests.cs
@@ -8,7 +8,7 @@ using UnityEngine.TestTools;
 using NUnit.Framework;
 using System.Collections;
 
-namespace SceneBoundariesChecker
+namespace SceneBoundariesCheckerTests
 {
     public class SceneBoundariesCheckerTests : TestsBase
     {
