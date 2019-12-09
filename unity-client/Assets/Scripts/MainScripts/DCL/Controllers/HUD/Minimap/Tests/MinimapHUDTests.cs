18dae2b1ca8fe033aac3026afdabf2715aaec2a7
Pravus <pravus@decentraland.org>
2019-12-09 14:06:11 -0300

    applied Pr feedback

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Minimap/Tests/MinimapHUDTests.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Minimap/Tests/MinimapHUDTests.cs
@@ -33,7 +33,7 @@ namespace Tests
             var controller = new MinimapHUDController();
 
             var view = GetViewFromController(controller);
-            Assert.IsTrue(Reflection_GetField<TextMeshProUGUI>(view, "sceneNameText").text == "Unnamed");
+            Assert.AreEqual("Unnamed", Reflection_GetField<TextMeshProUGUI>(view, "sceneNameText").text);
         }
 
         [UnityTest]
