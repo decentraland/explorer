ce4b15f70498564587ef90cb01f2b61420e99c73
Brian Amadori <black.pentagram@gmail.com>
2019-12-20 20:54:08 -0300

    fixes

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/AssetBundles/MaterialCachingHelper.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/AssetBundles/MaterialCachingHelper.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityGLTF.Cache;
@@ -6,10 +7,12 @@ namespace DCL
 {
     public static class MaterialCachingHelper
     {
-        public static void UseCachedMaterials(GameObject obj)
+        public static float timeBudgetMax = 0.003f;
+        public static float timeBudget = 0;
+        public static IEnumerator UseCachedMaterials(GameObject obj)
         {
             if (obj == null)
-                return;
+                yield break;
 
             var matList = new List<Material>(1);
 
@@ -19,20 +22,32 @@ namespace DCL
 
                 foreach (var mat in rend.sharedMaterials)
                 {
+                    float elapsedTime = Time.realtimeSinceStartup;
+
                     string crc = mat.ComputeCRC() + mat.name;
 
                     RefCountedMaterialData refCountedMat;
 
                     if (!PersistentAssetCache.MaterialCacheByCRC.ContainsKey(crc))
                     {
-                        mat.enableInstancing = true;
+                        //mat.enableInstancing = true;
                         PersistentAssetCache.MaterialCacheByCRC.Add(crc, new RefCountedMaterialData(crc, mat));
                     }
 
                     refCountedMat = PersistentAssetCache.MaterialCacheByCRC[crc];
                     refCountedMat.IncreaseRefCount();
 
+
                     matList.Add(refCountedMat.material);
+
+                    elapsedTime = Time.realtimeSinceStartup - elapsedTime;
+                    timeBudget -= elapsedTime;
+
+                    if (timeBudget < 0)
+                    {
+                        yield return null;
+                        timeBudget += timeBudgetMax;
+                    }
                 }
 
                 rend.sharedMaterials = matList.ToArray();
