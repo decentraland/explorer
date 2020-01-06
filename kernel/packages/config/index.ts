083fa99089b3d6a81b83b243fba2c8933cf4ca70
Pablo de Haro <pablitar@gmail.com>
2020-01-06 15:41:39 -0300

    updated url

--- kernel/packages/config/index.ts
+++ kernel/packages/config/index.ts
@@ -223,7 +223,7 @@ export function getServerConfigurations() {
     contentAsBundle: `https://content-as-bundle.decentraland.zone`,
     worldInstanceUrl: `wss://world-comm.decentraland.${TLDDefault}/connect`,
     comms: {
-      lighthouseUrl: 'https://katalyst-comms.decentraland.zone:443'
+      lighthouseUrl: 'http://katalyst-comms.decentraland.zone:9000'
     },
     profile: `https://profile.decentraland.${TLDDefault}/api/v1`,
     wearablesApi: `https://wearable-api.decentraland.org/v2`,
