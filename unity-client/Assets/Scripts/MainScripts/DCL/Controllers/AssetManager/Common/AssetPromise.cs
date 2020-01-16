cd722a3b5da76672e10923cc419971840017e23f
Brian Amadori <brian@decentraland.org>
2020-01-16 18:42:39 -0300

    tentative fix

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/Common/AssetPromise.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/Common/AssetPromise.cs
@@ -158,7 +158,6 @@ namespace DCL
             if (state == AssetPromiseState.LOADING)
             {
                 OnCancelLoading();
-                OnLoadFailure();
             }
             else if (state == AssetPromiseState.FINISHED)
             {
