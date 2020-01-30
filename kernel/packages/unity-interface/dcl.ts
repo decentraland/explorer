532e55b2e20e39d441c78094b1e13c37c8af38ec
Esteban Ordano <esteban@decentraland.org>
2020-01-29 23:39:43 -0300

    revertme: silent scene event not found error

--- kernel/packages/unity-interface/dcl.ts
+++ kernel/packages/unity-interface/dcl.ts
@@ -116,7 +116,7 @@ const browserInterface = {
       const parcelScene = scene.parcelScene as UnityParcelScene
       parcelScene.emit(data.eventType as IEventNames, data.payload)
     } else {
-      defaultLogger.error(`SceneEvent: Scene ${data.sceneId} not found`, data)
+      // defaultLogger.error(`SceneEvent: Scene ${data.sceneId} not found`, data)
     }
   },
 
