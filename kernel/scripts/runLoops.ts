b89bd16f82e25dc94ff07ba58eba82af2ebccdde
Pablitar <pablitar@gmail.com>
2020-01-30 14:15:42 -0300

    Bot position parameter

--- kernel/scripts/runLoops.ts
+++ kernel/scripts/runLoops.ts
@@ -1,5 +1,7 @@
 const puppeteer = require('puppeteer')
 
+const position =  process.env.BOT_POSITION ? process.env.BOT_POSITION : "20,20"
+
 async function main() {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
@@ -10,11 +12,10 @@ async function main() {
     }
   })
 
-  await page.goto('http://localhost:8080/?position=-5%2C-5&ws=ws%3A%2F%2Flocalhost%3A5001%2Floop&COMMS=v2-server', {
+  await page.goto(`http://localhost:8080/?position=${position}&ws=ws%3A%2F%2Flocalhost%3A5001%2Floop`, {
     waitUntil: 'networkidle2'
   })
 
-  let i = 0
   while (true) {
     // await page.screenshot({ path: `example-${i++}.png` })
 
