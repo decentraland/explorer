47d8ef5381104e961cd7b294813a75a15c60bd41
Pato <pato@decentraland.org>
2020-01-30 16:29:55 -0300

    fix tutorial content generator script

--- kernel/scripts/generateTutorialSceneContents.sh
+++ kernel/scripts/generateTutorialSceneContents.sh
@@ -1,7 +1,7 @@
 content="["
 for dir in `find ../static/loader/tutorial-scene -type f`; do
     if [[ $dir != *".DS_Store"*  && $dir != *"generate_contents"* && $dir != *"tutorialSceneContents.json"* ]]; then
-        path=${dir:2}
+        path=${dir:32}
         content+="{\"file\": \"$path\", \"hash\": \"$path\"},"
     fi
 done
