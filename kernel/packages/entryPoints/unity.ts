a6aa0319ab6a6c5e3aa4227eefa316d74d59b3af
Brian Amadori <brian@decentraland.org>
2020-02-06 16:46:44 -0300

    remove HUD record

--- kernel/packages/entryPoints/unity.ts
+++ kernel/packages/entryPoints/unity.ts
@@ -4,11 +4,10 @@ import { worldToGrid } from '../atomicHelpers/parcelScenePositions'
 import defaultLogger from '../shared/logger'
 import { signalRendererInitialized } from '../shared/renderer/actions'
 import { lastPlayerPosition, teleportObservable } from '../shared/world/positionThings'
-import { HUD, startUnityParcelLoading } from '../unity-interface/dcl'
+import { startUnityParcelLoading, unityInterface } from '../unity-interface/dcl'
 import { initializeUnity } from '../unity-interface/initializer'
 import { experienceStarted } from '../shared/loading/types'
 import { OPEN_AVATAR_EDITOR } from '../config/index'
-import { WelcomeHUDControllerModel } from 'shared/types'
 
 const container = document.getElementById('gameContainer')
 
@@ -18,13 +17,28 @@ if (!container) throw new Error('cannot find element #gameContainer')
 
 initializeUnity(container)
   .then(async _ => {
-    HUD.Minimap.configure({ active: true, visible: true })
-    HUD.Avatar.configure({ active: true, visible: true })
-    HUD.Notification.configure({ active: true, visible: true })
-    HUD.AvatarEditor.configure({ active: true, visible: OPEN_AVATAR_EDITOR })
-    HUD.Settings.configure({ active: true, visible: false })
-    HUD.Expressions.configure({ active: true, visible: true })
-    HUD.PlayerInfoCard.configure({ active: true, visible: true })
+    const i = unityInterface
+    
+    i.ConfigureMinimapHUD({ active: true, visible: true })
+    i.ConfigureAvatarHUD({ active: true, visible: true })
+    i.ConfigureNotificationHUD({ active: true, visible: true })
+    i.ConfigureAvatarEditorHUD({ active: true, visible: OPEN_AVATAR_EDITOR })
+    i.ConfigureSettingsHUD({ active: true, visible: false })
+    i.ConfigureExpressionsHUD({ active: true, visible: true })
+    i.ConfigurePlayerInfoCardHUD({ active: true, visible: true })
+
+    i.ConfigureWelcomeHUD({
+      active: true, 
+      visible: true, 
+      title: "Title test",
+      timeTarget: 1932,
+      timeText: "Time test",
+      showTime: true,
+      bodyText: "Body test",
+      buttonText: "Button test",
+      buttonAction: "asdsad",
+      showButton: true,
+    })
 
     global['globalStore'].dispatch(signalRendererInitialized())
     await startUnityParcelLoading()
@@ -33,19 +47,6 @@ initializeUnity(container)
       .then($ => {
         teleportObservable.notifyObservers(worldToGrid(lastPlayerPosition))
         global['globalStore'].dispatch(experienceStarted())
-
-        HUD.Welcome.configure<WelcomeHUDControllerModel>({ 
-          active: true, 
-          visible: true, 
-          title: "Title test",
-          timeTarget: 1932,
-          timeText: "Time test",
-          showTime: true,
-          bodyText: "Body test",
-          buttonText: "Button test",
-          buttonAction: "asdsad",
-          showButton: true,
-        })
       })
       .catch(defaultLogger.error)
 
