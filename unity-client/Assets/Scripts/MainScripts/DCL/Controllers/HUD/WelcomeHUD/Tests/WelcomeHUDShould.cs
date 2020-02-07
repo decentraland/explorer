efa5b17547deb559f23a40c23391ca8561841215
Brian Amadori <brian@decentraland.org>
2020-02-07 19:35:54 -0300

    re-enabled tests

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/WelcomeHUD/Tests/WelcomeHUDShould.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/WelcomeHUD/Tests/WelcomeHUDShould.cs
@@ -1,49 +1,53 @@
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine;
+
 namespace Tests
 {
     public class WelcomeHUDShould : TestsBase
     {
         WelcomeHUDController controller;
-        //protected override IEnumerator SetUp()
-        //{
-        //    yield return base.SetUp();
-        //    controller = new WelcomeHUDController();
-        //    var model = new WelcomeHUDController.Model()
-        //    {
-        //        hasWallet = true
-        //    };
+        protected override IEnumerator SetUp()
+        {
+            yield return base.SetUp();
+            controller = new WelcomeHUDController();
+            var model = new WelcomeHUDController.Model()
+            {
+                hasWallet = true
+            };
 
-        //    controller.Initialize(model);
+            controller.Initialize(model);
 
-        //}
+        }
 
-        //[Test]
-        //public void BeCreatedProperly()
-        //{
-        //    Assert.IsTrue(controller.view != null);
-        //}
+        [Test]
+        public void BeCreatedProperly()
+        {
+            Assert.IsTrue(controller.view != null);
+        }
 
 
-        //[Test]
-        //public void BehaveCorrectlyAfterCloseButtonIsPressed()
-        //{
-        //    Assert.IsTrue(Cursor.lockState == CursorLockMode.None);
+        [Test]
+        public void BehaveCorrectlyAfterCloseButtonIsPressed()
+        {
+            Assert.IsTrue(Cursor.lockState == CursorLockMode.None);
 
-        //    controller.view.closeButton.onClick.Invoke();
+            controller.view.closeButton.onClick.Invoke();
 
-        //    Assert.IsTrue(Cursor.lockState == CursorLockMode.Locked);
-        //    Assert.IsFalse(controller.view.gameObject.activeSelf);
-        //}
+            Assert.IsTrue(Cursor.lockState == CursorLockMode.Locked);
+            Assert.IsFalse(controller.view.gameObject.activeSelf);
+        }
 
-        //[Test]
-        //public void BehaveCorrectlyAfterConfirmButtonIsPressed()
-        //{
-        //    Assert.IsTrue(Cursor.lockState == CursorLockMode.None);
+        [Test]
+        public void BehaveCorrectlyAfterConfirmButtonIsPressed()
+        {
+            Assert.IsTrue(Cursor.lockState == CursorLockMode.None);
 
-        //    controller.view.confirmButton.onClick.Invoke();
+            controller.view.confirmButton.onClick.Invoke();
 
-        //    Assert.IsTrue(Cursor.lockState == CursorLockMode.Locked);
-        //    Assert.IsFalse(controller.view.gameObject.activeSelf);
-        //}
+            Assert.IsTrue(Cursor.lockState == CursorLockMode.Locked);
+            Assert.IsFalse(controller.view.gameObject.activeSelf);
+        }
 
     }
 }
