494f1c7992f6b74fe3a47647189c69d12356bb5f
Pablo de Haro <pablitar@gmail.com>
2020-02-20 11:53:53 -0300

    Deactivating peer logs using a little monkey patch

--- kernel/packages/shared/comms/v2/LighthouseWorldInstanceConnection.ts
+++ kernel/packages/shared/comms/v2/LighthouseWorldInstanceConnection.ts
@@ -180,13 +180,13 @@ export class LighthouseWorldInstanceConnection implements WorldInstanceConnectio
   private initializePeer() {
     this.statusHandler({ status: 'connecting', connectedPeers: this.connectedPeersCount() })
     this.peer = this.createPeer()
-    this.peer.logLevel = 'WARN'
+    // @ts-ignore
+    this.peer.log = () => {}
     global.__DEBUG_PEER = this.peer
     return this.peer
   }
 
   private connectedPeersCount(): number {
-    // @ts-ignore
     return this.peer ? this.peer.connectedCount() : 0
   }
 
