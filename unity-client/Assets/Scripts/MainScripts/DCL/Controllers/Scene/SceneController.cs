f267c2416351652d515253a1ee7993ee1a3e1a3d
Brian Amadori <brian@decentraland.org>
2020-02-20 17:58:21 -0300

    apply PR feedback

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/Scene/SceneController.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/Scene/SceneController.cs
@@ -1,4 +1,4 @@
-﻿using DCL.Controllers;
+using DCL.Controllers;
 using DCL.Helpers;
 using DCL.Interface;
 using DCL.Models;
@@ -435,8 +435,6 @@ namespace DCL
 
             if (MessagingControllersManager.i.ContainsController(sceneKey))
                 MessagingControllersManager.i.RemoveController(sceneKey);
-
-            Debug.Log("Unload scene..");
         }
 
         public void UnloadParcelSceneExecute(string sceneKey)
