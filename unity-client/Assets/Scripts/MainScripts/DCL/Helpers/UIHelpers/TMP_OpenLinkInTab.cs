8bae61003a41270b1f430c3dfe919b236e562c94
Pato <pato@decentraland.org>
2020-02-21 15:00:34 -0300

    chore: add link to blogspots

---
+++ unity-client/Assets/Scripts/MainScripts/DCL/Helpers/UIHelpers/TMP_OpenLinkInTab.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+using DCL.Interface;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class TMP_OpenLinkInTab : MonoBehaviour, IPointerClickHandler
+{
+    TextMeshProUGUI textMeshPro;
+
+    void Awake()
+    {
+        textMeshPro = GetComponent<TextMeshProUGUI>();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        int linkId = TMP_TextUtilities.FindIntersectingLink(textMeshPro, Input.mousePosition, null);
+        if (linkId != -1)
+        {
+            TMP_LinkInfo linkInfo = textMeshPro.textInfo.linkInfo[linkId];
+            WebInterface.OpenURL(linkInfo.GetLinkID());
+        }
+    }
+}
