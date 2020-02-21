e6e8af19aeee70a5c8e218d8510db66eae934e58
Pravus <pravus@decentraland.org>
2020-02-21 14:01:00 -0300

    removed comment

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/Scene/ParcelScene.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/Scene/ParcelScene.cs
@@ -367,7 +367,6 @@ namespace DCL.Controllers
             SceneController.i.OnMessageDecodeEnds?.Invoke("RemoveEntity");
             if (entities.ContainsKey(tmpRemoveEntityMessage.id))
             {
-                Debug.Log("removing entity " + tmpRemoveEntityMessage.id);
                 DecentralandEntity entity = entities[tmpRemoveEntityMessage.id];
 
                 if (!entity.markedForCleanup)
