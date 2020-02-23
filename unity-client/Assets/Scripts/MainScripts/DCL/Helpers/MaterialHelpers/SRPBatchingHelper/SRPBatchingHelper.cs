c38cff5ceb746b2134643d96fae2356934ac0e9d
Brian Amadori <brian@decentraland.org>
2020-02-23 20:29:16 -0300

    fixes

--- unity-client/Assets/Scripts/MainScripts/DCL/Helpers/MaterialHelpers/SRPBatchingHelper/SRPBatchingHelper.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Helpers/MaterialHelpers/SRPBatchingHelper/SRPBatchingHelper.cs
@@ -1,13 +1,11 @@
-using System.Text;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace DCL.Helpers
 {
 
     public static class SRPBatchingHelper
     {
-        static StringBuilder tmpStrBuilder = new StringBuilder(500);
-        public static void OptimizeMaterial(Renderer renderer, Material material)
+        public static void OptimizeMaterial(Renderer renderer, Material material, int crc)
         {
             //NOTE(Brian): Just enable these keywords so the SRP batcher batches more stuff.
             material.EnableKeyword("_EMISSION");
@@ -23,26 +21,6 @@ namespace DCL.Helpers
             else
                 baseQueue = (int)UnityEngine.Rendering.RenderQueue.Geometry;
 
-            UnityEngine.Rendering.CullMode cullMode = (UnityEngine.Rendering.CullMode)material.GetFloat("_Cull");
-            int _ZWrite = (int)material.GetFloat("_ZWrite");
-            int _SrcBlend = (int)material.GetFloat("_SrcBlend");
-            int _DstBlend = (int)material.GetFloat("_DstBlend");
-
-            var kw = material.shaderKeywords;
-
-            tmpStrBuilder.Clear();
-
-            for (int i = 0; i < kw.Length; i++)
-            {
-                tmpStrBuilder.Append(kw[i]);
-            }
-
-            tmpStrBuilder.Append(cullMode.ToString());
-            tmpStrBuilder.Append(_ZWrite.ToString());
-            tmpStrBuilder.Append(_SrcBlend.ToString());
-            tmpStrBuilder.Append(_DstBlend.ToString());
-
-            int crc = Shader.PropertyToID(tmpStrBuilder.ToString());
             int crcBucket = Mathf.Abs(crc) % 500;
 
             //NOTE(Brian): This is to move the rendering of animated stuff on top of the queue, so the SRP batcher
