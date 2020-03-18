06e89f6be122b7ea4eb1a05a487f9ccea9eadb9f
Pravus <pravus@decentraland.org>
2020-03-17 21:03:20 -0300

    Added icons panel and current scene panel in navmap

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/NavMap/NavmapView.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/NavMap/NavmapView.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using DCL.Helpers;
+using TMPro;
 
 public class NavmapView : MonoBehaviour
 {
     [SerializeField] InputAction_Trigger toggleNavMapAction;
     [SerializeField] Button closeButton;
     [SerializeField] GameObject scrollRectGameObject;
+    [SerializeField] TextMeshProUGUI currentSceneNameText;
+    [SerializeField] TextMeshProUGUI currentSceneCoordsText;
     InputAction_Trigger.Triggered toggleNavMapDelegate;
 
     void Awake()
