868fb1f4170fd5f5c2537c72f12e9a50dcb54f69
Brian Amadori <brian@decentraland.org>
2020-03-20 13:27:16 -0300

    remove unused code

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/MapRenderer/MapRenderer.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/MapRenderer/MapRenderer.cs
@@ -85,12 +85,6 @@ namespace DCL
             var gridPosition = this.playerGridPosition;
             playerPositionIcon.transform.localPosition = MapUtils.GetTileToLocalPosition(gridPosition.x, gridPosition.y);
             playerPositionIcon.transform.rotation = playerAngle;
-
-            //TODO(Brian): Update POI icons, avatar icons, etc.
-            foreach (var sceneInfo in scenesOfInterest)
-            {
-
-            }
         }
 
         public Vector3 GetViewportCenter()
