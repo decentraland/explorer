79f0fb552382185b40f995e585feed602724e15a
moliva <oliva.miguelh@gmail.com>
2020-03-25 13:25:06 -0300

    refactor: minor

--- kernel/test/unit/jsonFetch.test.tsx
+++ kernel/test/unit/jsonFetch.test.tsx
@@ -3,16 +3,30 @@ import sinon from 'sinon'
 import { jsonFetch } from 'atomicHelpers/jsonFetch'
 
 describe('jsonFetch', function() {
+  let originalFetch: any
+  let fake: any
+
+  beforeEach(function() {
+    originalFetch = globalThis.fetch
+    fake = sinon.stub()
+    globalThis.fetch = fake
+  })
+
+  afterEach(function() {
+    fake = undefined
+    globalThis.fetch = originalFetch
+  })
+
   it('does not cache a result if it fails', async () => {
-    const fake = sinon.stub()
     fake.onCall(0).returns(Promise.resolve({ ok: false }))
     fake.returns(Promise.resolve({ ok: true, json: () => ({ stub: true }) }))
-    globalThis.fetch = window.fetch = fake
 
     try {
       await jsonFetch('fakeurl')
       expect.fail('first jsonFetch should reject')
-    } catch (e) {}
+    } catch (e) {
+      // nothing to do here
+    }
 
     await jsonFetch('fakeurl')
 
@@ -20,9 +34,7 @@ describe('jsonFetch', function() {
   })
 
   it('caches a result if it succeeds', async () => {
-    const fake = sinon.stub()
     fake.returns(Promise.resolve({ ok: true, json: () => ({ stub: true }) }))
-    globalThis.fetch = window.fetch = fake
 
     const r1 = await jsonFetch('anotherfakeurl')
     const r2 = await jsonFetch('anotherfakeurl')
