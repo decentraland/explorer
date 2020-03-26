eeb04c198049af8308a63aff76489ad24ddfce32
Brian Amadori <brian@decentraland.org>
2020-03-26 11:30:24 -0300

    make gizmos draw optional to improve editor time performance. code cleanup.

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/MapRenderer/MapRenderer.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/MapRenderer/MapRenderer.cs
@@ -1,4 +1,4 @@
-﻿using DCL.Helpers;
+using DCL.Helpers;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -34,35 +34,34 @@ namespace DCL
 
         private void MapRenderer_OnSceneInfoUpdated(MinimapMetadata.MinimapSceneInfo sceneInfo)
         {
+            if (!sceneInfo.isPOI)
+                return;
+
             if (scenesOfInterest.Contains(sceneInfo))
                 return;
 
-            Debug.Log($"sceneInfo name = {sceneInfo.name} ... type = {sceneInfo.type}");
+            Debug.Log($"sceneInfo name = {sceneInfo.name} ... type = {sceneInfo.type} .. json = {JsonUtility.ToJson(sceneInfo, true)}");
+            scenesOfInterest.Add(sceneInfo);
 
-            if (sceneInfo.isPOI || sceneInfo.type == MinimapMetadata.TileType.Plaza)
+            GameObject go = new GameObject(sceneInfo.name);
+            go.transform.parent = overlayContainer.transform;
+
+            Vector2 centerTile = Vector2.zero;
+
+            foreach (var parcel in sceneInfo.parcels)
             {
-                scenesOfInterest.Add(sceneInfo);
-
-                GameObject go = new GameObject(sceneInfo.name);
-                go.transform.parent = overlayContainer.transform;
-
-                Vector2 centerTile = Vector2.zero;
-
-                foreach (var parcel in sceneInfo.parcels)
-                {
-                    centerTile += parcel;
-                }
-
-                centerTile /= sceneInfo.parcels.Count;
-
-                go.transform.localPosition = MapUtils.GetTileToLocalPosition(centerTile.x, centerTile.y);
-                var text = go.AddComponent<Text>();
-
-                text.text = sceneInfo.name;
-                text.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
-
-                scenesOfInterestMarkers.Add(sceneInfo, go);
+                centerTile += parcel;
             }
+
+            centerTile /= sceneInfo.parcels.Count;
+
+            go.transform.localPosition = MapUtils.GetTileToLocalPosition(centerTile.x, centerTile.y);
+            var text = go.AddComponent<Text>();
+
+            text.text = sceneInfo.name;
+            text.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+
+            scenesOfInterestMarkers.Add(sceneInfo, go);
         }
 
         public void OnDestroy()
