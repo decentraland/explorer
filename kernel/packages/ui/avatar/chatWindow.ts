38e586b184525b171a821466ff2a46770251cb66
Pravus <pravus@decentraland.org>
2020-03-26 13:14:36 -0300

    Implemented first approach at avoiding chat messages infinite-accumulation

--- kernel/packages/ui/avatar/chatWindow.ts
+++ kernel/packages/ui/avatar/chatWindow.ts
@@ -21,6 +21,7 @@ declare var dcl: DecentralandInterface
 const INITIAL_INPUT_TEXT_COLOR = Color4.White()
 const PRIMARY_TEXT_COLOR = Color4.White()
 const COMMAND_COLOR = Color4.FromHexString('#80ffe5ff')
+const MAX_LOGGED_MESSAGES = 10
 
 // UI creators -------------------
 dcl.subscribe('MESSAGE_RECEIVED')
@@ -45,21 +46,30 @@ function createLogMessage(parent: UIShape, props: { sender: string; message: str
   messageText.hAlign = 'left'
   messageText.vTextAlign = 'top'
   messageText.hTextAlign = 'left'
-  messageText.width = '350px'//330
+  messageText.width = '350px'
   messageText.adaptWidth = false
   messageText.adaptHeight = true
   messageText.textWrapping = true
   messageText.outlineColor = Color4.Black()
+
+  internalState.loggedMessages.push(messageText)
 
   messagesLogScrollContainer.valueY = 0
 
   return { component: messageText }
 }
 
+function updateLogMessage(index: number, message: MessageEntry) {
+  if(!internalState.loggedMessages[index]) return
+
+  internalState.loggedMessages[index].value = `<b>${message.sender}:</b> ${message.message}`
+}
+
 // -------------------------------
 const internalState = {
   commandsList: [] as Array<any>,
   messages: [] as Array<any>,
+  loggedMessages: [] as Array<UIText>,
   isFocused: false,
   isSliderVisible: false
 }
@@ -91,7 +101,7 @@ messagesLogScrollContainer.vAlign = 'top'
 messagesLogScrollContainer.hAlign = 'left'
 messagesLogScrollContainer.width = '100%'
 messagesLogScrollContainer.height = '90%'
-messagesLogScrollContainer.positionY = '-8px'//6
+messagesLogScrollContainer.positionY = '-8px'
 messagesLogScrollContainer.positionX = -5
 messagesLogScrollContainer.valueY = 1
 messagesLogScrollContainer.isVertical = false
@@ -102,8 +112,8 @@ const messagesLogStackContainer = new UIContainerStack(messagesLogScrollContaine
 messagesLogStackContainer.name = 'messages-log-stack-container'
 messagesLogStackContainer.vAlign = 'bottom'
 messagesLogStackContainer.hAlign = 'center'
-messagesLogStackContainer.width = '100%'//100
-messagesLogStackContainer.height = '100%'//90
+messagesLogStackContainer.width = '100%'
+messagesLogStackContainer.height = '100%'
 messagesLogStackContainer.spacing = 5//inter message
 messagesLogStackContainer.positionX = 4//position about the box
 
@@ -112,8 +122,8 @@ textInputContainer.color = Color4.Clear()
 textInputContainer.name = 'input-text-container'
 textInputContainer.vAlign = 'bottom'
 textInputContainer.hAlign = 'left'
-textInputContainer.width = '100%'//90
-textInputContainer.height = '16%'//20
+textInputContainer.width = '100%'
+textInputContainer.height = '16%'
 
 const textInput = new UIInputText(textInputContainer)
 textInput.name = 'input-text'
@@ -123,8 +133,8 @@ textInput.background = Color4.Clear()
 textInput.focusedBackground = Color4.Clear()
 textInput.placeholder = 'Press enter and start talking...'
 textInput.fontSize = 14
-textInput.width = '90%'//90
-textInput.height = '16%'//18
+textInput.width = '90%'
+textInput.height = '16%'
 textInput.thickness = 0
 textInput.vAlign = 'center'
 textInput.hAlign = 'center'
@@ -199,5 +209,16 @@ async function sendMsg(messageToSend: string) {
 
 function addMessage(messageEntry: MessageEntry): void {
   internalState.messages.push(messageEntry)
-  createLogMessage(messagesLogStackContainer, messageEntry)
+
+  if(internalState.messages.length <= MAX_LOGGED_MESSAGES) {
+    createLogMessage(messagesLogStackContainer, messageEntry)
+  } else {
+    // remove oldest element
+    internalState.messages.shift()
+
+    // update logged messages ui text elements
+    for (let index = 0; index < MAX_LOGGED_MESSAGES; index++) {
+      updateLogMessage(index, internalState.messages[index])
+    }
+  }
 }
