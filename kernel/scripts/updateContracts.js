8360adde5c9eafee4f4284d0dfa4e75f3e1769f8
Esteban Ordano <esteban@decentraland.org>
2020-04-23 12:02:06 -0300

    fix: compile-time fetch of file

---
+++ kernel/scripts/updateContracts.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch')
+const fs = require('fs')
+
+fetch('https://contracts.decentraland.org/addresses.json').then((res, error) => {
+  if (error) {
+    console.error(error)
+    process.exit(-1)
+  }
+  res.json().then((json, parseError) => {
+    if (error) {
+      console.error(error)
+      process.exit(-1)
+    }
+    fs.writeFileSync('packages/config/contracts.ts', 'export const contracts = ' + JSON.stringify(json, null, 2))
+    process.exit(0)
+  })
+}).catch(error => {
+  console.error(error)
+  preocess.exit(-1)
+})
