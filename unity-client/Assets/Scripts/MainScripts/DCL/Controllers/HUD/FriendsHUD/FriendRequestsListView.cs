e2724b3fa5a8583d445fe7936690afca92cc3219
Pravus <pravus@decentraland.org>
2020-04-28 17:04:07 -0300

    Started wiring entries creation

---
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/FriendsHUD/FriendRequestsListView.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class FriendRequestsListView : MonoBehaviour
+{
+    public GameObject friendRequestEntryPrefab;
+    public Transform receivedRequestsContainer;
+    public Transform sentRequestsContainer;
+
+    FriendRequestEntry friendRequestEntry;
+    Dictionary<string, FriendRequestEntry> friendEntries = new Dictionary<string, FriendRequestEntry>();
+
+    public void UpdateOrCreateFriendRequestEntry(string userId, bool isReceived)
+    {
+        friendRequestEntry = friendEntries[userId];
+
+        if (friendRequestEntry == null)
+        {
+            friendRequestEntry = Instantiate(friendRequestEntryPrefab).GetComponent<FriendRequestEntry>();
+
+            // TODO
+            // friendRequestEntry.Populate();
+        }
+
+        friendRequestEntry.transform.SetParent(isReceived ? receivedRequestsContainer : sentRequestsContainer);
+    }
+}
---
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/FriendsHUD/FriendsListView.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class FriendsListView : MonoBehaviour
+{
+    public GameObject friendEntryPrefab;
+    public Transform onlineFriendsContainer;
+    public Transform offlineFriendsContainer;
+
+    FriendEntry friendEntry;
+    Dictionary<string, FriendEntry> friendEntries = new Dictionary<string, FriendEntry>();
+
+    public void UpdateOrCreateFriendEntry(string userId, bool isOnline)
+    {
+        friendEntry = friendEntries[userId];
+
+        if (friendEntry == null)
+        {
+            friendEntry = Instantiate(friendEntryPrefab).GetComponent<FriendEntry>();
+
+            // TODO
+            // friendEntry.Populate();
+        }
+
+        friendEntry.transform.SetParent(isOnline ? onlineFriendsContainer : offlineFriendsContainer);
+    }
+}
