6376f3f1c750dc7198f209c4ff672713579ba150
Brian Amadori <brian@decentraland.org>
2020-04-28 15:29:53 -0300

    tab animation fixes

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/WorldChatWindowHUD/WorldChatWindowHUDView.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/WorldChatWindowHUD/WorldChatWindowHUDView.cs
@@ -17,6 +17,13 @@ public class WorldChatWindowHUDView : MonoBehaviour, IPointerClickHandler
     public CanvasGroup group;
     public WorldChatWindowHUDController controller;
 
+    TabMode tabMode = TabMode.WORLD;
+    enum TabMode
+    {
+        WORLD,
+        PRIVATE
+    }
+
     public static WorldChatWindowHUDView Create(UnityAction onPrivateMessages, UnityAction onWorldMessages)
     {
         var view = Instantiate(Resources.Load<GameObject>(VIEW_PATH)).GetComponent<WorldChatWindowHUDView>();
@@ -24,21 +31,41 @@ public class WorldChatWindowHUDView : MonoBehaviour, IPointerClickHandler
         return view;
     }
 
+    void OnEnable()
+    {
+        UpdateTabAnimators();
+    }
+
+    void UpdateTabAnimators()
+    {
+        switch (tabMode)
+        {
+            case TabMode.WORLD:
+                pmFilterButton.animator.SetBool(ANIM_PROPERTY_SELECTED, false);
+                worldFilterButton.animator.SetBool(ANIM_PROPERTY_SELECTED, true);
+                break;
+            case TabMode.PRIVATE:
+                pmFilterButton.animator.SetBool(ANIM_PROPERTY_SELECTED, true);
+                worldFilterButton.animator.SetBool(ANIM_PROPERTY_SELECTED, false);
+                break;
+        }
+    }
+
     private void Initialize(UnityAction onPrivateMessages, UnityAction onWorldMessages)
     {
         this.closeButton.onClick.AddListener(Toggle);
         this.pmFilterButton.onClick.AddListener(() =>
            {
-               pmFilterButton.animator.SetBool(ANIM_PROPERTY_SELECTED, true);
-               worldFilterButton.animator.SetBool(ANIM_PROPERTY_SELECTED, false);
                onPrivateMessages.Invoke();
+               tabMode = TabMode.PRIVATE;
+               UpdateTabAnimators();
            });
 
         this.worldFilterButton.onClick.AddListener(() =>
            {
-               pmFilterButton.animator.SetBool(ANIM_PROPERTY_SELECTED, false);
-               worldFilterButton.animator.SetBool(ANIM_PROPERTY_SELECTED, true);
                onWorldMessages.Invoke();
+               tabMode = TabMode.WORLD;
+               UpdateTabAnimators();
            });
     }
 
