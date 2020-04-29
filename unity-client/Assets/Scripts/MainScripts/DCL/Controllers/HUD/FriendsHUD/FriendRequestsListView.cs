0065801568b60d3062407e3d7395d57364457651
Pravus <pravus@decentraland.org>
2020-04-29 16:34:45 -0300

    Hooked up friend search input

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/FriendsHUD/FriendRequestsListView.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/FriendsHUD/FriendRequestsListView.cs
@@ -1,20 +1,25 @@
 ﻿using System.Collections.Generic;
+using System.Collections;
 using TMPro;
 using UnityEngine.UI;
 using UnityEngine;
 
 public class FriendRequestsListView : MonoBehaviour
 {
+    public float notificationsDuration = 3f;
+
     [SerializeField] GameObject friendRequestEntryPrefab;
     [SerializeField] Transform receivedRequestsContainer;
     [SerializeField] Transform sentRequestsContainer;
+
+    [SerializeField] TMP_InputField friendSearchInputField;
 
     [SerializeField] GameObject requestSentNotification;
     [SerializeField] TextMeshProUGUI requestSentNotificationText;
     [SerializeField] GameObject friendSearchFailedNotification;
     [SerializeField] GameObject acceptedFriendNotification;
     [SerializeField] TextMeshProUGUI acceptedFriendNotificationText;
-    
+
     [SerializeField] GameObject rejectRequestDialog;
     [SerializeField] TextMeshProUGUI rejectRequestDialogText;
     [SerializeField] Button rejectRequestDialogCancelButton;
@@ -26,6 +31,40 @@ public class FriendRequestsListView : MonoBehaviour
 
     FriendRequestEntry friendRequestEntry;
     Dictionary<string, FriendRequestEntry> friendRequestEntries = new Dictionary<string, FriendRequestEntry>();
+    IEnumerator currentNotificationRoutine = null;
+    GameObject currentNotification = null;
+
+    void Awake()
+    {
+        friendSearchInputField.onSubmit.AddListener(SendFriendRequest);
+    }
+
+    void SendFriendRequest(string targetFriendId)
+    {
+        // TODO: Check existence with kernel, if the user exists trigger requestSentNotification
+
+        TriggerNotification(friendSearchFailedNotification);
+    }
+
+    void TriggerNotification(GameObject notificationGameobject)
+    {
+        if (currentNotificationRoutine != null)
+        {
+            StopCoroutine(currentNotificationRoutine);
+            currentNotification.SetActive(false);
+            currentNotification = null;
+        }
+
+        notificationGameobject.SetActive(true);
+        StartCoroutine(WaitAndCloseNotification(notificationGameobject));
+    }
+
+    IEnumerator WaitAndCloseNotification(GameObject notificationGameobject)
+    {
+        currentNotification = notificationGameobject;
+
+        yield return WaitForSecondsCache.Get(notificationsDuration);
+    }
 
     public void UpdateOrCreateFriendRequestEntry(string userId, bool isReceived)
     {
