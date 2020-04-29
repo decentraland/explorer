499ad0dafc920bbca8c14e57880b281da3d606c2
Pravus <pravus@decentraland.org>
2020-04-29 13:17:08 -0300

    Connected notifications/Dialogs references in monobehaviour

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/FriendsHUD/FriendRequestsListView.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/FriendsHUD/FriendRequestsListView.cs
@@ -1,11 +1,28 @@
 ﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class FriendRequestsListView : MonoBehaviour
 {
-    public GameObject friendRequestEntryPrefab;
-    public Transform receivedRequestsContainer;
-    public Transform sentRequestsContainer;
+    [SerializeField] GameObject friendRequestEntryPrefab;
+    [SerializeField] Transform receivedRequestsContainer;
+    [SerializeField] Transform sentRequestsContainer;
+
+    [SerializeField] GameObject requestSentNotification;
+    [SerializeField] TextMeshProUGUI requestSentNotificationText;
+    [SerializeField] GameObject friendSearchFailedNotification;
+    [SerializeField] GameObject acceptedFriendNotification;
+    [SerializeField] TextMeshProUGUI acceptedFriendNotificationText;
+    
+    [SerializeField] GameObject rejectRequestDialog;
+    [SerializeField] TextMeshProUGUI rejectRequestDialogText;
+    [SerializeField] Button rejectRequestDialogCancelButton;
+    [SerializeField] Button rejectRequestDialogConfirmButton;
+    [SerializeField] GameObject cancelRequestDialog;
+    [SerializeField] TextMeshProUGUI cancelRequestDialogText;
+    [SerializeField] Button cancelRequestDialogCancelButton;
+    [SerializeField] Button cancelRequestDialogConfirmButton;
 
     FriendRequestEntry friendRequestEntry;
     Dictionary<string, FriendRequestEntry> friendRequestEntries = new Dictionary<string, FriendRequestEntry>();
