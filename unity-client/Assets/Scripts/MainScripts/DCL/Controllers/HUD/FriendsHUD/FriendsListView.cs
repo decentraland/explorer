5b20d0ba667dc1aa3ca0926d05c0776957c1d2a9
Pravus <pravus@decentraland.org>
2020-04-30 01:21:04 -0300

    Fixed friend entry initialization

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/FriendsHUD/FriendsListView.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/FriendsHUD/FriendsListView.cs
@@ -1,5 +1,6 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FriendsListView : MonoBehaviour
 {
@@ -30,6 +31,9 @@ public class FriendsListView : MonoBehaviour
 
         friendEntry.Populate(model);
         friendEntry.transform.SetParent(model.status == FriendEntry.Model.Status.ONLINE ? onlineFriendsContainer : offlineFriendsContainer);
+        friendEntry.transform.localScale = Vector3.one;
+
+        LayoutRebuilder.ForceRebuildLayoutImmediate(friendEntry.transform.parent as RectTransform);
     }
 
     public void RemoveFriend(string userId)
@@ -37,7 +41,39 @@ public class FriendsListView : MonoBehaviour
         if (!friendEntries.ContainsKey(userId))
             return;
 
+        RectTransform containerRectTransform = friendEntries[userId].transform.parent as RectTransform;
+
         Object.Destroy(friendEntries[userId].gameObject);
         friendEntries.Remove(userId);
+
+        LayoutRebuilder.ForceRebuildLayoutImmediate(containerRectTransform);
+    }
+
+    [ContextMenu("AddFakeOnlineFriend")]
+    public void AddFakeOnlineFriend()
+    {
+        var model1 = new FriendEntry.Model()
+        {
+            status = FriendEntry.Model.Status.ONLINE,
+            userName = "Pravus",
+        };
+
+        string id1 = Random.Range(0, 1000000).ToString();
+
+        UpdateOrCreateFriendEntry(id1, model1);
+    }
+
+    [ContextMenu("AddFakeOfflineFriend")]
+    public void AddFakeOfflineFriend()
+    {
+        var model1 = new FriendEntry.Model()
+        {
+            status = FriendEntry.Model.Status.OFFLINE,
+            userName = "Brian",
+        };
+
+        string id1 = Random.Range(0, 1000000).ToString();
+
+        UpdateOrCreateFriendEntry(id1, model1);
     }
 }
