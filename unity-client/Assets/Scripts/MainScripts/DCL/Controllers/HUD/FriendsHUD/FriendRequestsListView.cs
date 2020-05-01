bedd919ea1d8b438ad6d5ca0e2d3c5e41075d64a
Pravus <pravus@decentraland.org>
2020-05-01 14:23:00 -0300

    Improved friend requests list notifications and search input logic

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/FriendsHUD/FriendRequestsListView.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/FriendsHUD/FriendRequestsListView.cs
@@ -38,7 +38,7 @@ public class FriendRequestsListView : MonoBehaviour
     [SerializeField] Button cancelRequestDialogConfirmButton;
 
     Dictionary<string, FriendRequestEntry> friendRequestEntries = new Dictionary<string, FriendRequestEntry>();
-    IEnumerator currentNotificationRoutine = null;
+    Coroutine currentNotificationRoutine = null;
     GameObject currentNotification = null;
     FriendRequestEntry selectedRequestEntry = null;
 
@@ -61,6 +61,7 @@ public class FriendRequestsListView : MonoBehaviour
     void Awake()
     {
         friendSearchInputField.onSubmit.AddListener(SendFriendRequest);
+        friendSearchInputField.onValueChanged.AddListener(OnSearchInputValueChanged);
 
         playerPassportButton.onClick.AddListener(() => { OnPassport?.Invoke(selectedRequestEntry.userId); ToggleMenuPanel(selectedRequestEntry); });
         blockPlayerButton.onClick.AddListener(() => { OnBlock?.Invoke(selectedRequestEntry.userId); ToggleMenuPanel(selectedRequestEntry); });
@@ -89,34 +90,68 @@ public class FriendRequestsListView : MonoBehaviour
     void SendFriendRequest(string friendId)
     {
         // TODO: Check existence with kernel, if the user exists trigger requestSentNotification
+        bool targetUserExists = false;
 
-        requestSentNotificationText.text = $"Your request to {friendId} successfully sent!";
-        TriggerNotification(requestSentNotification);
+        if (targetUserExists)
+        {
+            requestSentNotificationText.text = $"Your request to {friendId} successfully sent!";
+            TriggerNotification(requestSentNotification);
 
-        OnFriendRequestSent?.Invoke(friendId);
-        // If friend Id doesn't exist:
-        // TriggerNotification(friendSearchFailedNotification);
+            // TODO: make sure the input clears and has the placeholder text again
+            // friendSearchInputField.
+
+            addFriendButton.gameObject.SetActive(false);
+
+            OnFriendRequestSent?.Invoke(friendId);
+        }
+        else
+        {
+            TriggerNotification(friendSearchFailedNotification);
+
+            addFriendButton.interactable = false;
+        }
+    }
+
+    void OnSearchInputValueChanged(string friendId)
+    {
+        if (!addFriendButton.gameObject.activeSelf)
+            addFriendButton.gameObject.SetActive(true);
+
+        if (!addFriendButton.interactable)
+            addFriendButton.interactable = true;
+
+        DismissCurrentNotification();
+    }
+
+    void DismissCurrentNotification()
+    {
+        if (currentNotificationRoutine == null) return;
+
+        StopCoroutine(currentNotificationRoutine);
+        currentNotificationRoutine = null;
+
+        currentNotification.SetActive(false);
+        currentNotification = null;
     }
 
     void TriggerNotification(GameObject notificationGameobject)
     {
-        if (currentNotificationRoutine != null)
-        {
-            StopCoroutine(currentNotificationRoutine);
-            currentNotification.SetActive(false);
-            currentNotification = null;
-        }
+        DismissCurrentNotification();
 
-        notificationGameobject.SetActive(true);
-        StartCoroutine(WaitAndCloseNotification(notificationGameobject));
-    }
-
-    IEnumerator WaitAndCloseNotification(GameObject notificationGameobject)
-    {
         currentNotification = notificationGameobject;
 
+        notificationGameobject.SetActive(true);
+        currentNotificationRoutine = StartCoroutine(WaitAndCloseCurrentNotification(notificationGameobject));
+    }
+
+    IEnumerator WaitAndCloseCurrentNotification(GameObject notificationGameobject)
+    {
         yield return WaitForSecondsCache.Get(notificationsDuration);
+
+        currentNotificationRoutine = null;
+
         notificationGameobject.SetActive(false);
+        currentNotification = null;
     }
 
     public bool CreateEntry(string userId)
