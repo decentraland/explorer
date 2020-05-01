98ffb224df2ac3af499e57cbd79690979d88cf14
moliva <oliva.miguelh@gmail.com>
2020-05-01 14:00:55 -0300

    feat: adding loopback after outgoing friendship update action

--- kernel/packages/shared/chat/actions.ts
+++ kernel/packages/shared/chat/actions.ts
@@ -1,7 +1,6 @@
 import { action } from 'typesafe-actions'
 import { ChatMessage, FriendshipAction } from '../types'
-import { SocialAPI } from 'dcl-social-client'
-import { SocialData, ChatState } from './types'
+import { ChatState } from './types'
 
 export const MESSAGE_RECEIVED = 'Message received'
 export const messageReceived = (message: ChatMessage) => action(MESSAGE_RECEIVED, message)
@@ -15,24 +14,15 @@ export const SEND_PRIVATE_MESSAGE = '[Request] Send private message'
 export const sendPrivateMessage = (userId: string, message: string) => action(SEND_PRIVATE_MESSAGE, { userId, message })
 export type SendPrivateMessage = ReturnType<typeof sendPrivateMessage>
 
-export const SOCIAL_CLIENT_INITIALIZED = 'Social client initalized'
-export const clientInitialized = (
-  client: SocialAPI,
-  socialInfo: Record<string, SocialData>,
-  friends: string[],
-  toFriendRequests: string[],
-  fromFriendRequests: string[]
-) => action(SOCIAL_CLIENT_INITIALIZED, { client, socialInfo, friends, toFriendRequests, fromFriendRequests })
-export type ClientInitialized = ReturnType<typeof clientInitialized>
-
 export const UPDATE_FRIENDSHIP = 'Update friendship'
 export const updateFriendship = (_action: FriendshipAction, userId: string, incoming: boolean) =>
   action(UPDATE_FRIENDSHIP, { action: _action, userId }, { incoming })
 export type UpdateFriendship = ReturnType<typeof updateFriendship>
 
 export const UPDATE_PRIVATE_MESSAGING = 'Update private messaging state'
-export const updateState = (state: ChatState['privateMessaging']) => action(UPDATE_PRIVATE_MESSAGING, state)
-export type UpdateState = ReturnType<typeof updateState>
+export const updatePrivateMessagingState = (state: ChatState['privateMessaging']) =>
+  action(UPDATE_PRIVATE_MESSAGING, state)
+export type UpdatePrivateMessagingState = ReturnType<typeof updatePrivateMessagingState>
 
 export const UPDATE_USER_DATA = 'Update user data'
 export const updateUserData = (userId: string, socialId: string, conversationId?: string) =>
