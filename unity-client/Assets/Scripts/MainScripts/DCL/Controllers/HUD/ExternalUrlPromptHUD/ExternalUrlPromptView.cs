3ed1748e30afbb9b723a8aa583c8e30b2c1f9db1
Pato <pato@decentraland.org>
2020-05-05 17:16:58 -0300

    added prompt prefab

---
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/ExternalUrlPromptHUD/ExternalUrlPromptView.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ExternalUrlPromptView : MonoBehaviour
+{
+    [SerializeField] GameObject content;
+    [SerializeField] Button closeButton;
+    [SerializeField] Button continueButton;
+    [SerializeField] Button cancelButton;
+    [SerializeField] TMPro.TextMeshProUGUI domainText;
+    [SerializeField] TMPro.TextMeshProUGUI urlText;
+    [SerializeField] Toggle trustToggle;
+}
