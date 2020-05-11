66eb8f9d6be68f38c5ddbe4136d79398bf407d10
Pravus <pravus@decentraland.org>
2020-05-11 17:08:01 -0300

    Created private chat window prefab and its view and controller (WIP)

---
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/PrivateChatWindow/PrivateChatWindowHUDController.cs
@@ -0,0 +1,41 @@
+﻿using DCL;
+using DCL.Interface;
+using System.Collections;
+using UnityEngine;
+
+public class PrivateChatWindowHUDController : IHUD
+{
+    private ChatHUDController chatHudController;
+    public PrivateChatWindowHUDView view;
+
+    private IChatController chatController;
+
+    public void SetVisibility(bool visible)
+    {
+        view.gameObject.SetActive(visible);
+
+        if (visible)
+        {
+            view.StartCoroutine(ForceLayoutDelayed());
+        }
+    }
+
+    public void Dispose()
+    {
+        if (chatController != null)
+            chatController.OnAddMessage -= OnAddMessage;
+
+        Object.Destroy(view);
+    }
+
+    void OnAddMessage(ChatMessage message)
+    {
+        view.chatHudView.controller.AddChatMessage(ChatHUDController.ChatMessageToChatEntry(message));
+    }
+
+    IEnumerator ForceLayoutDelayed()
+    {
+        yield return null;
+        view.chatHudView.ForceUpdateLayout();
+    }
+}
---
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/PrivateChatWindow/PrivateChatWindowHUDView.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class PrivateChatWindowHUDView : MonoBehaviour
+{
+    const string VIEW_PATH = "PrivateChatWindow";
+
+    public Button closeButton;
+    public ChatHUDView chatHudView;
+    public PrivateChatWindowHUDController controller;
+    public TMP_Text windowTitleText;
+
+    public static PrivateChatWindowHUDView Create(UnityAction onPrivateMessages, UnityAction onWorldMessages)
+    {
+        var view = Instantiate(Resources.Load<GameObject>(VIEW_PATH)).GetComponent<PrivateChatWindowHUDView>();
+        view.Initialize(onPrivateMessages, onWorldMessages);
+        return view;
+    }
+
+    private void Initialize(UnityAction onPrivateMessages, UnityAction onWorldMessages)
+    {
+        this.closeButton.onClick.AddListener(Toggle);
+    }
+
+    public void Toggle()
+    {
+        if (gameObject.activeSelf)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            gameObject.SetActive(true);
+            chatHudView.ForceUpdateLayout();
+        }
+    }
+}
