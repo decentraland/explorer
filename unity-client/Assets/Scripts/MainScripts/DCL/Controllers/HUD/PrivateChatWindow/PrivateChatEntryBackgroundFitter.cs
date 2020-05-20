d6b0c048739d4f436cc3b33c14f8cf79fce05b37
Pravus <pravus@decentraland.org>
2020-05-20 12:44:10 -0300

    Fixed chat messages dynamic background

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/PrivateChatWindow/PrivateChatEntryBackgroundFitter.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/PrivateChatWindow/PrivateChatEntryBackgroundFitter.cs
@@ -7,10 +7,14 @@ public class PrivateChatEntryBackgroundFitter : MonoBehaviour
     public RectTransform rectTransform;
     public RectTransform parentContainerRectTransform;
     public TextMeshProUGUI messageText;
+    public bool leftMessage = true;
 
     void Update()
     {
-        Vector2 textSize = new Vector2(messageText.textBounds.size.x + messageText.margin.x * 2, messageText.textBounds.size.y + messageText.margin.y * 2);
+        Vector2 textSize = new Vector2(messageText.bounds.size.x + messageText.margin.x + messageText.margin.z,
+                                    messageText.bounds.size.y + messageText.margin.y * 2);
+
+        messageText.transform.localPosition = new Vector3(leftMessage ? (messageText.margin.x + messageText.margin.z) / 2 : 0f, messageText.transform.localPosition.y, 0);
 
         if (parentContainerRectTransform)
         {
