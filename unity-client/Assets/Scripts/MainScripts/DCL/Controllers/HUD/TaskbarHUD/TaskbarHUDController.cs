090b52fa7ef3bd5f17c05f6e0ec4cd76ff0baa8f
Santi Andrade <sandrade@decentraland.org>
2020-05-28 18:07:17 +0200

    Code legibility improvement

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/TaskbarHUD/TaskbarHUDController.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/TaskbarHUD/TaskbarHUDController.cs
@@ -313,8 +313,10 @@ public class TaskbarHUDController : IHUD
 
     public void OnPressReturn()
     {
-        if ((privateChatWindowHud != null && privateChatWindowHud.view.gameObject.activeSelf) ||
-            (friendsHud != null && friendsHud.view.friendRequestsList.gameObject.activeSelf))
+        bool isPrivateChatWindowOpen = privateChatWindowHud != null && privateChatWindowHud.view.gameObject.activeSelf;
+        bool isFriendRequestsWindowOpen = friendsHud != null &&  friendsHud.view.friendRequestsList.gameObject.activeSelf;
+
+        if (isPrivateChatWindowOpen || isFriendRequestsWindowOpen)
             return;
 
         worldChatWindowHud.OnPressReturn();
