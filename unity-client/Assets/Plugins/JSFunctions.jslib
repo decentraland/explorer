e3051496eddcb82670c45c3305ec79e27bdfc74b
Pravus <pravus@decentraland.org>
2020-05-29 15:25:22 -0300

    Added mouse lock/unlock manual js call

--- unity-client/Assets/Plugins/JSFunctions.jslib
+++ unity-client/Assets/Plugins/JSFunctions.jslib
@@ -8,5 +8,14 @@ mergeInto(LibraryManager.library, {
   },
   MessageFromEngine: function(type, message) {
     window.DCL.MessageFromEngine(Pointer_stringify(type), Pointer_stringify(message));
+  },
+  LockBrowserCursor: function() {
+    var canvas = document.getElementsByTagName("canvas")[0];
+    canvas.requestPointerLock = canvas.requestPointerLock || canvas.mozRequestPointerLock;
+    canvas.requestPointerLock();
+  },
+    UnlockBrowserCursor: function() {
+    document.exitPointerLock = document.exitPointerLock || document.mozExitPointerLock;
+    document.exitPointerLock();
   }
 });
--- unity-client/Assets/Scripts/MainScripts/DCL/Helpers/Utils/Utils.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Helpers/Utils/Utils.cs
@@ -8,6 +8,10 @@ using UnityEngine.EventSystems;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 
+#if UNITY_WEBGL && !UNITY_EDITOR
+using System.Runtime.InteropServices;
+#endif
+
 namespace DCL.Helpers
 {
     public static class Utils
@@ -475,26 +479,51 @@ namespace DCL.Helpers
         }
 
         private static int lockedInFrame = -1;
+        private static int unlockedInFrame = -1;
         public static bool LockedThisFrame() => lockedInFrame == Time.frameCount;
 
         //NOTE(Brian): Made as an independent flag because the CI doesn't work well with the Cursor.lockState check.
         public static bool isCursorLocked = false;
 
+#if UNITY_WEBGL && !UNITY_EDITOR
+    [DllImport("__Internal")] public static extern void LockBrowserCursor();
+#endif
         public static void LockCursor()
         {
+            if (isCursorLocked) return;
+
             isCursorLocked = true;
             lockedInFrame = Time.frameCount;
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
 
             EventSystem.current.SetSelectedGameObject(null);
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+            // We have to "manually" notify the browser that we require the lock/unlock or sometimes we get a false-lock that messes up the interactions
+            LockBrowserCursor();
+#endif
         }
 
+#if UNITY_WEBGL && !UNITY_EDITOR
+    [DllImport("__Internal")] public static extern void UnlockBrowserCursor();
+#endif
         public static void UnlockCursor()
         {
+            if (!isCursorLocked) return;
+
             isCursorLocked = false;
+            unlockedInFrame = Time.frameCount;
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
+
+            EventSystem.current.SetSelectedGameObject(null);
+
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+            // We have to "manually" notify the browser that we require the lock/unlock or sometimes we get a false-lock that messes up the interactions
+            UnlockBrowserCursor();
+#endif
         }
 
         public static void DestroyAllChild(this Transform transform)
