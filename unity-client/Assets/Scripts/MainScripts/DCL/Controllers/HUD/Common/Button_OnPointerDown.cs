7ac555368b83f79972c5e03772ccf2ebebb9cb97
Pravus <pravus@decentraland.org>
2020-06-03 09:59:59 -0300

    Added summary comments in Button_OnPointerDown

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Common/Button_OnPointerDown.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/Common/Button_OnPointerDown.cs
@@ -3,7 +3,9 @@ using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-// In oder to lock/unlock the cursor in the same frame a button is clicked we need to use OnPointerDown (event not accesible in the regular Button)
+/// <summary>
+/// In oder to lock/unlock the cursor in the same frame a button is clicked we need to use OnPointerDown (event not accesible in the regular Button)
+/// </summary>
 public class Button_OnPointerDown : Button
 {
     public event UnityAction onPointerDown;
