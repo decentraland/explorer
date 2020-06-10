8c19ab3fd254d5c631cb75104c8c040a7f57eb7b
Pato <pato@decentraland.org>
2020-06-10 19:39:12 -0300

    group together requests to OpenSea api in a single request and cache responses

---
+++ unity-client/Assets/Scripts/MainScripts/DCL/Helpers/NFTHelper/NFTMarkets/OpenSea_Internal/Request.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+#pragma warning disable CS0162
+
+namespace DCL.Helpers.NFT.Markets.OpenSea_Internal
+{
+    internal class RequestController
+    {
+        internal const bool VERBOSE = false;
+
+        List<RequestGroup> requestGroup = new List<RequestGroup>();
+
+        public Request AddRequest(string assetContractAddress, string tokenId)
+        {
+            RequestGroup group = null;
+            for (int i = 0; i < requestGroup.Count; i++)
+            {
+                if (requestGroup[i].isOpen)
+                {
+                    group = requestGroup[i];
+                    break;
+                }
+            }
+            if (group == null)
+            {
+                group = CreateNewGroup();
+            }
+            return group.AddRequest(assetContractAddress, tokenId);
+        }
+
+        RequestGroup CreateNewGroup()
+        {
+            if (VERBOSE) Debug.Log("RequestGroup created");
+            RequestGroup group = new RequestGroup(OnGroupClosed);
+            requestGroup.Add(group);
+            return group;
+        }
+
+        void OnGroupClosed(RequestGroup group)
+        {
+            if (VERBOSE) Debug.Log("RequestGroup closed");
+            if (requestGroup.Contains(group))
+            {
+                requestGroup.Remove(group);
+            }
+        }
+    }
+
+    class RequestGroup : IDisposable
+    {
+        const string API_URL_ASSETS = "https://api.opensea.io/api/v1/assets?";
+        const float WAIT_TIME = 0.2f;
+
+        public bool isOpen { private set; get; }
+
+        List<Request> requests = new List<Request>();
+
+        Coroutine fetchRoutine = null;
+        Action<RequestGroup> onGroupClosed = null;
+
+        public RequestGroup(Action<RequestGroup> onGroupClosed)
+        {
+            isOpen = true;
+            this.onGroupClosed = onGroupClosed;
+            fetchRoutine = CoroutineStarter.Start(Fetch());
+        }
+
+        public Request AddRequest(string assetContractAddress, string tokenId)
+        {
+            Request request = new Request(assetContractAddress, tokenId);
+            requests.Add(request);
+            return request;
+        }
+
+        IEnumerator Fetch()
+        {
+            yield return new WaitForSeconds(WAIT_TIME);
+            isOpen = false;
+            this.onGroupClosed?.Invoke(this);
+            this.onGroupClosed = null;
+
+            string url = API_URL_ASSETS;
+
+            for (int i = 0; i < requests.Count; i++)
+            {
+                url += requests[i].ToString();
+                if (i < requests.Count - 1)
+                {
+                    url += "&";
+                }
+            }
+
+            if (RequestController.VERBOSE) Debug.Log($"Request to OpenSea {url}");
+
+            using (UnityWebRequest request = UnityWebRequest.Get(url))
+            {
+                yield return request.SendWebRequest();
+
+                AssetsResponse response = null;
+
+                if (!request.isNetworkError && !request.isHttpError)
+                {
+                    response = Utils.FromJsonWithNulls<AssetsResponse>(request.downloadHandler.text);
+                }
+
+                for (int i = 0; i < requests.Count; i++)
+                {
+                    if (response != null)
+                        requests[i].Resolve(response);
+                    else
+                        requests[i].Resolve(request.error);
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            isOpen = false;
+            if (fetchRoutine != null) CoroutineStarter.Stop(fetchRoutine);
+            fetchRoutine = null;
+            if (this.onGroupClosed != null)
+            {
+                this.onGroupClosed?.Invoke(this);
+                this.onGroupClosed = null;
+            }
+        }
+    }
+
+    internal class Request
+    {
+        string assetContractAddress;
+        string tokenId;
+        bool resolved = false;
+
+        AssetResponse assetResponse = null;
+        string error = null;
+
+        public Request(string assetContractAddress, string tokenId)
+        {
+            this.assetContractAddress = assetContractAddress;
+            this.tokenId = tokenId;
+            if (RequestController.VERBOSE) Debug.Log($"Request created {this.ToString()}");
+        }
+
+        public void Resolve(AssetsResponse response)
+        {
+            AssetResponse asset = null;
+            for (int i = 0; i < response.assets.Length; i++)
+            {
+                asset = response.assets[i];
+                if (asset.token_id == tokenId && String.Equals(asset.asset_contract.address, assetContractAddress, StringComparison.OrdinalIgnoreCase))
+                {
+                    if (RequestController.VERBOSE) Debug.Log($"Request resolved {this.ToString()}");
+                    assetResponse = asset;
+                    break;
+                }
+            }
+            if (assetResponse == null)
+            {
+                error = $"asset {assetContractAddress}/{tokenId} not found in api response";
+                if (RequestController.VERBOSE) Debug.Log($"Request not found {JsonUtility.ToJson(response)}");
+            }
+            resolved = true;
+        }
+
+        public void Resolve(string error)
+        {
+            this.error = error;
+            resolved = true;
+        }
+
+        public IEnumerator OnResolved(Action<AssetResponse> onSuccess, Action<string> onError)
+        {
+            yield return new WaitUntil(() => resolved);
+
+            if (assetResponse != null)
+            {
+                onSuccess?.Invoke(assetResponse);
+            }
+            else
+            {
+                onError?.Invoke(error);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"asset_contract_addresses={assetContractAddress}&token_ids={tokenId}";
+        }
+    }
+
+}
