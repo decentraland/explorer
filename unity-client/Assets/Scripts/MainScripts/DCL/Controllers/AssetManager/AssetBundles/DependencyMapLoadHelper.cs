234bcc1b65854f0427ff43f27c0eec2b8d48d70e
Brian Amadori <brian@decentraland.org>
2020-06-11 16:39:45 -0300

    add persistent depmaps caching

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/AssetBundles/DependencyMapLoadHelper.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/AssetBundles/DependencyMapLoadHelper.cs
@@ -1,6 +1,8 @@
-﻿using DCL.Helpers;
+﻿using System;
+using DCL.Helpers;
 using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using UnityEngine;
 using UnityEngine.Networking;
 using WaitUntil = DCL.WaitUntil;
@@ -9,6 +11,9 @@ using WaitUntil = DCL.WaitUntil;
 public static class DependencyMapLoadHelper
 {
     static bool VERBOSE = false;
+
+    private const string PERSISTENT_CACHE_KEY = "DepMapCache";
+    private static bool persistentCacheLoaded = false;
 
     public static Dictionary<string, List<string>> dependenciesMap = new Dictionary<string, List<string>>();
 
@@ -30,6 +35,8 @@ public static class DependencyMapLoadHelper
     public static IEnumerator GetDepMap(string baseUrl, string hash)
     {
         string url = baseUrl + hash + ".depmap";
+
+        LoadPersistentCache();
 
         if (failedRequests.Contains(hash))
             yield break;
@@ -59,7 +66,32 @@ public static class DependencyMapLoadHelper
             AssetDependencyMap map = JsonUtility.FromJson<AssetDependencyMap>(depmapRequest.downloadHandler.text);
 
             dependenciesMap.Add(hash, new List<string>(map.dependencies));
+
+            SavePersistentCache();
+
             downloadingDepmap.Remove(hash);
+        }
+    }
+
+    private static void SavePersistentCache()
+    {
+        //NOTE(Brian): Use JsonConvert because unity JsonUtility doesn't support dictionaries
+        string cacheJson = JsonConvert.SerializeObject(dependenciesMap);
+        PlayerPrefs.SetString(PERSISTENT_CACHE_KEY, cacheJson);
+    }
+
+    private static void LoadPersistentCache()
+    {
+        if (!persistentCacheLoaded)
+        {
+            persistentCacheLoaded = true;
+
+            string depMapCache = PlayerPrefs.GetString(PERSISTENT_CACHE_KEY, String.Empty);
+
+            if (!string.IsNullOrEmpty(depMapCache))
+            {
+                dependenciesMap = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(depMapCache);
+            }
         }
     }
 }
