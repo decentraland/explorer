7e100d9811eec1eedbd927bd5f166f528cb962ed
Pablo de Haro <pablitar@gmail.com>
2020-06-18 16:55:06 -0300

    Fixing linting issues

--- kernel/packages/atomicHelpers/arrayEquals.ts
+++ kernel/packages/atomicHelpers/arrayEquals.ts
@@ -1,7 +1,7 @@
-export function arrayEquals(a: any[] | undefined, b: any[] | undefined): boolean {
+export function arrayEquals(a: any[] | undefined | null, b: any[] | undefined | null): boolean {
   if (a === b) return true
-  if (a == null || b == null) return false
-  if (a.length != b.length) return false
+  if (a === undefined || b === undefined || a === null || b === null) return false
+  if (a.length !== b.length) return false
 
   for (let i = 0; i < a.length; ++i) {
     if (a[i] !== b[i]) return false
