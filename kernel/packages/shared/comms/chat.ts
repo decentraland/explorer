8d986da660f8b2827e288c412251c23fb05b61d0
Pato <pato@decentraland.org>
2020-07-02 19:41:00 -0300

    add chat system message when a friend comes online

--- kernel/packages/shared/comms/chat.ts
+++ kernel/packages/shared/comms/chat.ts
@@ -1,9 +1,11 @@
-import { MessageEntry, ChatMessageType } from 'shared/types'
+import { MessageEntry, ChatMessageType, PresenceStatus } from 'shared/types'
 import { uuid } from 'atomicHelpers/math'
 import { StoreContainer } from '../store/rootTypes'
 import { messageReceived } from '../chat/actions'
+import { getProfile } from 'shared/profiles/selectors'
 
 declare const globalThis: StoreContainer
+let friendStatus: any = {}
 
 export enum ChatEventType {
   MESSAGE_RECEIVED = 'MESSAGE_RECEIVED',
@@ -24,4 +26,28 @@ export function notifyStatusThroughChat(status: string) {
       body: status
     })
   )
+}
+
+export function notifyFriendOnlineStatusThroughChat(userId: string, status: PresenceStatus) {
+  const friendName = getProfile(globalThis.globalStore.getState(), userId)?.name
+
+  if (friendName === undefined) {
+    return
+  }
+
+  if (friendStatus[friendName] === undefined) {
+    friendStatus[friendName] = status
+    return
+  }
+  if (status === PresenceStatus.ONLINE && friendStatus[friendName] !== status) {
+    friendStatus[friendName] = status
+    globalThis.globalStore.dispatch(
+      messageReceived({
+        messageId: uuid(),
+        messageType: ChatMessageType.SYSTEM,
+        timestamp: Date.now(),
+        body: `${friendName} is online.`
+      })
+    )
+  }
 }
