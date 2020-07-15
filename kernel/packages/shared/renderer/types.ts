e3c0a1fbf04af22cee35243bb18e36482d9cdcf4
moliva <oliva.miguelh@gmail.com>
2020-07-15 20:10:40 -0300

    fix: avoid circular dep

--- kernel/packages/shared/renderer/types.ts
+++ kernel/packages/shared/renderer/types.ts
@@ -1,11 +1,9 @@
-import { UnityInterfaceContainer } from 'unity-interface/dcl'
-
 export const RENDERER_INITIALIZED = 'Renderer initialized'
 export const PARCEL_LOADING_STARTED = 'Parcel loading started'
 
 export type RendererState = {
   initialized: boolean
-  instancedJS: Promise<UnityInterfaceContainer> | undefined
+  instancedJS: Promise<any> | undefined
 }
 
 export type RootRendererState = {
