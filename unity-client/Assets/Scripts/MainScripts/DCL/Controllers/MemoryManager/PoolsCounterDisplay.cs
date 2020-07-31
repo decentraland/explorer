37ae4d7e9e2bb4f35cb3662f4fbe0d5c072489b9
Santi Andrade <sandrade@decentraland.org>
2020-07-31 16:25:43 +0200

    Update the temporal UI metrics

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/MemoryManager/PoolsCounterDisplay.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/MemoryManager/PoolsCounterDisplay.cs
@@ -1,8 +1,10 @@
 using DCL;
-using Kongregate;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
+#if UNITY_WEBGL && !UNITY_EDITOR
+using Kongregate;
+#endif
 
 public class PoolsCounterDisplay : MonoBehaviour
 {
@@ -16,9 +18,7 @@ public class PoolsCounterDisplay : MonoBehaviour
         string targetText = string.Empty;
 
 #if UNITY_WEBGL && !UNITY_EDITOR
-        targetText += $"Free Memory: {(WebGLMemoryStats.GetFreeMemorySize() * 100f / WebGLMemoryStats.GetTotalMemorySize()).ToString("##.00")}%\n";
-#else
-        targetText += "Free Memory: 0.00%\n";
+        targetText += $"Used Memory: {WebGLMemoryStats.GetUsedMemorySize() / 1024 / 1024}MB/{WebGLMemoryStats.GetTotalMemorySize() / 1024 / 1024}MB ({(WebGLMemoryStats.GetFreeMemorySize() * 100f / WebGLMemoryStats.GetTotalMemorySize()).ToString("##.00")}%)\n";
 #endif
 
         targetText += $"Total Pools: {PoolManager.i.pools.Count} (Not Persistent: {PoolManager.i.pools.Count(p => !p.Value.persistent)})\n";
