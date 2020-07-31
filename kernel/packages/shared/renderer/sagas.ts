efb85422a247196dbea5504fba4d1ba5d6b6f178
Pravus <pravus@decentraland.org>
2020-07-31 14:39:08 -0300

    mini refactor to avoid lint problems

--- kernel/packages/shared/renderer/sagas.ts
+++ kernel/packages/shared/renderer/sagas.ts
@@ -112,70 +112,81 @@ namespace DCL {
   }
 
   export async function InitializeGIF(imgSource: string, callback: (image: any) => void) {
-    // imgSource = "https://github.com/samthor/fastgif/blob/master/testdata/elves.gif?raw=true";
-    // image.src = "https://i.imgur.com/ZKMnXce.png";
-    // imgSource = "santa.gif";
-    const decoder = new fastgif.Decoder();
+    const decoder = new fastgif.Decoder()
 
-    defaultLogger.log('pravs - InitializeGIF (kernel) - GIF URL is: ' + imgSource);
+    defaultLogger.log('pravs - InitializeGIF (kernel) - GIF URL is: ' + imgSource)
 
-    const canvas = document.createElement('canvas');
-    canvas.id = 'gif-processing-canvas';
-    const ctx = canvas.getContext('2d');
+    const canvas = document.createElement('canvas')
+    canvas.id = 'gif-processing-canvas'
+    const ctx = canvas.getContext('2d')
 
-    if(!ctx) {
-      defaultLogger.log('pravs - InitializeGIF (kernel) - GIF canvas context is null ?!?!?!');
+    if (!ctx) {
+      defaultLogger.log('pravs - InitializeGIF (kernel) - GIF canvas context is null ?!?!?!')
       return
     }
 
-    const response = await fetch(imgSource, { mode: 'no-cors' });
-    const buffer = await response.arrayBuffer();
-    const frames = await decoder.decode(buffer); // an array of {imageData: ImageData, delay: number}
-    let framesAsImage = new Array();
-    let convertedFramesCount = 0;
+    const response = await fetch(imgSource, { mode: 'no-cors' })
+    const buffer = await response.arrayBuffer()
+    const frames = await decoder.decode(buffer) // an array of {imageData: ImageData, delay: number}
+    let framesAsImage = new Array()
+    let convertedFramesCount = 0
 
-    defaultLogger.log("pravs - InitializeGIF (kernel) - Started processing " + frames.length + " frames.");
+    defaultLogger.log('pravs - InitializeGIF (kernel) - Started processing ' + frames.length + ' frames.')
     for (let frame of frames) {
-      defaultLogger.log("pravs - InitializeGIF (kernel) - processing frame w: " + frame.imageData.width + ", h:" + frame.imageData.height);
+      defaultLogger.log(
+        'pravs - InitializeGIF (kernel) - processing frame w: ' +
+          frame.imageData.width +
+          ', h:' +
+          frame.imageData.height
+      )
 
-      canvas.width = frame.imageData.width;
-      canvas.height = frame.imageData.height;
-      ctx.putImageData(frame.imageData, 0, 0);
+      canvas.width = frame.imageData.width
+      canvas.height = frame.imageData.height
+      ctx.putImageData(frame.imageData, 0, 0)
 
-      const image = new Image();
-      image.crossOrigin = "";
-      image.src = canvas.toDataURL();
+      const image = new Image()
+      image.crossOrigin = ''
+      image.src = canvas.toDataURL()
 
-      framesAsImage.push(image);
+      framesAsImage.push(image)
 
       image.onload = function () {
-        convertedFramesCount++;
+        convertedFramesCount++
 
-        defaultLogger.log("pravs - InitializeGIF (kernel) - image loaded, converted frames count: " + convertedFramesCount + "/" + frames.length);
+        defaultLogger.log(
+          'pravs - InitializeGIF (kernel) - image loaded, converted frames count: ' +
+            convertedFramesCount +
+            '/' +
+            frames.length
+        )
 
-        if(convertedFramesCount === frames.length) {
-          StartGIF(frames, framesAsImage, callback);
+        if (convertedFramesCount === frames.length) {
+          StartGIFPromise(frames, framesAsImage, callback)
 
           // we don't need this canvas anymore, so we remove it
-          canvas.parentNode?.removeChild(canvas);
+          canvas.parentNode?.removeChild(canvas)
         }
       }
     }
   }
 
-  async function StartGIF(frames: any[], framesAsImage: any[], updateUnityTex: (image: any) => void)
-  {
-    defaultLogger.log("pravs - InitializeGIF (kernel) - Finished converting frames to Image");
+  function StartGIFPromise(frames: any[], framesAsImage: any[], updateUnityTex: (image: any) => void): void {
+    const promise = StartGIF(frames, framesAsImage, updateUnityTex)
+    promise.catch((error) => defaultLogger.log('ERROR IN StartGIFPromise!', error))
+  }
+
+  async function StartGIF(frames: any[], framesAsImage: any[], updateUnityTex: (image: any) => void) {
+    defaultLogger.log('pravs - InitializeGIF (kernel) - Finished converting frames to Image')
 
     // Play the GIF
-    let frameIndex = 0;
+    let frameIndex = 0
     while (true) {
-      updateUnityTex(framesAsImage[frameIndex]);
+      updateUnityTex(framesAsImage[frameIndex])
 
-      await new Promise((resolve) => window.setTimeout(resolve, frames[frameIndex].delay));
+      await new Promise((resolve) => window.setTimeout(resolve, frames[frameIndex].delay))
 
       if (++frameIndex === frames.length) {
-        frameIndex = 0;
+        frameIndex = 0
       }
     }
   }
