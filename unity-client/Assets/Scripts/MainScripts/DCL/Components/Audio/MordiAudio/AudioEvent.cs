b6cc9c15e088a8d4bb080ae8c8fd3e728ebfc3d8
Martin <martin.lande@hotmail.com>
2020-08-01 12:20:32 +0200

    Initial commit
    + Added reorderable list plugin to plugins-folder
    + Added AudioContainer and AudioEvent scripts. (MainScripts > DCL > Components > Audio > MordiAudio)
    + Added a few common HUD sound effects (Controllers/HUD/Common/SFX)

---
+++ unity-client/Assets/Scripts/MainScripts/DCL/Components/Audio/MordiAudio/AudioEvent.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ReorderableList;
+
+namespace MordiAudio
+{
+    [System.Serializable]
+    public class AudioEvent
+    {
+        [System.Serializable]
+        public class AudioClipList : ReorderableArray<AudioClip>
+        {
+        }
+
+        // Index for clips-array
+        private int index;
+
+        public string name;
+        public bool loop = false;
+        [Range(0f, 1f)]
+        public float volume = 1.0f;
+        [Range(0f, 1f)]
+        public float randomPitch = 0.0f;
+        public bool playOnAwake = false;
+        [Reorderable]
+        public AudioClipList clips;
+
+        [HideInInspector]
+        public AudioSource source;
+
+        public void Initialize()
+        {
+            RandomizeIndex();
+        }
+
+        public void RandomizeIndex()
+        {
+            int newIndex;
+            do
+            {
+                newIndex = Random.Range(0, clips.Length);
+            } while (clips.Length > 1 && newIndex == index);
+            index = newIndex;
+        }
+
+        public void Play(bool oneShot = false)
+        {
+
+            // Check if AudioSource is active
+            if (!source.gameObject.activeSelf)
+            {
+                return;
+            }
+
+            // Set clip
+            source.clip = clips[index];
+
+            // Set pitch
+            source.pitch = 1f + Random.Range(0f, randomPitch) - (randomPitch * 0.5f);
+
+            // Play from source
+            if (oneShot)
+            {
+                source.PlayOneShot(source.clip);
+            }
+            else
+            {
+                source.Play();
+            }
+
+            RandomizeIndex();
+        }
+
+        public void Stop()
+        {
+            source.Stop();
+        }
+
+        public void SetIndex(int index)
+        {
+            this.index = index;
+        }
+    }
+}
+
