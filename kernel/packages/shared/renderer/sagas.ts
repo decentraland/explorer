12057f3fa8164d59864ff08ee98de133fcff8305
Brian Amadori <brian@decentraland.org>
2020-08-05 18:07:00 -0300

    first proof of concept, try to match 1280x720 canvas size without breaking aspect ratio

--- kernel/packages/shared/renderer/sagas.ts
+++ kernel/packages/shared/renderer/sagas.ts
@@ -99,6 +99,10 @@ function* handleMessageFromEngine(
 }
 
 namespace DCL {
+
+  // This exposes JSEvents emscripten's object 
+  export let JSEvents:any
+
   // This function get's called by the engine
   export function EngineStarted() {
     globalThis.globalStore.dispatch(engineStarted())
