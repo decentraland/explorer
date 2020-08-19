8cfac4ec078aa64d2658f5a73899bc26f5dfa450
Pablo de Haro <pablitar@gmail.com>
2020-08-19 16:58:39 -0300

    Added script to compile engine proto

---
+++ kernel/packages/shared/proto/compile-protobuf.sh
@@ -0,0 +1,2 @@
+protoc --plugin=../../../node_modules/ts-protoc-gen/bin/protoc-gen-ts --js_out="import_style=commonjs,binary:." --ts_out="." engineinterface.proto
+protoc --csharp_out=../../../../unity-client/Assets/Scripts/MainScripts/DCL/Models/Protocol --csharp_opt=base_namespace=DCL.Interface engineinterface.proto
