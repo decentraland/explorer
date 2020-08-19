e684e18b83573144283b8ec16e5904829153600f
Pablo de Haro <pablitar@gmail.com>
2020-08-19 12:25:33 -0300

    Added scene to reproduce the issue

--- kernel/public/test-scenes/0.100.fractions/game.ts
+++ kernel/public/test-scenes/0.100.fractions/game.ts
@@ -1,3 +1,5 @@
+import { Entity, engine, Vector3, Transform, BoxShape, log } from 'decentraland-ecs/src'
+
 /// --- Spawner function ---
 
 function spawnCube(x: number, y: number, z: number) {
@@ -5,10 +7,12 @@ function spawnCube(x: number, y: number, z: number) {
   const cube = new Entity()
 
   // add a transform to the entity
-  cube.addComponent(new Transform({
+  cube.addComponent(
+    new Transform({
       position: new Vector3(x, y, z),
       scale: new Vector3(0.1, 0.1, 0.1)
-  }))
+    })
+  )
 
   // add a shape to the entity
   cube.addComponent(new BoxShape())
@@ -22,9 +26,11 @@ function spawnCube(x: number, y: number, z: number) {
 /// --- Spawn cubes ---
 let y = 0
 let x = 0.125
-for (let i = 0; i < 12; i=i+0.1) {
-        spawnCube(x, y, 8)
-        x = x + 0.125
-        y = y + 0.1
-    }
+let a = 0
+for (let i = 0; i < 12; i = i + 0.1) {
+  spawnCube(x, y, 8)
+  log(`${a}: y value: ${y}`)
+  x = x + 0.125
+  y = y + 0.1
+  a++
 }
