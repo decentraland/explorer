4a586eae0f75c83c85ff1623d7262b91ff0a643e
Marcos NC <marcosnc@gmail.com>
2020-08-20 09:24:10 -0300

    Initial refactor for SceneController.

---
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/Scene/Environment.cs
@@ -0,0 +1,34 @@
+﻿namespace DCL
+{
+    class Environment : Singleton<Environment>
+    {
+        public MessagingControllersManager messagingControllersManager { get; }
+
+        /*
+         * TODO: Continue moving static beans to this class
+         * 
+        MemoryManager memoryManager;
+        PointerEventsController pointerEventsController;
+        ParcelScenesCleaner parcelScenesCleaner; // This is a static member of ParcelScene
+        PoolManager poolManager; // This should be created through a Factory, and that factopry should execute the code in the method EnsureEntityPool
+
+        */
+
+        public Environment()
+        {
+            messagingControllersManager = new MessagingControllersManager();
+        }
+
+        public void Initialize(IMessageProcessHandler messageHandler)
+        {
+            messagingControllersManager.Initialize(messageHandler);
+        }
+
+        public void Restart(IMessageProcessHandler messageHandler)
+        {
+            messagingControllersManager.Cleanup();
+
+            this.Initialize(messageHandler);
+        }
+    }
+}
