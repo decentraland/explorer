2e162c2cc093242fb0e3e3121fe4127f08e4494f
Santi Andrade <sandrade@decentraland.org>
2020-08-24 19:26:07 +0200

    aDD TutorialStep_OpenPlayerMenuOptions

---
+++ unity-client/Assets/Tutorial/Scripts/TutorialStep_OpenPlayerMenuOptions.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using UnityEngine;
+
+namespace DCL.Tutorial
+{
+    /// <summary>
+    /// Class that represents the onboarding tutorial step related to how to open the player menu options.
+    /// </summary>
+    public class TutorialStep_OpenPlayerMenuOptions : TutorialStep_WithProgressBar
+    {
+        [SerializeField] InputAction_Hold confirmInputAction;
+
+        public override IEnumerator OnStepExecute()
+        {
+            yield return new WaitUntil(() => confirmInputAction.isOn);
+        }
+    }
+}
