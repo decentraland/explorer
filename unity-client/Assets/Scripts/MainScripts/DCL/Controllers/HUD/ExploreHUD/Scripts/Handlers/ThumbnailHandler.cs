d84f27dd8840da8248e6f10e36996f07e28a45fb
Pato <pato@decentraland.org>
2020-09-10 11:39:47 -0300

    added handlers

---
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/ExploreHUD/Scripts/Handlers/ThumbnailHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Networking;
+using DCL.Helpers;
+
+internal class ThumbnailHandler : IDisposable
+{
+    public Texture2D texture { private set; get; }
+
+    UnityWebRequest thumbnailRequest = null;
+
+    public void FetchThumbnail(string url, Action<Texture2D> onSuccess, Action onFail)
+    {
+        if (texture != null)
+        {
+            onSuccess?.Invoke(texture);
+        }
+        else if (string.IsNullOrEmpty(url))
+        {
+            onFail?.Invoke();
+        }
+        else if (thumbnailRequest == null)
+        {
+            thumbnailRequest = UnityWebRequestTexture.GetTexture(url);
+            UnityWebRequestAsyncOperation op = thumbnailRequest.SendWebRequest();
+            op.completed += (_) =>
+            {
+                if (thumbnailRequest == null)
+                    return;
+
+                bool success = thumbnailRequest.WebRequestSucceded();
+                if (success)
+                {
+                    texture = ((DownloadHandlerTexture)thumbnailRequest.downloadHandler).texture;
+                    texture.Compress(true);
+                    onSuccess?.Invoke(texture);
+                }
+
+                thumbnailRequest.Dispose();
+                thumbnailRequest = null;
+
+                if (!success)
+                {
+                    Debug.Log($"Error downloading: {url}");
+                    onFail?.Invoke();
+                }
+            };
+        }
+    }
+
+    public void Dispose()
+    {
+        GameObject.Destroy(texture);
+        if (thumbnailRequest != null)
+        {
+            thumbnailRequest.Abort();
+            thumbnailRequest.Dispose();
+            thumbnailRequest = null;
+        }
+    }
+}
