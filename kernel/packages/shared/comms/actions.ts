b21249e0d5fb56910edd2a29d060651b5db1b97c
Pablo de Haro <pablitar@gmail.com>
2020-09-14 15:27:20 -0300

    Fixing linting issues

--- kernel/packages/shared/comms/actions.ts
+++ kernel/packages/shared/comms/actions.ts
@@ -5,12 +5,16 @@ export const voicePlayingUpdate = (userId: string, playing: boolean) =>
   action(VOICE_PLAYING_UPDATE, { userId, playing })
 export type VoicePlayingUpdate = ReturnType<typeof voicePlayingUpdate>
 
-/**Action to trigger voice chat recording */
+/**
+ * Action to trigger voice chat recording
+ */
 export const SET_VOICE_CHAT_RECORDING = 'Set Voice Chat Recording'
 export const setVoiceChatRecording = (recording: boolean) => action(SET_VOICE_CHAT_RECORDING, { recording })
 export type SetVoiceChatRecording = ReturnType<typeof setVoiceChatRecording>
 
-/**Action triggered when recording starts or stops */
+/**
+ * Action triggered when recording starts or stops
+ */
 export const VOICE_RECORDING_UPDATE = 'Voice Recording Update'
 export const voiceRecordingUpdate = (recording: boolean) => action(VOICE_RECORDING_UPDATE, { recording })
 export type VoiceRecordingUpdate = ReturnType<typeof voiceRecordingUpdate>
