8bebb1622d0df8b850676553c5438527da978132
Pato <pato@decentraland.org>
2020-09-23 21:05:08 -0300

    fixes and messaging formatting

--- kernel/packages/shared/social/communityEvents.ts
+++ kernel/packages/shared/social/communityEvents.ts
@@ -2,6 +2,9 @@ import { defaultLogger } from 'shared/logger'
 import { notifyStatusThroughChat } from '../comms/chat'
 
 const DEFAULT_FETCH_INTERVAL = 60000
+const INTERVAL_FETCH_ADDED_TIME = 5000
+const INTERVAL_TIME = 30000
+const EVENTS_API_URL = `https://events.decentraland.org/api/events`
 
 let fetchIntervalTime: number = DEFAULT_FETCH_INTERVAL
 let lastFetchTime: number = 0
@@ -17,13 +20,90 @@ export function initCommunityEvents() {
   initialMessageSent = false
   initialized = true
   fetchAndReportEvents(Date.now())
-  setInterval(eventsInterval, 30000)
+  setInterval(eventsInterval, INTERVAL_TIME)
 }
 
 function eventsInterval() {
   const now = Date.now()
-  if (now - lastFetchTime > fetchIntervalTime) {
+  if (now - lastFetchTime > fetchIntervalTime + INTERVAL_FETCH_ADDED_TIME) {
     fetchAndReportEvents(now)
+  }
+}
+
+function fetchAndReportEvents(now: number) {
+  fetchEvents()
+    .then((events) => {
+      if (events !== null) {
+        lastFetchTime = now
+
+        if (!initialMessageSent) {
+          sendInitialMessage(events)
+        }
+
+        reportUnreportedLiveEvents(events)
+
+        fetchIntervalTime = events.nextFetchInterval
+      }
+    })
+    .catch()
+}
+
+async function fetchEvents(): Promise<Events | null> {
+  try {
+    const response = await fetch(EVENTS_API_URL)
+    const json = (await response.json()) as EventApiResponse
+    if (Array.isArray(json.data)) {
+      return processEvents(json.data)
+    }
+  } catch (e) {
+    defaultLogger.error(e)
+  }
+  return null
+}
+
+function processEvents(events: EventJsonData[]): Events {
+  let result: Events = {
+    liveId: [],
+    todayId: [],
+    all: {},
+    nextFetchInterval: DEFAULT_FETCH_INTERVAL
+  }
+
+  const date = new Date()
+  for (let event of events) {
+    const startDate = new Date(event.next_start_at)
+
+    if (event.live) {
+      result.liveId.push(event.id)
+      if (!event.all_day) {
+        const millisecondsLeft = startDate.getTime() + event.duration - startDate.getTime()
+        if (millisecondsLeft > 0 && millisecondsLeft < result.nextFetchInterval) {
+          result.nextFetchInterval = millisecondsLeft
+        }
+      }
+    } else if (isToday(date, startDate)) {
+      result.todayId.push(event.id)
+      const millisecondsToStart = startDate.getTime() - date.getTime()
+      if (millisecondsToStart > 0 && millisecondsToStart < result.nextFetchInterval) {
+        result.nextFetchInterval = millisecondsToStart
+      }
+    }
+    result.all[event.id] = event
+  }
+  return result
+}
+
+function reportUnreportedLiveEvents(events: Events) {
+  const unreportedLiveEvents = events.liveId.filter(
+    (eventId) => liveEventsReported.filter((id) => id === eventId).length === 0
+  )
+  let message: string = ''
+  for (let eventId of unreportedLiveEvents) {
+    liveEventsReported.push(eventId)
+    message += formatLiveEventString(events.all[eventId])
+  }
+  if (message !== '') {
+    notifyStatusThroughChat(message)
   }
 }
 
@@ -55,90 +135,24 @@ function formatLiveEventString(event: EventJsonData) {
 }
 
 function formatTodayEventString(event: EventJsonData) {
-  let date = new Date(event.next_start_at)
+  let today = new Date()
+  let start = new Date(event.next_start_at)
+  const millisecondsToStart = start.getTime() - today.getTime()
+
+  if (millisecondsToStart < 60 * 60 * 1000) {
+    let minutes = millisecondsToStart / (60 * 1000)
+    let seconds = millisecondsToStart / 1000
+
+    return `<u>Event today</u>: <i>${event.name}</i> <nobr>@ ${event.position[0]},${
+      event.position[1]
+    }</nobr> <nobr>in ${minutes > 2 ? Math.round(minutes) + ' mins' : Math.round(seconds) + ' secs'}</nobr>\n`
+  }
+
   return `<u>Event today</u>: <i>${event.name}</i> <nobr>@ ${event.position[0]},${
     event.position[1]
-  }</nobr> <nobr>${date.getHours()}:${date
+  }</nobr> <nobr>${start.getHours()}:${start
     .getMinutes()
     .toLocaleString(undefined, { minimumIntegerDigits: 2 })}hrs</nobr>\n`
-}
-
-function reportUnreportedLiveEvents(events: Events) {
-  const unreportedLiveEvents = events.liveId.filter(
-    (eventId) => liveEventsReported.filter((id) => id !== eventId).length === 0
-  )
-  let message: string = ''
-  for (let eventId of unreportedLiveEvents) {
-    liveEventsReported.push(eventId)
-    message += formatLiveEventString(events.all[eventId])
-  }
-  if (message !== '') {
-    notifyStatusThroughChat(message)
-  }
-}
-
-function fetchAndReportEvents(now: number) {
-  fetchEvents()
-    .then((events) => {
-      if (events !== null) {
-        lastFetchTime = now
-
-        if (!initialMessageSent) {
-          sendInitialMessage(events)
-        }
-
-        reportUnreportedLiveEvents(events)
-
-        fetchIntervalTime = events.interval
-      }
-    })
-    .catch()
-}
-
-async function fetchEvents(): Promise<Events | null> {
-  try {
-    const response = await fetch(`https://events.decentraland.org/api/events`)
-    const json = (await response.json()) as EventApiResponse
-    if (Array.isArray(json.data)) {
-      return processEvents(json.data)
-    }
-  } catch (e) {
-    defaultLogger.error(e)
-  }
-  return null
-}
-
-function processEvents(events: EventJsonData[]): Events {
-  let result: Events = {
-    liveId: [],
-    todayId: [],
-    all: {},
-    interval: DEFAULT_FETCH_INTERVAL
-  }
-
-  const date = new Date()
-
-  for (let event of events) {
-    const startDate = new Date(event.next_start_at)
-
-    if (event.live) {
-      result.liveId.push(event.id)
-      if (!event.all_day) {
-        const millisecondsLeft = startDate.getTime() + event.duration - startDate.getTime()
-        if (millisecondsLeft > 0 && millisecondsLeft < result.interval) {
-          result.interval = millisecondsLeft
-        }
-      }
-    } else if (isToday(date, startDate)) {
-      result.todayId.push(event.id)
-      const millisecondsToStart = startDate.getTime() - date.getTime()
-      if (millisecondsToStart > 0 && millisecondsToStart < result.interval) {
-        result.interval = millisecondsToStart
-      }
-    }
-    result.all[event.id] = event
-  }
-  return result
 }
 
 function isToday(now: Date, date: Date): boolean {
@@ -168,5 +182,5 @@ type Events = {
   liveId: string[]
   todayId: string[]
   all: Record<string, EventJsonData>
-  interval: number
+  nextFetchInterval: number
 }
