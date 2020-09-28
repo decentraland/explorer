bbc218a9a9941bccfdedeb98c50982a71cd53f79
Nicolas Chamo <nicolas@chamo.com.ar>
2020-09-28 13:23:58 -0300

    Return null

--- unity-client/Assets/Scripts/MainScripts/DCL/Components/Avatar/AvatarModifiers/TriggerArea.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Components/Avatar/AvatarModifiers/TriggerArea.cs
@@ -16,13 +16,15 @@ public class BoxTriggerArea : TriggerArea
     public override HashSet<GameObject> DetectAvatars(Vector3 center, Quaternion rotation)
     {
         Collider[] colliders = Physics.OverlapBox(center, box * 0.5f, rotation, LayerMask.GetMask("AvatarTriggerDetection"), QueryTriggerInteraction.Collide);
-        HashSet<GameObject> result = new HashSet<GameObject>();
-        if (colliders.Length > 0)
+        if (colliders.Length == 0)
         {
-            foreach (Collider collider in colliders)
-            {
-                result.Add(collider.transform.parent.gameObject);
-            }
+            return null;
+        }
+
+        HashSet<GameObject> result = new HashSet<GameObject>();
+        foreach (Collider collider in colliders)
+        {
+            result.Add(collider.transform.parent.gameObject);
         }
         return result;
     }
