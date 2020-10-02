a6ed68c5dd06fcd398d40ec20b7afb29d25f575b
Brian Amadori <brian@decentraland.org>
2020-10-02 18:16:32 -0300

    RenderProfiles draft

---
+++ unity-client/Assets/Rendering/Profiles/RenderProfileAvatar.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DCL.Helpers;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "DCL/Rendering/Create RenderProfileAvatar", fileName = "RenderProfileAvatar", order = 0)]
+public class RenderProfileAvatar : ScriptableObject
+{
+    [System.Serializable]
+    private class MaterialProfile
+    {
+        public Vector3 lightDirection;
+
+        public void Apply()
+        {
+            Shader.SetGlobalVector(ShaderUtils._LightDir, lightDirection);
+        }
+    }
+
+    public enum ProfileID
+    {
+        AVATAR_EDITOR,
+        IN_WORLD,
+    }
+
+    [SerializeField] private MaterialProfile avatarEditorProfile;
+    [SerializeField] private MaterialProfile inWorldProfile;
+
+    public void Apply(ProfileID id)
+    {
+        switch (id)
+        {
+            case ProfileID.AVATAR_EDITOR:
+                avatarEditorProfile.Apply();
+                break;
+            case ProfileID.IN_WORLD:
+                inWorldProfile.Apply();
+                break;
+        }
+    }
+}
