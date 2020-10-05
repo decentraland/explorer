9f1cffdeb66cfddfdccd9c622954a296f48871d9
Brian Amadori <brian@decentraland.org>
2020-10-05 18:22:22 -0300

    render profile bridge draft

---
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/RenderProfileBridge/RenderProfileBridge.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DCL;
+using UnityEngine;
+
+public class RenderProfileBridge : MonoBehaviour
+{
+    public enum ID
+    {
+        DEFAULT,
+        HALLOWEEN,
+    }
+
+    public void SetRenderProfile(ID id)
+    {
+        switch (id)
+        {
+            case ID.DEFAULT:
+                RenderProfileManifest.currentProfile = RenderProfileManifest.i.defaultProfile;
+                break;
+            case ID.HALLOWEEN:
+                RenderProfileManifest.currentProfile = RenderProfileManifest.i.halloweenProfile;
+                break;
+        }
+
+        RenderProfileManifest.currentProfile.Apply();
+    }
+}
