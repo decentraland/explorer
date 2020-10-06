93dd3727b4ffbb5bb055602f19b85b306b41eb68
Brian Amadori <brian@decentraland.org>
2020-10-06 20:07:21 -0300

    Set proper color values. Add editor-time bool to copy RenderSettings to RenderProfileWorld instances.

--- unity-client/Assets/Rendering/Profiles/RenderProfileWorld.cs
+++ unity-client/Assets/Rendering/Profiles/RenderProfileWorld.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using DCL;
 using DCL.Helpers;
@@ -14,6 +15,31 @@ public class RenderProfileWorld : ScriptableObject
     [SerializeField] private Color fogColor;
 
     public RenderProfileAvatar avatarProfile;
+
+#if UNITY_EDITOR
+    public bool fillWithRenderSettings;
+
+    //NOTE(Brian): Workaround to make an editor-time action.
+    //             ContextMenu doesn't seem to work with ScriptableObjects.
+    private void OnValidate()
+    {
+        if (fillWithRenderSettings)
+        {
+            FillWithRenderSettings();
+        }
+
+        fillWithRenderSettings = false;
+    }
+
+    public void FillWithRenderSettings()
+    {
+        skyboxMaterial = RenderSettings.skybox;
+        equatorColor = RenderSettings.ambientEquatorColor;
+        skyColor = RenderSettings.ambientSkyColor;
+        groundColor = RenderSettings.ambientGroundColor;
+        fogColor = RenderSettings.fogColor;
+    }
+#endif
 
     public void Apply()
     {
