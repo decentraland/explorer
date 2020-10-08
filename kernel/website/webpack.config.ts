5f88a3f60949a19d8badf778e4d028357c198691
Ezequiel Camino <ezequiel@decentraland.org>
2020-10-08 12:29:26 -0300

    add react build to makefile

--- kernel/website/webpack.config.ts
+++ kernel/website/webpack.config.ts
@@ -35,7 +35,7 @@ const webpackConfig = (env): Configuration => ({
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              publicPath: '/public/path/to/'
+              publicPath: '/'
             }
           },
           {
