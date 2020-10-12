f394bdb0659df36d1d91ad1682700a5565811e00
Ezequiel Camino <ezequiel@decentraland.org>
2020-10-12 12:40:49 -0300

    updates init from decentraland-kernel

--- website/src/store/index.ts
+++ website/src/store/index.ts
@@ -1,3 +1,19 @@
+import { initShared, initWeb, loadUnity } from "decentraland-kernel";
+
 export const getKernelStore = () => {
+  const start = Date.now();
+  // create kernel store
+  initShared();
+  const container = document.getElementById("gameContainer") as HTMLElement;
+  //async initialize
+  initWeb(container)
+    .then((initResult: any) => {
+      console.log("website-initWeb completed at: ", Date.now() - start);
+      return loadUnity(initResult).then(() => {
+        console.log("website-loadUnity completed at: ", Date.now() - start);
+      });
+    })
+    .then(() => console.log("website-initUnity completed"));
+
   return (window as any).globalThis.globalStore;
 };
