8d009ac3257a212610d224ca7bd169c11367d378
Ezequiel Camino <ezequiel@decentraland.org>
2020-10-13 23:50:45 -0300

    adds post install to copy decentraland-kernel files

---
+++ website/scripts/post-install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Post Install Script:"
+echo "Copy files & dir from decentraland-kernel to public"
+cp -r ./node_modules/decentraland-kernel/default-profile ./public;
+cp -r ./node_modules/decentraland-kernel/dist/website.js ./public;
+cp -r ./node_modules/decentraland-kernel/loader ./public;
+cp -r ./node_modules/decentraland-kernel/systems ./public;
+cp -r ./node_modules/decentraland-kernel/unity ./public;
+cp -r ./node_modules/decentraland-kernel/voice-chat-codec ./public;
+echo "\nPost install script done! 😘😘😘\n"
