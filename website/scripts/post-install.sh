ad5c88ea0d4ab2bc94d9ce864531d4e0a876a79b
Ezequiel Camino <ezequiel@decentraland.org>
2020-10-20 14:22:45 -0300

    updates website post-install scritps and circleci

--- website/scripts/post-install.sh
+++ website/scripts/post-install.sh
@@ -10,8 +10,21 @@ cp -r ./node_modules/decentraland-kernel/unity ./public;
 cp -r ./node_modules/decentraland-kernel/voice-chat-codec ./public;
 
 echo "Setting kernel version"
-hash=$(git rev-parse HEAD);
-echo "REACT_APP_EXPLORER_VERSION=${hash}" >> .env
+hash=$(git rev-parse --short HEAD)
+envFile=.env
+if [ ! -f "$envFile" ]; then
+  echo "file $envFile does not exist. creating..."
+  echo "REACT_APP_EXPLORER_VERSION=${hash}" >.env
+else
+  echo "file $envFile exists. replacing..."
+  count=$(cat .env | grep -c "REACT_APP_EXPLORER_VERSION")
+  if [ "$count" -eq 0 ]; then
+    echo "REACT_APP_EXPLORER_VERSION=${hash}" >>.env
+  else
+    sed -i '' -e "s/REACT_APP_EXPLORER_VERSION.*/REACT_APP_EXPLORER_VERSION=${hash}/" .env
+  fi
+fi
+
 echo ""
 echo "Post install script done! 😘😘😘"
 echo ""
