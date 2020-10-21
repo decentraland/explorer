24e19451484a995f1a40470adacde85ed819a823
Pato <pato@decentraland.org>
2020-10-21 17:43:37 -0300

    add mute switch prefab

---
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/UsersAroundListHUD/SwitchToggle/Scripts/SwitchToggle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchToggle : Toggle
+{
+    private static readonly int animatorOnBoolean = Animator.StringToHash("isOn");
+
+    [SerializeField] Animator switchAnimator;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        OnValueChanged(this.isOn);
+        this.onValueChanged.AddListener(OnValueChanged);
+    }
+
+    private void OnValueChanged(bool value)
+    {
+        switchAnimator.SetBool(animatorOnBoolean, value);
+    }
+}
