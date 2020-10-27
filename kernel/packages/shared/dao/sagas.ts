930f6326d31929e405ec1266b64f89e203b85de0
Ezequiel Camino <ezequiel@decentraland.org>
2020-10-26 23:26:13 -0300

    Adds dafault catalyst

--- kernel/packages/shared/dao/sagas.ts
+++ kernel/packages/shared/dao/sagas.ts
@@ -41,10 +41,49 @@ const CATALYST_CANDIDATES_KEY = CACHE_KEY + '-' + SET_CATALYST_CANDIDATES
 const CACHE_TLD_KEY = 'tld'
 
 export function* daoSaga(): any {
+  yield call(loadDefaultCatalystRealms)
   yield takeEvery(WEB3_INITIALIZED, loadCatalystRealms)
 
   yield takeEvery([INIT_CATALYST_REALM, SET_CATALYST_REALM], cacheCatalystRealm)
   yield takeEvery([SET_CATALYST_CANDIDATES, SET_ADDED_CATALYST_CANDIDATES], cacheCatalystCandidates)
+}
+
+function* loadDefaultCatalystRealms() {
+  yield call(waitForMetaConfigurationInitialization)
+  if (WORLD_EXPLORER) {
+    let cachedRealm: Realm | null = getFromLocalStorage(CACHE_KEY)
+    if (cachedRealm && !(yield checkValidRealm(cachedRealm))) {
+      cachedRealm = null
+    }
+    const tld = getDefaultTLD().toLowerCase() !== 'org' ? 'zone' : 'org'
+    const realm: Realm = cachedRealm || {
+      domain: `https://peer.decentraland.${tld}`,
+      catalystName: 'fenrir',
+      layer: 'blue',
+      lighthouseVersion: '0.2'
+    }
+    // set default realm domain as whiteList
+    yield put(
+      setContentWhitelist([
+        {
+          domain: realm.domain,
+          catalystName: realm.catalystName,
+          elapsed: 0,
+          status: 0,
+          layer: {
+            name: realm.layer,
+            usersCount: 0,
+            maxUsers: 100,
+            usersParcels: []
+          },
+          score: -50,
+          lighthouseVersion: realm.lighthouseVersion
+        }
+      ])
+    )
+    yield put(initCatalystRealm(realm))
+    yield put(catalystRealmInitialized())
+  }
 }
 
 /**
@@ -172,7 +211,7 @@ async function checkValidRealm(realm: Realm) {
 }
 
 function* cacheCatalystRealm(action: InitCatalystRealm | SetCatalystRealm) {
-  saveToLocalStorage(CACHE_KEY, action.payload)
+  return saveToLocalStorage(CACHE_KEY, action.payload)
 }
 
 function* cacheCatalystCandidates(action: SetCatalystCandidates | SetAddedCatalystCandidates) {
