b52c92b520ede44642104ea3c3d1e2d07fcebf4d
Brian Amadori <brian@decentraland.org>
2020-10-29 23:44:14 -0300

    fix: improved efficiency of SRPBatchingHelper sorting
    - skinned meshes are now taken into account for the sorting, as they are now supported for SRP batching.
    - inverted sort criteria for cull type parameter to improve batching in genesis plaza
    - removed unneeded parameter in OptimizeMaterial
    - increased cullMode sorting bucket granularity from 100 to 150 to make sure more draw calls get batched together in complex scenes

--- unity-client/Assets/Scripts/MainScripts/DCL/Helpers/MaterialHelpers/MaterialCachingHelper/MaterialCachingHelper.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Helpers/MaterialHelpers/MaterialCachingHelper/MaterialCachingHelper.cs
@@ -62,9 +62,11 @@ namespace DCL.Helpers
 
                 matList.Clear();
 
-                for (int i1 = 0; i1 < r.sharedMaterials.Length; i1++)
+                var sharedMats = r.sharedMaterials;
+
+                for (int i1 = 0; i1 < sharedMats.Length; i1++)
                 {
-                    Material mat = r.sharedMaterials[i1];
+                    Material mat = sharedMats[i1];
 
                     float elapsedTime = Time.realtimeSinceStartup;
 
@@ -108,14 +110,14 @@ namespace DCL.Helpers
                             else
                                 materialCopy.SetFloat("_Surface", 0);
 #endif
-                            SRPBatchingHelper.OptimizeMaterial(r, materialCopy);
+                            SRPBatchingHelper.OptimizeMaterial(materialCopy);
 
+                            materialCopy.name += $" (crc: {materialCopy.ComputeCRC()})";
                             PersistentAssetCache.MaterialCacheByCRC.Add(hash, new RefCountedMaterialData(hash, materialCopy));
                         }
 
                         refCountedMat = PersistentAssetCache.MaterialCacheByCRC[hash];
                         refCountedMat.IncreaseRefCount();
-
                         matList.Add(refCountedMat.material);
                     }
                     else
