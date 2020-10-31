1fd874c3e442d6eeec28bbd79f4be648b793533c
Pato <pato@decentraland.org>
2020-10-30 21:44:02 -0300

    feat: add kernel configuration info

---
+++ unity-client/Assets/Scripts/MainScripts/DCL/KernelConfiguration/KernelConfig.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using DCL.Helpers;
+
+[CreateAssetMenu(fileName = "KernelConfiguration", menuName = "KernelConfiguration")]
+public class KernelConfig : ScriptableObject
+{
+    public delegate void OnKernelConfigChanged(KernelConfigModel current, KernelConfigModel previous);
+
+    public event OnKernelConfigChanged OnChange;
+
+    [SerializeField] KernelConfigModel value;
+
+    List<Promise<KernelConfigModel>> initializationPromises = new List<Promise<KernelConfigModel>>();
+    internal bool initialized = false;
+
+    public void Set(KernelConfigModel newValue)
+    {
+        if (newValue == null)
+        {
+            return;
+        }
+
+        if (!initialized)
+        {
+            Initialize(newValue);
+        }
+
+        if (newValue.Equals(value))
+            return;
+
+        var previous = value;
+        value = newValue;
+        OnChange?.Invoke(value, previous);
+    }
+
+    public KernelConfigModel Get()
+    {
+        return value;
+    }
+
+    public Promise<KernelConfigModel> EnsureConfigInitialized()
+    {
+        var newPromise = new Promise<KernelConfigModel>();
+        if (initialized)
+        {
+            newPromise.Resolve(value);
+        }
+        else
+        {
+            initializationPromises.Add(newPromise);
+        }
+        return newPromise;
+    }
+
+    private void Initialize(KernelConfigModel newValue)
+    {
+        if (initializationPromises?.Count > 0)
+        {
+            for (int i = 0; i < initializationPromises.Count; i++)
+            {
+                initializationPromises[i].Resolve(newValue);
+            }
+            initializationPromises.Clear();
+            initializationPromises = null;
+        }
+        initialized = true;
+    }
+}
---
+++ unity-client/Assets/Scripts/MainScripts/DCL/KernelConfiguration/KernelConfigController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+public static class KernelConfigController
+{
+    static KernelConfig config = null;
+
+    static public KernelConfig GetConfig()
+    {
+        if (!config)
+        {
+            config = Resources.Load<KernelConfig>("KernelConfiguration");
+        }
+        return config;
+    }
+
+
+    static internal void SetConfig(KernelConfigModel newConfig)
+    {
+        GetConfig().Set(newConfig);
+    }
+
+    static internal void SetConfig(string configJson)
+    {
+        try
+        {
+            var prevConfig = GetConfig().Get().Clone();
+            JsonUtility.FromJsonOverwrite(configJson, prevConfig);
+            SetConfig(prevConfig);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Error setting KernelConfig {e.Message}");
+        }
+    }
+}
