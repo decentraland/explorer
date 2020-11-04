01ab7e1f8c727f73b27a415812d534894df0f603
Pravus <pravus@decentraland.org>
2020-11-04 17:45:10 -0300

    Added test, although it is always pending and doesn't run locally...

---
+++ kernel/test/unit/engine.test.tsx
@@ -0,0 +1,28 @@
+import { expect } from 'chai'
+import { Entity, BoxShape, Transform, Vector3, engine } from 'decentraland-ecs/src';
+
+describe.skip('Engine tests', () => {
+  it('getComponentGroupReturnsCachedGroup', () => {
+
+    let box = new Entity()
+    box.addComponent(new BoxShape())
+    box.addComponent(
+      new Transform({
+        position: new Vector3(8, 0, 8),
+      })
+    )
+    const firstComponentGroup = engine.getComponentGroup(BoxShape, Transform)
+    const secondComponentGroup = engine.getComponentGroup(BoxShape, Transform)
+    const thirdComponentGroup = engine.getComponentGroup(BoxShape, Transform)
+
+    expect(firstComponentGroup).to.same(
+      secondComponentGroup,
+      'returnCachedComponentGroup01'
+    )
+
+    expect(secondComponentGroup).to.same(
+      thirdComponentGroup,
+      'returnCachedComponentGroup02'
+    )
+  })
+})
