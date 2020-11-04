480bb546ef4e217e122b050f14d7def74cb5635e
Pablo de Haro <pablitar@gmail.com>
2020-11-04 12:29:39 -0300

    VOICE_CHAT_DISABLED flag should override allowlist

--- kernel/packages/shared/meta/selectors.ts
+++ kernel/packages/shared/meta/selectors.ts
@@ -36,7 +36,7 @@ export const getMessageOfTheDay = (store: RootMetaState): MessageOfTheDayConfig 
 export const isVoiceChatEnabled = (store: RootMetaState): boolean =>
   (!!getCommsConfig(store).voiceChatEnabled && !VOICE_CHAT_DISABLED_FLAG) || VOICE_CHAT_ENABLED_FLAG
 
-export const getVoiceChatWhitelist = (store: RootMetaState): string[] => getCommsConfig(store).voiceChatAllowlist ?? []
+export const getVoiceChatAllowlist = (store: RootMetaState): string[] => getCommsConfig(store).voiceChatAllowlist ?? []
 
 export const isVoiceChatEnabledFor = (store: RootMetaState, userId: string): boolean =>
-  isVoiceChatEnabled(store) || getVoiceChatWhitelist(store).includes(userId)
+  isVoiceChatEnabled(store) || (getVoiceChatAllowlist(store).includes(userId) && !VOICE_CHAT_DISABLED_FLAG)
