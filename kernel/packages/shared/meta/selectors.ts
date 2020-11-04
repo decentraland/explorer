7ceab4bd3d39c41c0b4fb4b31c3c31b40401154d
Pablo de Haro <pablitar@gmail.com>
2020-11-04 12:17:27 -0300

    Voice chat now disabled by default. Added allowlist for voice chat

--- kernel/packages/shared/meta/selectors.ts
+++ kernel/packages/shared/meta/selectors.ts
@@ -1,6 +1,6 @@
 import { CommsConfig, MessageOfTheDayConfig, RootMetaState } from './types'
 import { Vector2Component } from 'atomicHelpers/landHelpers'
-import { VOICE_CHAT_DISABLED_FLAG } from 'config'
+import { VOICE_CHAT_DISABLED_FLAG, VOICE_CHAT_ENABLED_FLAG } from 'config'
 
 export const getAddedServers = (store: RootMetaState): string[] => {
   const { config } = store.meta
@@ -34,4 +34,10 @@ export const getMessageOfTheDay = (store: RootMetaState): MessageOfTheDayConfig 
   store.meta.config.world ? store.meta.config.world.messageOfTheDay || null : null
 
 export const isVoiceChatEnabled = (store: RootMetaState): boolean =>
-  !getCommsConfig(store).voiceChatDisabled && !VOICE_CHAT_DISABLED_FLAG
+  (!!getCommsConfig(store).voiceChatEnabled && !VOICE_CHAT_DISABLED_FLAG) || VOICE_CHAT_ENABLED_FLAG
+
+export const getVoiceChatWhitelist = (store: RootMetaState): string[] =>
+  getCommsConfig(store).voiceChatAllowlist ?? ['0x8f75ac49512b65990537122edab456b81597ebfa']
+
+export const isVoiceChatEnabledFor = (store: RootMetaState, userId: string): boolean =>
+  isVoiceChatEnabled(store) || getVoiceChatWhitelist(store).includes(userId)
