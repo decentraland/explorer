e0a926aa3a6f22d821cb0aa0b04461f4f2364ba2
Pato <pato@decentraland.org>
2020-11-05 17:09:48 -0300

    hide voicechat settings when disabled

---
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsHUD/TempScripts/DisableGameObjectsOnVoiceChatDisabled.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+internal class DisableGameObjectsOnVoiceChatDisabled : MonoBehaviour
+{
+    [SerializeField] GameObject[] gameObjects;
+
+    void Awake()
+    {
+        KernelConfig.i.EnsureConfigInitialized().Then(config => DoChanges(config));
+
+        KernelConfig.i.OnChange += OnKernelConfigChanged;
+    }
+
+    void OnDestroy()
+    {
+        KernelConfig.i.OnChange -= OnKernelConfigChanged;
+    }
+
+    void OnKernelConfigChanged(KernelConfigModel current, KernelConfigModel previous)
+    {
+        if (current.comms.voiceChatEnabled == previous.comms.voiceChatEnabled)
+        {
+            return;
+        }
+        DoChanges(current);
+    }
+
+    void DoChanges(KernelConfigModel config)
+    {
+        for (int i = 0; i < gameObjects.Length; i++)
+        {
+            gameObjects[i].SetActive(config.comms.voiceChatEnabled);
+        }
+    }
+}
---
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsHUD/TempScripts/MoveVerticalOnVoiceChatDisabled.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+internal class MoveVerticalOnVoiceChatDisabled : MonoBehaviour
+{
+    [SerializeField] float targetPositionY;
+    [SerializeField] RectTransform target;
+
+    float defaultPositionY;
+
+    void Awake()
+    {
+        defaultPositionY = target.anchoredPosition.y;
+
+        KernelConfig.i.EnsureConfigInitialized().Then(config => DoChanges(config));
+
+        KernelConfig.i.OnChange += OnKernelConfigChanged;
+    }
+
+    void OnDestroy()
+    {
+        KernelConfig.i.OnChange -= OnKernelConfigChanged;
+    }
+
+    void OnKernelConfigChanged(KernelConfigModel current, KernelConfigModel previous)
+    {
+        if (current.comms.voiceChatEnabled == previous.comms.voiceChatEnabled)
+        {
+            return;
+        }
+        DoChanges(current);
+    }
+
+    void DoChanges(KernelConfigModel config)
+    {
+        if (!target)
+            return;
+
+        target.anchoredPosition = new Vector3(target.anchoredPosition.x, config.comms.voiceChatEnabled ? defaultPositionY : targetPositionY);
+    }
+}
