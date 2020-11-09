5824f9c986f5283ba64c0551b5b7a37ad50c8618
Pato <pato@decentraland.org>
2020-11-09 12:27:08 -0300

    markers handler update

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/MapRenderer/GlobalUsersPositionMarker/UserPositionMarkersHandler.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/MapRenderer/GlobalUsersPositionMarker/UserPositionMarkersHandler.cs
@@ -5,9 +5,12 @@ using Random = UnityEngine.Random;
 
 internal class UserPositionMarkersHandler : IDisposable
 {
-    readonly Queue<UserPositionMarker> availableMarkers;
-    readonly Queue<UserPositionMarker> onUseMarkers;
+    readonly List<UserPositionMarker> availableMarkers;
+    readonly List<UserPositionMarker> usedMarkers;
     readonly Func<float, float, Vector3> coordToMapPosition;
+
+    readonly ExclusionArea exclusionArea;
+    readonly ScenesFilter scenesFilter;
 
     int maxMarkers;
 
@@ -16,51 +19,55 @@ internal class UserPositionMarkersHandler : IDisposable
         this.maxMarkers = maxMarkers;
         this.coordToMapPosition = coordToMapPosFunc;
 
-        availableMarkers = new Queue<UserPositionMarker>(maxMarkers);
-        onUseMarkers = new Queue<UserPositionMarker>(maxMarkers);
+        exclusionArea = new ExclusionArea();
+        scenesFilter = new ScenesFilter();
+
+        availableMarkers = new List<UserPositionMarker>(maxMarkers);
+        usedMarkers = new List<UserPositionMarker>(maxMarkers);
 
         for (int i = 0; i < maxMarkers; i++)
         {
             var marker = new UserPositionMarker(GameObject.Instantiate(markerPrefab, overlayContainer));
-            SetMarkerAsAvailable(marker);
+            availableMarkers.Add(marker);
+            marker.gameObject.SetActive(false);
         }
     }
 
     public void Dispose()
     {
-        while (availableMarkers.Count > 0)
+        for (int i = 0; i < availableMarkers.Count; i++)
         {
-            var marker = availableMarkers.Dequeue();
-            marker.Dispose();
+            availableMarkers[i].Dispose();
         }
-        while (onUseMarkers.Count > 0)
+        for (int i = 0; i < usedMarkers.Count; i++)
         {
-            var marker = onUseMarkers.Dequeue();
-            marker.Dispose();
+            usedMarkers[i].Dispose();
         }
+        availableMarkers.Clear();
+        usedMarkers.Clear();
     }
 
     public void SetExclusionArea(Vector2Int center, int area)
     {
-
+        exclusionArea.position = center;
+        exclusionArea.area = area;
+        ApplyExclusionArea();
     }
 
     public void SetMarkers(List<HotScenesController.HotSceneInfo> hotScenes)
     {
-
+        var parcelList = scenesFilter.Filter(hotScenes, maxMarkers);
+        ResfreshMarkersPoolLists(parcelList.Count);
+        for (int i = 0; i < parcelList.Count && i < usedMarkers.Count; i++)
+        {
+            SetMarker(usedMarkers[i], parcelList[i]);
+        }
     }
 
-    private void SetMarkerAsAvailable(UserPositionMarker marker)
+    private void SetMarker(UserPositionMarker marker, Vector2Int coords)
     {
-        availableMarkers.Enqueue(marker);
-        marker.gameObject.SetActive(false);
-    }
-
-    private void SetMarkerAsUsed(UserPositionMarker marker, Vector2Int coords)
-    {
-        onUseMarkers.Enqueue(marker);
         SetMarkerAtCoord(marker, coords);
-        marker.gameObject.SetActive(true);
+        marker.gameObject.SetActive(!exclusionArea.Contains(coords));
     }
 
     private void SetMarkerAtCoord(UserPositionMarker marker, Vector2Int coords)
@@ -72,5 +79,44 @@ internal class UserPositionMarkersHandler : IDisposable
             coords.y + Random.Range(-0.5f, 0.5f));
 
         marker.coords = coords;
+    }
+
+    private void ApplyExclusionArea()
+    {
+        if (usedMarkers.Count == 0)
+        {
+            return;
+        }
+
+        using (var iterator = usedMarkers.GetEnumerator())
+        {
+            while (iterator.MoveNext())
+            {
+                iterator.Current.gameObject.SetActive(!exclusionArea.Contains(iterator.Current.coords));
+            }
+        }
+    }
+
+    private void ResfreshMarkersPoolLists(int amountToBeUsed)
+    {
+        if (amountToBeUsed > usedMarkers.Count)
+        {
+            for (int i = 0; i < amountToBeUsed - usedMarkers.Count && i < availableMarkers.Count; i++)
+            {
+                var marker = availableMarkers[i];
+                availableMarkers.RemoveAt(i);
+                usedMarkers.Add(marker);
+            }
+        }
+        else if (amountToBeUsed < usedMarkers.Count)
+        {
+            for (int i = 0; i < usedMarkers.Count - amountToBeUsed && i < usedMarkers.Count; i++)
+            {
+                var marker = usedMarkers[i];
+                usedMarkers.RemoveAt(i);
+                marker.gameObject.SetActive(false);
+                availableMarkers.Add(marker);
+            }
+        }
     }
 }
