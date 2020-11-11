bd19a676e865afc3ebbce6be73f7009d5634327e
Alejandro Jimenez <ajimenez@decentraland.org>
2020-11-11 12:18:14 +0100

    WIP Graphic Card Warning

---
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/GraphicCardWarningHUD/GraphicCardNotification.cs
@@ -0,0 +1,20 @@
+﻿using DCL.Interface;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GraphicCardNotification : Notification
+{
+    [SerializeField] private string moreInfoUrl;
+    [SerializeField] private Button moreInfoButton;
+
+    private void Awake()
+    {
+        moreInfoButton.gameObject.SetActive(string.IsNullOrEmpty(moreInfoUrl));
+        moreInfoButton.onClick.AddListener(OpenMoreInfoUrl);
+    }
+
+    private void OpenMoreInfoUrl()
+    {
+        WebInterface.OpenURL(moreInfoUrl);
+    }
+}
---
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/GraphicCardWarningHUD/GraphicCardWarningHUDController.cs
@@ -0,0 +1,41 @@
+﻿
+public class GraphicCardWarningHUDController : IHUD
+{
+    private const string GRAPHIC_CARD_MESSAGE = "An integrated Graphic Card has been detected.\nYou might encounter performance issues";
+
+    [System.Serializable]
+    public class Model
+    {
+        public string graphicCardVendor;
+    }
+
+    private Model model;
+
+    public GraphicCardWarningHUDController() { }
+
+    public GraphicCardWarningHUDController(Model model)
+    {
+        this.model = model;
+    }
+
+    public void SetVisibility(bool visible)
+    {
+        if (visible)
+        {
+            NotificationsController.i.ShowNotification(new Notification.Model
+            {
+                buttonMessage = "Dismish",
+                destroyOnFinish = true,
+                groupID = "GraphicCard",
+                message = GRAPHIC_CARD_MESSAGE,
+                timer = 0,
+                type = NotificationFactory.Type.GRAPHIC_CARD_WARNING
+            });
+        }
+    }
+
+    public void Dispose()
+    {
+
+    }
+}
