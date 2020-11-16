546f30ac097845c45d7769fecaedbf38e6d74fce
Alejandro Jimenez <ajimenez@decentraland.org>
2020-11-16 12:13:49 +0100

    Now mouse sensitivity modify camera speed and not acceleration.

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/Settings/SettingsControllers/GeneralSettingsController.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/Settings/SettingsControllers/GeneralSettingsController.cs
@@ -5,14 +5,27 @@ namespace DCL.SettingsController
 {
     public class GeneralSettingsController : MonoBehaviour
     {
+        private const float SENSITIVITY_MAX_MULTIPLIER = 2f;
+        private const float SENSITIVITY_MIN_MULTIPLIER = 0.5f;
+
         public CinemachineFreeLook thirdPersonCamera;
         public CinemachineVirtualCamera firstPersonCamera;
 
         private CinemachinePOV povCamera;
 
+        private float defaultFirstPersonSpeedX;
+        private float defaultFirstPersonSpeedY;
+        private float defaultThirdPersonSpeedX;
+        private float defaultThirdPersonSpeedY;
+
         void Awake()
         {
             povCamera = firstPersonCamera.GetCinemachineComponent<CinemachinePOV>();
+
+            defaultFirstPersonSpeedX = povCamera.m_HorizontalAxis.m_MaxSpeed;
+            defaultFirstPersonSpeedY = povCamera.m_VerticalAxis.m_MaxSpeed;
+            defaultThirdPersonSpeedX = thirdPersonCamera.m_XAxis.m_MaxSpeed;
+            defaultThirdPersonSpeedY = thirdPersonCamera.m_YAxis.m_MaxSpeed;
         }
 
         void Start()
@@ -32,10 +45,12 @@ namespace DCL.SettingsController
 
         void ApplyGeneralSettings(DCL.SettingsData.GeneralSettings settings)
         {
-            thirdPersonCamera.m_XAxis.m_AccelTime = settings.mouseSensitivity;
-            thirdPersonCamera.m_YAxis.m_AccelTime = settings.mouseSensitivity;
-            povCamera.m_HorizontalAxis.m_AccelTime = settings.mouseSensitivity;
-            povCamera.m_VerticalAxis.m_AccelTime = settings.mouseSensitivity;
+            float sensitivityMultiplier = Mathf.LerpUnclamped(SENSITIVITY_MIN_MULTIPLIER, SENSITIVITY_MAX_MULTIPLIER, settings.mouseSensitivity);
+            povCamera.m_HorizontalAxis.m_MaxSpeed = defaultFirstPersonSpeedX*sensitivityMultiplier;
+            povCamera.m_VerticalAxis.m_MaxSpeed = defaultFirstPersonSpeedY*sensitivityMultiplier;
+            thirdPersonCamera.m_XAxis.m_MaxSpeed =  defaultThirdPersonSpeedX*sensitivityMultiplier;
+            thirdPersonCamera.m_YAxis.m_MaxSpeed = defaultThirdPersonSpeedY*sensitivityMultiplier;
+
             AudioListener.volume = settings.sfxVolume;
             DCL.Interface.WebInterface.ApplySettings(settings.voiceChatVolume, (int)settings.voiceChatAllow);
         }
