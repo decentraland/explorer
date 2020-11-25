8e3063f053a2def4c3dda68302991b08e7d07e53
Brian Amadori <brian@decentraland.org>
2020-11-25 01:36:35 -0300

    apply PR feedback

--- unity-client/Assets/Rendering/Culling/CullingController.cs
+++ unity-client/Assets/Rendering/Culling/CullingController.cs
@@ -9,6 +9,21 @@ using static DCL.Rendering.CullingControllerUtils;
 
 namespace DCL.Rendering
 {
+    public interface ICullingController
+    {
+        void Start();
+        void Stop();
+        event CullingController.DataReport OnDataReport;
+        void SetDirty();
+
+        void SetSettings(CullingControllerSettings settings);
+        CullingControllerSettings GetSettings();
+
+        void SetObjectCulling(bool enabled);
+        void SetAnimationCulling(bool enabled);
+        void SetShadowCulling(bool enabled);
+    }
+
     /// <summary>
     /// CullingController has the following responsibilities:
     /// - Hides small renderers (detail objects).
