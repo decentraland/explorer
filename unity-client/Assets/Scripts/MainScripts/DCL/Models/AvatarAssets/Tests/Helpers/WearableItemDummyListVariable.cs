e53e4f2a6c9aa859585f08b2cfa02d7a727ceee2
Alejandro Jimenez <ajimenez@decentraland.org>
2020-12-01 16:37:18 +0100

    SO for local wearables catalog now doesn't extend ListVariable to reduce the overhead from cleaning up the memory when used in runtime

--- unity-client/Assets/Scripts/MainScripts/DCL/Models/AvatarAssets/Tests/Helpers/WearableItemDummyListVariable.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Models/AvatarAssets/Tests/Helpers/WearableItemDummyListVariable.cs
@@ -7,8 +7,9 @@ using UnityEditor;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "WearableItemList", menuName = "Variables/Tests/WearableItemList")]
-public class WearableItemDummyListVariable : ListVariable<WearableItemDummy>
+public class WearableItemDummyListVariable : ScriptableObject
 {
+    public List<WearableItemDummy> list;
 
     [ContextMenu("Load test catalog")]
     private void FillWithTestCatalog()
