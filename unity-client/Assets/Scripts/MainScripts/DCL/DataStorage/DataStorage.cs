f0585fc1af4ff9ad3acace4dc7f627bb80d82ab3
Pato <pato@decentraland.org>
2020-12-02 16:45:55 -0300

    feat: data storage

---
+++ unity-client/Assets/Scripts/MainScripts/DCL/DataStorage/DataStorage.cs
@@ -0,0 +1,7 @@
+﻿using Variables.RealmsInfo;
+
+public static class DataStorage
+{
+    static public readonly CurrentRealmVariable playerRealm = new CurrentRealmVariable();
+    static public readonly RealmsVariable realmsInfo = new RealmsVariable();
+}
---
+++ unity-client/Assets/Scripts/MainScripts/DCL/DataStorage/Variables/RealmsInfo/CurrentRealmVariable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Variables.RealmsInfo
+{
+    public class CurrentRealmVariable : BaseVariable<CurrentRealmModel>
+    {
+        public override bool Equals(CurrentRealmModel other)
+        {
+            if (value == null)
+            {
+                return other == null;
+            }
+
+            return value.Equals(other);
+        }
+    }
+
+    [Serializable]
+    public class CurrentRealmModel
+    {
+        public string layer;
+        public string serverName;
+
+        public bool Equals(CurrentRealmModel other)
+        {
+            return Equals(other.serverName, other.layer);
+        }
+
+        public bool Equals(string serverName, string layer)
+        {
+            return this.serverName == serverName && this.layer == layer;
+        }
+    }
+}
