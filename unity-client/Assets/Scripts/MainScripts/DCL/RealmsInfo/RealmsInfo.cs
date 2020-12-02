ed11235e09ec9b69681bec0eb3929771bd1dd323
Pato <pato@decentraland.org>
2020-12-02 16:44:00 -0300

    data containers

---
+++ unity-client/Assets/Scripts/MainScripts/DCL/RealmsInfo/RealmsInfo.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Variables.RealmsInfo;
+
+public class RealmsInfo
+{
+    public static RealmsInfo i
+    {
+        get
+        {
+            if (info == null)
+            {
+                info = new RealmsInfo();
+            }
+            return info;
+        }
+    }
+
+    public CurrentRealmVariable currentRealm => DataStorage.playerRealm;
+    public RealmsVariable realms => DataStorage.realmsInfo;
+
+    static private RealmsInfo info = null;
+
+    private RealmsInfoModel model = new RealmsInfoModel();
+
+    public void Set(string json)
+    {
+        JsonUtility.FromJsonOverwrite(json, model);
+        Set(model);
+    }
+
+    public void Set(RealmsInfoModel newModel)
+    {
+        model = newModel;
+        currentRealm.Set(model.current);
+        realms.Set(model.realms);
+    }
+}
---
+++ unity-client/Assets/Scripts/MainScripts/DCL/RealmsInfo/RealmsInfoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Variables.RealmsInfo;
+
+[Serializable]
+public class RealmsInfoModel
+{
+    public CurrentRealmModel current;
+    public RealmModel[] realms;
+
+    public RealmsInfoModel Clone()
+    {
+        RealmsInfoModel clone = (RealmsInfoModel)this.MemberwiseClone();
+        return clone;
+    }
+}
