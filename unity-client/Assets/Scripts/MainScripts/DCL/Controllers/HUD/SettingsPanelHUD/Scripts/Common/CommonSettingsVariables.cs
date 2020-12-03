02fe0290acc91ab217e00c81c7c5ff7906a4ae7d
Santi Andrade <sandrade@decentraland.org>
2020-12-03 16:05:00 +0100

    Use the new data store for the CommonSettingsVariables

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/Common/CommonSettingsVariables.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/Common/CommonSettingsVariables.cs
@@ -4,20 +4,7 @@ namespace DCL.SettingsPanelHUD.Common
 {
     public static class CommonSettingsVariables
     {
-        private static BooleanVariable shouldSetQualityPresetAsCustomValue = new BooleanVariable();
-        public static BooleanVariable shouldSetQualityPresetAsCustom => GetOrCreate(ref shouldSetQualityPresetAsCustomValue);
-
-        private static BooleanVariable shadowStateValue = new BooleanVariable();
-        public static BooleanVariable shadowState => GetOrCreate(ref shadowStateValue);
-
-        internal static T GetOrCreate<T>(ref T variable) where T : Object
-        {
-            if (variable == null)
-            {
-                variable = new BaseVariable<T>();
-            }
-
-            return variable;
-        }
+        public static readonly BaseVariable<bool> shouldSetQualityPresetAsCustom = new BaseVariable<bool>();
+        public static readonly BaseVariable<bool> shadowState = new BaseVariable<bool>();
     }
 }
