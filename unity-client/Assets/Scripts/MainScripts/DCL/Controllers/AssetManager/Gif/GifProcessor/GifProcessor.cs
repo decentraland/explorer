c81dc44316f4a406ccb5641f6e4ee7e53f5055a6
Pato <pato@decentraland.org>
2020-12-11 18:00:56 -0300

    gif asset promise

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/Gif/GifProcessor/GifProcessor.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/Gif/GifProcessor/GifProcessor.cs
@@ -3,17 +3,29 @@ using System;
 using DCL.Helpers;
 using UnityEngine.Networking;
 
+/// <summary>
+/// GifProcessor: Is in charge of choosing which gif processor tu use (typescript's webworker through GIFProcessingBridge or Unity's plugin UniGif)
+/// for downloading, processing and discarding gifs
+/// </summary>
 public class GifProcessor
 {
     private bool jsGIFProcessingEnabled = false;
     private UnityWebRequest webRequest;
+    private string url;
 
-    public GifProcessor()
+    public GifProcessor(string url)
     {
+        this.url = url;
         KernelConfig.i.EnsureConfigInitialized().Then(config => jsGIFProcessingEnabled = config.gifSupported);
     }
 
-    public IEnumerator Load(string url, Action<GifFrameData[]> OnSuccess, Action OnFail)
+    /// <summary>
+    /// Request the download and processing of a gif
+    /// </summary>
+    /// <param name="OnSuccess">success callback with gif's frames arry</param>
+    /// <param name="OnFail">fail callback</param>
+    /// <returns></returns>
+    public IEnumerator Load(Action<GifFrameData[]> OnSuccess, Action OnFail)
     {
         if (jsGIFProcessingEnabled)
         {
@@ -25,11 +37,20 @@ public class GifProcessor
         }
     }
 
-    public void DisposeGif(string url)
+    /// <summary>
+    /// Notify processor that the gif is disposed.
+    /// If using UniGif plugin we just cancel the download if pending
+    /// If using webworker we send a message to kernel to cancel download and/or remove created texture from memory
+    /// </summary>
+    public void DisposeGif()
     {
         if (jsGIFProcessingEnabled)
         {
             DCL.GIFProcessingBridge.i.DeleteGIF(url);
+        }
+        else if (!(webRequest is null))
+        {
+            webRequest.Abort();
         }
     }
 
