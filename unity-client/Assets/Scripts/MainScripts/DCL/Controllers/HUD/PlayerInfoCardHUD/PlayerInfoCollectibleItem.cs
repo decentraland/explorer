8555602cfb056f1b3a53ee546d89566e024b08c9
Pravus <pravus@decentraland.org>
2021-01-04 12:14:46 -0300

    Added check to avoid trying to destroy the preloaded (not-instantiated) asset sprite as that triggers an error in unity

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/PlayerInfoCardHUD/PlayerInfoCollectibleItem.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/PlayerInfoCardHUD/PlayerInfoCollectibleItem.cs
@@ -8,9 +8,11 @@ public class PlayerInfoCollectibleItem : MonoBehaviour
 
     internal WearableItem collectible;
     private AssetPromise_Texture thumbnailPromise;
+    private Sprite placeholderSprite;
 
     public void Initialize(WearableItem collectible)
     {
+        placeholderSprite = thumbnail.sprite;
         this.collectible = collectible;
 
         if (this.collectible == null) return;
@@ -48,7 +50,8 @@ public class PlayerInfoCollectibleItem : MonoBehaviour
 
     private void OnThumbnailReady(Asset_Texture texture)
     {
-        if (thumbnail.sprite != null)
+        // we can't destroy the referenced asset placeholder sprite or we get the "Destroying assets it not permitted to avoid data loss" error
+        if (thumbnail.sprite != null && thumbnail.sprite != placeholderSprite)
             Destroy(thumbnail.sprite);
 
         thumbnail.sprite = ThumbnailsManager.CreateSpriteFromTexture(texture.texture);
