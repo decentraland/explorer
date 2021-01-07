c0c37e82d224f577230415153a79d319603ea44e
Santi Andrade <sandrade@decentraland.org>
2021-01-07 18:17:01 +0100

    Refactorize DetailObjectCulling and DetailObjectCullingSize

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/ControlsModule/SpecificControllers/DetailObjectCullingControlController.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/ControlsModule/SpecificControllers/DetailObjectCullingControlController.cs
@@ -6,16 +6,27 @@ namespace DCL.SettingsPanelHUD.Controls
     [CreateAssetMenu(menuName = "Settings/Controllers/Controls/Detail Object Culling", fileName = "DetailObjectCullingControlController")]
     public class DetailObjectCullingControlController : SettingsControlController
     {
+        const string DETAIL_OBJECT_CULLING_SETTINGS_KEY = "Settings.DetailObjectCulling";
+
         public override object GetStoredValue()
         {
-            return currentQualitySetting.enableDetailObjectCulling;
+            string storedValue = PlayerPrefs.GetString(DETAIL_OBJECT_CULLING_SETTINGS_KEY);
+            if (!string.IsNullOrEmpty(storedValue))
+                return System.Convert.ToBoolean(storedValue);
+            else
+                return Settings.i.qualitySettingsPresets.defaultPreset.enableDetailObjectCulling;
         }
 
         public override void OnControlChanged(object newValue)
         {
-            bool detailObjectCullingValue = (bool)newValue;
-            currentQualitySetting.enableDetailObjectCulling = detailObjectCullingValue;
-            CommonSettingsScriptableObjects.detailObjectCullingDisabled.Set(!detailObjectCullingValue);
+            bool newBoolValue = (bool)newValue;
+
+            Environment.i.platform.cullingController.SetObjectCulling(newBoolValue);
+            Environment.i.platform.cullingController.SetShadowCulling(newBoolValue);
+            Environment.i.platform.cullingController.MarkDirty();
+
+            CommonSettingsScriptableObjects.detailObjectCullingDisabled.Set(!newBoolValue);
+            PlayerPrefs.SetString(DETAIL_OBJECT_CULLING_SETTINGS_KEY, newBoolValue.ToString());
         }
     }
 }
--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/ControlsModule/SpecificControllers/DetailObjectCullingSizeControlController.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/ControlsModule/SpecificControllers/DetailObjectCullingSizeControlController.cs
@@ -1,18 +1,62 @@
-﻿using UnityEngine;
+using DCL.Rendering;
+using DCL.SettingsController;
+using System;
+using UnityEngine;
 
 namespace DCL.SettingsPanelHUD.Controls
 {
     [CreateAssetMenu(menuName = "Settings/Controllers/Controls/Detail Object Culling Size", fileName = "DetailObjectCullingSizeControlController")]
     public class DetailObjectCullingSizeControlController : SettingsControlController
     {
+        const string DETAIL_OBJECT_CULLING_SIZE_SETTINGS_KEY = "Settings.DetailObjectCullingSize";
+        const string DETAIL_OBJECT_CULLING_SETTINGS_KEY = "Settings.DetailObjectCulling";
+
+        private QualitySettingsController qualitySettings; // TODO (Santi): Refactorize!
+
+        public override void Initialize(ISettingsControlView settingsControlView)
+        {
+            base.Initialize(settingsControlView);
+
+            qualitySettings = GameObject.FindObjectOfType<QualitySettingsController>();
+        }
+
         public override object GetStoredValue()
         {
-            return currentQualitySetting.detailObjectCullingThreshold;
+            float storedValue = PlayerPrefs.GetFloat(DETAIL_OBJECT_CULLING_SIZE_SETTINGS_KEY, -1);
+            if (storedValue != -1)
+                return storedValue;
+            else
+                return Settings.i.qualitySettingsPresets.defaultPreset.detailObjectCullingThreshold;
         }
 
         public override void OnControlChanged(object newValue)
         {
-            currentQualitySetting.detailObjectCullingThreshold = (float)newValue;
+            float newFloatValue = (float)newValue;
+
+            string detailObjectCullingStoredValue = PlayerPrefs.GetString(DETAIL_OBJECT_CULLING_SETTINGS_KEY);
+            bool detailObjectCullingIsEnabled;
+            if (!string.IsNullOrEmpty(detailObjectCullingStoredValue))
+                detailObjectCullingIsEnabled = Convert.ToBoolean(detailObjectCullingStoredValue);
+            else
+                detailObjectCullingIsEnabled = Settings.i.qualitySettingsPresets.defaultPreset.enableDetailObjectCulling;
+
+            if (detailObjectCullingIsEnabled)
+            {
+                var settings = Environment.i.platform.cullingController.GetSettingsCopy();
+
+                settings.rendererProfile = CullingControllerProfile.Lerp(
+                    qualitySettings.cullingControllerSettingsData.rendererProfileMin,
+                    qualitySettings.cullingControllerSettingsData.rendererProfileMax,
+                    newFloatValue / 100.0f);
+
+                settings.skinnedRendererProfile = CullingControllerProfile.Lerp(
+                    qualitySettings.cullingControllerSettingsData.skinnedRendererProfileMin,
+                    qualitySettings.cullingControllerSettingsData.skinnedRendererProfileMax,
+                    newFloatValue / 100.0f);
+
+                Environment.i.platform.cullingController.SetSettings(settings);
+                PlayerPrefs.SetFloat(DETAIL_OBJECT_CULLING_SIZE_SETTINGS_KEY, newFloatValue);
+            }
         }
     }
 }
