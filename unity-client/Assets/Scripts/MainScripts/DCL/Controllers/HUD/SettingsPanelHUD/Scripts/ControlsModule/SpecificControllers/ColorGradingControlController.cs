0986eeee02d7a9879f8262e7e35007e4b286c472
Santi Andrade <sandrade@decentraland.org>
2021-01-07 15:49:27 +0100

    Refactorize ColorGrading

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/ControlsModule/SpecificControllers/ColorGradingControlController.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/ControlsModule/SpecificControllers/ColorGradingControlController.cs
@@ -1,19 +1,44 @@
+using DCL.SettingsController;
 using DCL.SettingsPanelHUD.Common;
+using System;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 namespace DCL.SettingsPanelHUD.Controls
 {
     [CreateAssetMenu(menuName = "Settings/Controllers/Controls/Color Grading", fileName = "ColorGradingControlController")]
     public class ColorGradingControlController : SettingsControlController
     {
+        const string COLOR_GRADING_SETTINGS_KEY = "Settings.ColorGrading";
+
+        private QualitySettingsController qualitySettings; // TODO (Santi): Refactorize!
+
+        public override void Initialize(ISettingsControlView settingsControlView)
+        {
+            base.Initialize(settingsControlView);
+
+            qualitySettings = GameObject.FindObjectOfType<QualitySettingsController>();
+        }
+
         public override object GetStoredValue()
         {
-            return currentQualitySetting.colorGrading;
+            string storedValue = PlayerPrefs.GetString(COLOR_GRADING_SETTINGS_KEY);
+            if (!String.IsNullOrEmpty(storedValue))
+                return Convert.ToBoolean(storedValue);
+            else
+                return Settings.i.qualitySettingsPresets.defaultPreset.colorGrading;
         }
 
         public override void OnControlChanged(object newValue)
         {
-            currentQualitySetting.colorGrading = (bool)newValue;
+            bool newMuteSoundValue = (bool)newValue;
+
+            Tonemapping toneMapping;
+            if (qualitySettings.postProcessVolume.profile.TryGet<Tonemapping>(out toneMapping))
+            {
+                toneMapping.active = newMuteSoundValue;
+                PlayerPrefs.SetString(COLOR_GRADING_SETTINGS_KEY, newValue.ToString());
+            }
         }
 
         public override void PostApplySettings()
