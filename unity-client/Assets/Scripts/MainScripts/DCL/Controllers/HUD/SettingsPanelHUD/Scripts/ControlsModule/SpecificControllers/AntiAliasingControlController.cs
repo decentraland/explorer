5629da8f11d9339d60c9b3b4f49ec26f72e6eacd
Santi Andrade <sandrade@decentraland.org>
2021-01-07 13:33:09 +0100

    Refactorize AntiAliasing

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/ControlsModule/SpecificControllers/AntiAliasingControlController.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/ControlsModule/SpecificControllers/AntiAliasingControlController.cs
@@ -1,30 +1,43 @@
 using DCL.SettingsPanelHUD.Common;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 namespace DCL.SettingsPanelHUD.Controls
 {
     [CreateAssetMenu(menuName = "Settings/Controllers/Controls/AntiAliasing", fileName = "AntiAliasingControlController")]
     public class AntiAliasingControlController : SettingsControlController
     {
+        const string ANTI_ALIASING_SETTINGS_KEY = "Settings.AntiAliasing";
         public const string TEXT_OFF = "OFF";
 
         private SliderSettingsControlView sliderView;
+        private UniversalRenderPipelineAsset lightweightRenderPipelineAsset = null;
 
         public override void Initialize(ISettingsControlView settingsControlView)
         {
             base.Initialize(settingsControlView);
 
             sliderView = (SliderSettingsControlView)view;
+
+            if (lightweightRenderPipelineAsset == null)
+                lightweightRenderPipelineAsset = GraphicsSettings.renderPipelineAsset as UniversalRenderPipelineAsset;
         }
 
         public override object GetStoredValue()
         {
-            float antiAliasingValue =
-                currentQualitySetting.antiAliasing == UnityEngine.Rendering.Universal.MsaaQuality.Disabled
-                    ? 0
-                    : ((int)currentQualitySetting.antiAliasing >> 2) + 1;
+            int storedValue = PlayerPrefs.GetInt(ANTI_ALIASING_SETTINGS_KEY, -1);
+            if (storedValue != -1)
+                return AntiAliasingToSliderValue((MsaaQuality)storedValue);
+            else
+                return AntiAliasingToSliderValue(Settings.i.qualitySettingsPresets.defaultPreset.antiAliasing);
+        }
 
-            return antiAliasingValue;
+        private float AntiAliasingToSliderValue(MsaaQuality antiAliasingValue)
+        {
+            return antiAliasingValue == MsaaQuality.Disabled
+                ? 0
+                : ((int)antiAliasingValue >> 2) + 1;
         }
 
         public override void OnControlChanged(object newValue)
@@ -32,7 +45,11 @@ namespace DCL.SettingsPanelHUD.Controls
             float newFloatValue = (float)newValue;
 
             int antiAliasingValue = 1 << (int)newFloatValue;
-            currentQualitySetting.antiAliasing = (UnityEngine.Rendering.Universal.MsaaQuality)antiAliasingValue;
+            if (lightweightRenderPipelineAsset != null)
+            {
+                lightweightRenderPipelineAsset.msaaSampleCount = antiAliasingValue;
+                PlayerPrefs.SetInt(ANTI_ALIASING_SETTINGS_KEY, antiAliasingValue);
+            }
 
             if (newFloatValue == 0)
                 sliderView.OverrideIndicatorLabel(TEXT_OFF);
