b79fffc9621f5f7d425ced6c29fbf3d97e68ebb6
Santi Andrade <sandrade@decentraland.org>
2021-01-07 12:23:16 +0100

    Refactorize AllowVoiceChat and VoiceChatVolume

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/ControlsModule/SpecificControllers/AllowVoiceChatControlController.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/ControlsModule/SpecificControllers/AllowVoiceChatControlController.cs
@@ -5,14 +5,27 @@ namespace DCL.SettingsPanelHUD.Controls
     [CreateAssetMenu(menuName = "Settings/Controllers/Controls/Allow Voice Chat", fileName = "AllowVoiceChatControlController")]
     public class AllowVoiceChatControlController : SettingsControlController
     {
+        const string ALLOW_VOICE_CHAT_SETTINGS_KEY = "Settings.AllowVoiceChat";
+        const string VOICE_CHAT_VOLUME_SETTINGS_KEY = "Settings.VoiceChatVolume";
+
         public override object GetStoredValue()
         {
-            return (int)currentGeneralSettings.voiceChatAllow;
+            int storedValue = PlayerPrefs.GetInt(ALLOW_VOICE_CHAT_SETTINGS_KEY, -1);
+            if (storedValue != -1)
+                return storedValue;
+            else
+                return (int)Settings.i.GetDefaultGeneralSettings().voiceChatAllow;
         }
 
         public override void OnControlChanged(object newValue)
         {
-            currentGeneralSettings.voiceChatAllow = (SettingsData.GeneralSettings.VoiceChatAllow)newValue;
+            int newAllowVoiceChatValue = (int)newValue;
+            PlayerPrefs.SetInt(ALLOW_VOICE_CHAT_SETTINGS_KEY, newAllowVoiceChatValue);
+
+            float voiceChatVolumeStoredValue = PlayerPrefs.GetFloat(VOICE_CHAT_VOLUME_SETTINGS_KEY, -1);
+            Interface.WebInterface.ApplySettings(
+                voiceChatVolumeStoredValue != -1 ? voiceChatVolumeStoredValue : Settings.i.GetDefaultGeneralSettings().voiceChatVolume,
+                newAllowVoiceChatValue);
         }
     }
 }
--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/ControlsModule/SpecificControllers/VoiceChatVolumeControlController.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/ControlsModule/SpecificControllers/VoiceChatVolumeControlController.cs
@@ -5,14 +5,27 @@ namespace DCL.SettingsPanelHUD.Controls
     [CreateAssetMenu(menuName = "Settings/Controllers/Controls/Voice Chat Volume", fileName = "VoiceChatVolumeControlController")]
     public class VoiceChatVolumeControlController : SettingsControlController
     {
+        const string VOICE_CHAT_VOLUME_SETTINGS_KEY = "Settings.VoiceChatVolume";
+        const string ALLOW_VOICE_CHAT_SETTINGS_KEY = "Settings.AllowVoiceChat";
+
         public override object GetStoredValue()
         {
-            return currentGeneralSettings.voiceChatVolume * 100;
+            float storedValue = PlayerPrefs.GetFloat(VOICE_CHAT_VOLUME_SETTINGS_KEY, -1);
+            if (storedValue != -1)
+                return storedValue * 100;
+            else
+                return Settings.i.GetDefaultGeneralSettings().voiceChatVolume * 100;
         }
 
         public override void OnControlChanged(object newValue)
         {
-            currentGeneralSettings.voiceChatVolume = (float)newValue * 0.01f;
+            float newVoiceChatVolumeValue = (float)newValue * 0.01f;
+            PlayerPrefs.SetFloat(VOICE_CHAT_VOLUME_SETTINGS_KEY, newVoiceChatVolumeValue);
+
+            int allowVoiceChatStoredValue = PlayerPrefs.GetInt(ALLOW_VOICE_CHAT_SETTINGS_KEY, -1);
+            Interface.WebInterface.ApplySettings(
+                newVoiceChatVolumeValue,
+                allowVoiceChatStoredValue != -1 ? allowVoiceChatStoredValue : (int)Settings.i.GetDefaultGeneralSettings().voiceChatAllow);
         }
     }
 }
