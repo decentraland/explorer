50d5b17be03827fe2f5e81c37837dcdbd46c920a
Santi Andrade <sandrade@decentraland.org>
2021-01-07 18:23:37 +0100

    Refactorize DrawDistance

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/ControlsModule/SpecificControllers/DrawDistanceControlController.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/ControlsModule/SpecificControllers/DrawDistanceControlController.cs
@@ -1,3 +1,4 @@
+using DCL.SettingsController;
 using DCL.SettingsPanelHUD.Common;
 using UnityEngine;
 
@@ -6,14 +7,40 @@ namespace DCL.SettingsPanelHUD.Controls
     [CreateAssetMenu(menuName = "Settings/Controllers/Controls/Draw Distance", fileName = "DrawDistanceControlController")]
     public class DrawDistanceControlController : SettingsControlController
     {
+        const string DRAW_DISTANCE_SETTINGS_KEY = "Settings.DrawDistance";
+
+        private QualitySettingsController qualitySettings; // TODO (Santi): Refactorize!
+
+        public override void Initialize(ISettingsControlView settingsControlView)
+        {
+            base.Initialize(settingsControlView);
+
+            qualitySettings = GameObject.FindObjectOfType<QualitySettingsController>();
+        }
+
         public override object GetStoredValue()
         {
-            return currentQualitySetting.cameraDrawDistance;
+            float storedValue = PlayerPrefs.GetFloat(DRAW_DISTANCE_SETTINGS_KEY, -1);
+            if (storedValue != -1)
+                return storedValue;
+            else
+                return Settings.i.qualitySettingsPresets.defaultPreset.cameraDrawDistance;
         }
 
         public override void OnControlChanged(object newValue)
         {
-            currentQualitySetting.cameraDrawDistance = (float)newValue;
+            float newFloatValue = (float)newValue;
+
+            if (qualitySettings.thirdPersonCamera)
+                qualitySettings.thirdPersonCamera.m_Lens.FarClipPlane = newFloatValue;
+
+            if (qualitySettings.firstPersonCamera)
+                qualitySettings.firstPersonCamera.m_Lens.FarClipPlane = newFloatValue;
+
+            RenderSettings.fogEndDistance = newFloatValue;
+            RenderSettings.fogStartDistance = newFloatValue * 0.8f;
+
+            PlayerPrefs.SetFloat(DRAW_DISTANCE_SETTINGS_KEY, newFloatValue);
         }
 
         public override void PostApplySettings()
