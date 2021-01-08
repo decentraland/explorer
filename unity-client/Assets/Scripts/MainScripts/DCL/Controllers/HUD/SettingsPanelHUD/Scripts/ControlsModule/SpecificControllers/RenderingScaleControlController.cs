ab7907356890a4b4b8fb911e9b8f52ed3c928dfa
Santi Andrade <sandrade@decentraland.org>
2021-01-08 11:30:14 +0100

    Refactorize RenderingScale

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/ControlsModule/SpecificControllers/RenderingScaleControlController.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/ControlsModule/SpecificControllers/RenderingScaleControlController.cs
@@ -1,22 +1,46 @@
 using DCL.SettingsPanelHUD.Common;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 namespace DCL.SettingsPanelHUD.Controls
 {
     [CreateAssetMenu(menuName = "Settings/Controllers/Controls/Rendering Scale", fileName = "RenderingScaleControlController")]
     public class RenderingScaleControlController : SettingsControlController
     {
+        const string RENDERING_SCALE_SETTINGS_KEY = "Settings.RenderingScale";
+
+        private UniversalRenderPipelineAsset lightweightRenderPipelineAsset = null;
+
+        public override void Initialize(ISettingsControlView settingsControlView)
+        {
+            base.Initialize(settingsControlView);
+
+            if (lightweightRenderPipelineAsset == null)
+                lightweightRenderPipelineAsset = GraphicsSettings.renderPipelineAsset as UniversalRenderPipelineAsset;
+        }
+
         public override object GetStoredValue()
         {
-            return currentQualitySetting.renderScale;
+            float storedValue = PlayerPrefs.GetFloat(RENDERING_SCALE_SETTINGS_KEY, -1);
+            if (storedValue != -1)
+                return storedValue;
+            else
+                return Settings.i.qualitySettingsPresets.defaultPreset.renderScale;
         }
 
         public override void OnControlChanged(object newValue)
         {
-            float renderingScaleValue = (float)newValue;
+            float newFloatValue = (float)newValue;
 
-            currentQualitySetting.renderScale = renderingScaleValue;
-            ((SliderSettingsControlView)view).OverrideIndicatorLabel(renderingScaleValue.ToString("0.0"));
+            currentQualitySetting.renderScale = newFloatValue;
+            ((SliderSettingsControlView)view).OverrideIndicatorLabel(newFloatValue.ToString("0.0"));
+
+            if (lightweightRenderPipelineAsset != null)
+            {
+                lightweightRenderPipelineAsset.renderScale = newFloatValue;
+                PlayerPrefs.SetFloat(RENDERING_SCALE_SETTINGS_KEY, newFloatValue);
+            }
         }
 
         public override void PostApplySettings()
