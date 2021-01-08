f8b955df25d3303b9aac74798f895397060df02d
Santi Andrade <sandrade@decentraland.org>
2021-01-08 11:46:26 +0100

    Refactorize ShadowDistance

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/ControlsModule/SpecificControllers/ShadowDistanceControlController.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/ControlsModule/SpecificControllers/ShadowDistanceControlController.cs
@@ -1,19 +1,44 @@
 using DCL.SettingsPanelHUD.Common;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 namespace DCL.SettingsPanelHUD.Controls
 {
     [CreateAssetMenu(menuName = "Settings/Controllers/Controls/Shadow Distance", fileName = "ShadowDistanceControlController")]
     public class ShadowDistanceControlController : SettingsControlController
     {
+        const string SHADOW_DISTANCE_SETTINGS_KEY = "Settings.ShadowDistance";
+
+        private UniversalRenderPipelineAsset lightweightRenderPipelineAsset = null;
+
+        public override void Initialize(ISettingsControlView settingsControlView)
+        {
+            base.Initialize(settingsControlView);
+
+            if (lightweightRenderPipelineAsset == null)
+            {
+                lightweightRenderPipelineAsset = GraphicsSettings.renderPipelineAsset as UniversalRenderPipelineAsset;
+            }
+        }
+
         public override object GetStoredValue()
         {
-            return currentQualitySetting.shadowDistance;
+            float storedValue = PlayerPrefs.GetFloat(SHADOW_DISTANCE_SETTINGS_KEY, -1);
+            if (storedValue != -1)
+                return storedValue;
+            else
+                return Settings.i.qualitySettingsPresets.defaultPreset.shadowDistance;
         }
 
         public override void OnControlChanged(object newValue)
         {
-            currentQualitySetting.shadowDistance = (float)newValue;
+            float newFloatValue = (float)newValue;
+
+            if (lightweightRenderPipelineAsset)
+                lightweightRenderPipelineAsset.shadowDistance = newFloatValue;
+
+            PlayerPrefs.SetFloat(SHADOW_DISTANCE_SETTINGS_KEY, newFloatValue);
         }
 
         public override void PostApplySettings()
