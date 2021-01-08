daa1cd6579a1593a84c541ea87f567da1001cbe7
Santi Andrade <sandrade@decentraland.org>
2021-01-08 11:02:13 +0100

    Refactorize FPSLimit

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/ControlsModule/SpecificControllers/FPSLimitControlController.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/SettingsPanelHUD/Scripts/ControlsModule/SpecificControllers/FPSLimitControlController.cs
@@ -1,19 +1,32 @@
 using DCL.SettingsPanelHUD.Common;
+using System;
 using UnityEngine;
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+using System.Runtime.InteropServices;
+#endif
 
 namespace DCL.SettingsPanelHUD.Controls
 {
     [CreateAssetMenu(menuName = "Settings/Controllers/Controls/FPS Limit", fileName = "FPSLimitControlController")]
     public class FPSLimitControlController : SettingsControlController
     {
+        const string FPS_LIMIT_SETTINGS_KEY = "Settings.FPSLimit";
+
         public override object GetStoredValue()
         {
-            return currentQualitySetting.fpsCap;
+            string storedValue = PlayerPrefs.GetString(FPS_LIMIT_SETTINGS_KEY);
+            if (!String.IsNullOrEmpty(storedValue))
+                return Convert.ToBoolean(storedValue);
+            else
+                return Settings.i.qualitySettingsPresets.defaultPreset.fpsCap;
         }
 
         public override void OnControlChanged(object newValue)
         {
-            currentQualitySetting.fpsCap = (bool)newValue;
+            bool newBoolValue = (bool)newValue;
+            ToggleFPSCap(newBoolValue);
+            PlayerPrefs.SetString(FPS_LIMIT_SETTINGS_KEY, newBoolValue.ToString());
         }
 
         public override void PostApplySettings()
@@ -22,5 +35,13 @@ namespace DCL.SettingsPanelHUD.Controls
 
             CommonSettingsEvents.RaiseSetQualityPresetAsCustom();
         }
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+        [DllImport("__Internal")] public static extern void ToggleFPSCap(bool useFPSCap);
+#else
+        public static void ToggleFPSCap(bool useFPSCap)
+        {
+        }
+#endif
     }
 }
