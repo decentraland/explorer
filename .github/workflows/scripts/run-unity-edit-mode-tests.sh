4e43e479ed2ead0eaf8ca8dec476feb70633b21b
Agustin Mendez <github@menduz.com>
2021-01-11 17:19:02 -0300

    wip

---
+++ .github/workflows/scripts/run-unity-edit-mode-tests.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity} \
+        -batchmode \
+        -logFile "$PROJECT_PATH/editmode.log" \
+        -projectPath "$PROJECT_PATH" \
+        -runTests \
+        -testPlatform EditMode \
+        -testResults "$PROJECT_PATH/editmode-results.xml" \
+        -enableCodeCoverage \
+        -coverageResultsPath "$PROJECT_PATH/CodeCoverage/" \
+        -coverageOptions "assemblyFilters:-*unity*" \
+        -burst-disable-compilation
+
+# Catch exit code
+EDIT_MODE_EXIT_CODE=$?
+
+# Print unity log output
+cat "$PROJECT_PATH/editmode.log"
+
+# Display results
+if [ $EDIT_MODE_EXIT_CODE -eq 0 ]; then
+  echo "Run succeeded, no failures occurred";
+elif [ $EDIT_MODE_EXIT_CODE -eq 2 ]; then
+  echo "Run succeeded, some tests failed";
+elif [ $EDIT_MODE_EXIT_CODE -eq 3 ]; then
+  echo "Run failure (other failure)";
+else
+  echo "Unexpected exit code $EDIT_MODE_EXIT_CODE";
+fi
---
+++ .github/workflows/scripts/run-unity-play-mode-tests.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity} \
+        -batchmode \
+        -logFile "$PROJECT_PATH/playmode.log" \
+        -projectPath "$PROJECT_PATH" \
+        -runTests \
+        -testPlatform PlayMode \
+        -testResults "$PROJECT_PATH/playmode-results.xml" \
+        -enableCodeCoverage \
+        -coverageResultsPath "$PROJECT_PATH/CodeCoverage/" \
+        -coverageOptions "assemblyFilters:-*unity*" \
+        -burst-disable-compilation
+
+# Catch exit code
+EDIT_MODE_=$?
+
+# Print unity log output
+cat "$PROJECT_PATH/playmode.log"
+
+# Display results
+if [ $EDIT_MODE_ -eq 0 ]; then
+  echo "Run succeeded, no failures occurred";
+elif [ $EDIT_MODE_ -eq 2 ]; then
+  echo "Run succeeded, some tests failed";
+elif [ $EDIT_MODE_ -eq 3 ]; then
+  echo "Run failure (other failure)";
+else
+  echo "Unexpected exit code $EDIT_MODE_";
+fi
