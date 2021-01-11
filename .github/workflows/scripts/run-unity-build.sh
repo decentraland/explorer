bd9913bbe758380b212d3f9ab8503d8830cec043
Agustin Mendez <github@menduz.com>
2021-01-11 18:52:48 -0300

    wip

--- .github/workflows/scripts/run-unity-build.sh
+++ .github/workflows/scripts/run-unity-build.sh
@@ -7,7 +7,7 @@ echo "Building for $BUILD_TARGET"
 export BUILD_PATH="$PROJECT_PATH/Builds/$BUILD_NAME/"
 mkdir -p $BUILD_PATH
 
-${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity} \
+xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity \
   -projectPath "$PROJECT_PATH" \
   -quit \
   -batchmode \
--- .github/workflows/scripts/run-unity-edit-mode-tests.sh
+++ .github/workflows/scripts/run-unity-edit-mode-tests.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity} \
+xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity \
         -batchmode \
         -logFile - \
         -projectPath "$PROJECT_PATH" \
