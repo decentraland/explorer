f7696d4473eeb287fc1c9d3eab3c756548624b33
Alejandro Jimenez <ajimenez@decentraland.org>
2021-01-14 12:48:43 +0100

    Distinction between QuestsPanel and QuestsTracker

---
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/QuestsTrackerHUD/QuestTrackerHUDController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class QuestTrackerHUDController : IHUD
+{
+    private QuestTrackerHUDView view;
+
+    public void Dispose()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Initialize()
+    {
+        view = QuestTrackerHUDView.Create();
+    }
+
+    public void SetVisibility(bool visible)
+    {
+        view?.gameObject.SetActive(visible);
+    }
+}
+
+public class QuestTrackerHUDView : MonoBehaviour
+{
+    public static QuestTrackerHUDView Create()
+    {
+        throw new NotImplementedException();
+    }
+}
