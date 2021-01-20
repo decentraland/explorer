f2b8547ded14f95a8f9893ce479a251d7e6004ea
Agustin Mendez <github@menduz.com>
2021-01-20 12:57:10 -0300

    progress on PE loading

---
+++ kernel/public/test-portable-experiences/p1/src/game.ts
@@ -0,0 +1,51 @@
+/// --- Set up a system ---
+
+class RotatorSystem {
+  // this group will contain every entity that has a Transform component
+  group = engine.getComponentGroup(Transform)
+
+  update(dt: number) {
+    // iterate over the entities of the group
+    for (let entity of this.group.entities) {
+      // get the Transform component of the entity
+      const transform = entity.getComponent(Transform)
+
+      // mutate the rotation
+      transform.rotate(Vector3.Up(), dt * 10)
+    }
+  }
+}
+
+// Add a new instance of the system to the engine
+engine.addSystem(new RotatorSystem())
+
+/// --- Spawner function ---
+
+function spawnCube(x: number, y: number, z: number) {
+  // create the entity
+  const cube = new Entity()
+
+  // add a transform to the entity
+  cube.addComponent(new Transform({ position: new Vector3(x, y, z) }))
+
+  // add a shape to the entity
+  cube.addComponent(new BoxShape())
+
+  // add the entity to the engine
+  engine.addEntity(cube)
+
+  return cube
+}
+
+/// --- Spawn a cube ---
+
+const cube = spawnCube(8, 1, 8)
+
+cube.addComponent(
+  new OnClick(() => {
+    cube.getComponent(Transform).scale.z *= 1.1
+    cube.getComponent(Transform).scale.x *= 0.9
+
+    spawnCube(Math.random() * 8 + 1, Math.random() * 8, Math.random() * 8 + 1)
+  })
+)
