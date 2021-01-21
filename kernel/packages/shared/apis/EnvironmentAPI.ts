0d5ececd949c2ad665a2bb1dd2ceea53d6a28479
Agustina Aldasoro <agusaldasoro@gmail.com>
2021-01-21 14:26:18 -0300

    Test commit

--- kernel/packages/shared/apis/EnvironmentAPI.ts
+++ kernel/packages/shared/apis/EnvironmentAPI.ts
@@ -18,6 +18,7 @@ declare const window: any
 @registerAPI('EnvironmentAPI')
 export class EnvironmentAPI extends ExposableAPI {
   data!: EnvironmentData<any>
+
   /**
    * Returns the coordinates and the definition of a parcel
    */
