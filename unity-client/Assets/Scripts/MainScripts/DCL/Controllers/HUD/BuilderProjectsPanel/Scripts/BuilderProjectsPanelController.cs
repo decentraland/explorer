eafcfa0a4ce3c0648c495e1e757658d6604f5cbb
Pato <pato@decentraland.org>
2021-01-29 19:07:36 -0300

    add class for easy mock data

---
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/BuilderProjectsPanel/Scripts/BuilderProjectsPanelController.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+
+public class BuilderProjectsPanelController
+{
+    internal readonly BuilderProjectsPanelView view;
+    internal readonly SectionsController sectionsController;
+    internal readonly ScenesViewController scenesViewController;
+
+    public BuilderProjectsPanelController() : this(
+        Object.Instantiate(Resources.Load<BuilderProjectsPanelView>("BuilderProjectsPanel")))
+    {
+    }
+
+    internal BuilderProjectsPanelController(BuilderProjectsPanelView view)
+    {
+        this.view = view;
+        view.name = "_BuilderProjectsPanel";
+
+        sectionsController = new SectionsController(view.sectionViewFactory, view.sectionsContainer);
+        scenesViewController = new ScenesViewController(view.sceneCardViewPrefab);
+
+        view.OnScenesToggleChanged += OnSceneToggleChanged;
+        view.OnInWorldScenesToggleChanged += OnInWorldScenesToggleChanged;
+        view.OnProjectsToggleChanged += OnProjectsToggleChanged;
+        view.OnLandToggleChanged += OnLandToggleChanged;
+
+        sectionsController.OnSectionShow += OnSectionShow;
+        sectionsController.OnSectionHide += OnSectionHide;
+
+        IDeployedSceneListener viewDeployedSceneListener = view;
+        IProjectSceneListener viewProjectSceneListener = view;
+        scenesViewController.OnDeployedSceneAdded += viewDeployedSceneListener.OnSceneAdded;
+        scenesViewController.OnDeployedSceneRemoved += viewDeployedSceneListener.OnSceneRemoved;
+        scenesViewController.OnDeployedScenesSet += viewDeployedSceneListener.OnSetScenes;
+        scenesViewController.OnProjectSceneAdded += viewProjectSceneListener.OnSceneAdded;
+        scenesViewController.OnProjectSceneRemoved += viewProjectSceneListener.OnSceneRemoved;
+        scenesViewController.OnProjectScenesSet += viewProjectSceneListener.OnSetScenes;
+        scenesViewController.NotifySet(viewDeployedSceneListener);
+        scenesViewController.NotifySet(viewProjectSceneListener);
+    }
+
+    void OnSceneToggleChanged(bool isOn)
+    {
+        if (isOn) sectionsController.OpenSection(SectionsController.SectionId.SCENES_MAIN);
+    }
+
+    void OnInWorldScenesToggleChanged(bool isOn)
+    {
+        if (isOn) sectionsController.OpenSection(SectionsController.SectionId.SCENES_DEPLOYED);
+    }
+
+    void OnProjectsToggleChanged(bool isOn)
+    {
+        if (isOn) sectionsController.OpenSection(SectionsController.SectionId.SCENES_PROJECT);
+    }
+
+    void OnLandToggleChanged(bool isOn)
+    {
+        if (isOn) sectionsController.OpenSection(SectionsController.SectionId.LAND);
+    }
+
+    void OnSectionShow(SectionBase sectionBase)
+    {
+        if (sectionBase is IDeployedSceneListener deployedSceneListener)
+        {
+            scenesViewController.OnDeployedSceneAdded += deployedSceneListener.OnSceneAdded;
+            scenesViewController.OnDeployedSceneRemoved += deployedSceneListener.OnSceneRemoved;
+            scenesViewController.OnDeployedScenesSet += deployedSceneListener.OnSetScenes;
+            scenesViewController.NotifySet(deployedSceneListener);
+        }
+        if (sectionBase is IProjectSceneListener projectSceneListener)
+        {
+            scenesViewController.OnProjectSceneAdded += projectSceneListener.OnSceneAdded;
+            scenesViewController.OnProjectSceneRemoved += projectSceneListener.OnSceneRemoved;
+            scenesViewController.OnProjectScenesSet += projectSceneListener.OnSetScenes;
+            scenesViewController.NotifySet(projectSceneListener);
+        }
+    }
+
+    void OnSectionHide(SectionBase sectionBase)
+    {
+        if (sectionBase is IDeployedSceneListener deployedSceneListener)
+        {
+            scenesViewController.OnDeployedSceneAdded -= deployedSceneListener.OnSceneAdded;
+            scenesViewController.OnDeployedSceneRemoved -= deployedSceneListener.OnSceneRemoved;
+            scenesViewController.OnDeployedScenesSet -= deployedSceneListener.OnSetScenes;
+        }
+        if (sectionBase is IProjectSceneListener projectSceneListener)
+        {
+            scenesViewController.OnProjectSceneAdded -= projectSceneListener.OnSceneAdded;
+            scenesViewController.OnProjectSceneRemoved -= projectSceneListener.OnSceneRemoved;
+            scenesViewController.OnProjectScenesSet -= projectSceneListener.OnSetScenes;
+        }
+    }
+}
---
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/BuilderProjectsPanel/Scripts/MockBuilderProjectPanel_Controller.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+internal static class MockBuilderProjectPanel_Controller
+{
+    private static bool initialized = false;
+    private static BuilderProjectsPanelController controller;
+
+    public static void Initialize(BuilderProjectsPanelView view)
+    {
+        if (initialized) return;
+        initialized = true;
+
+        controller = new BuilderProjectsPanelController(view);
+    }
+
+    public static void RunSectionScenesTestSequence()
+    {
+        if (!initialized)
+        {
+            Debug.LogError("Mock not initialized");
+            return;
+        }
+        RunSequence(SectionSceneTestSequence());
+    }
+
+    private static void RunSequence(IEnumerator coroutine)
+    {
+        controller.view.StartCoroutine(coroutine);
+    }
+
+    private static IEnumerator SectionSceneTestSequence()
+    {
+        const float TIME = 2;
+        List<ISceneData> scenes = new List<ISceneData>();
+        var scenesViewController = controller.scenesViewController;
+
+        yield return new WaitForSeconds(TIME);
+        Debug.Log("ADD PROJECT");
+
+        scenes.Add(new SceneData()
+        {
+            id = "MyProject1",
+            isDeployed = false,
+            name = "MyProject1"
+        });
+        scenesViewController.SetScenes(scenes);
+
+        yield return new WaitForSeconds(TIME);
+        Debug.Log("ADD DEPLOY");
+
+        scenes.Add(new SceneData()
+        {
+            id = "MyDeploy1",
+            isDeployed = true,
+            name = "MyDeploy1"
+        });
+        scenesViewController.SetScenes(scenes);
+
+        yield return new WaitForSeconds(TIME);
+        Debug.Log("ADD DEPLOY2");
+
+        scenes.Add(new SceneData()
+        {
+            id = "MyDeploy2",
+            isDeployed = true,
+            name = "MyDeploy2"
+        });
+        scenesViewController.SetScenes(scenes);
+
+        yield return new WaitForSeconds(TIME);
+        Debug.Log("ADD DEPLOY3");
+
+        scenes.Add(new SceneData()
+        {
+            id = "MyDeploy3",
+            isDeployed = true,
+            name = "MyDeploy3"
+        });
+        scenesViewController.SetScenes(scenes);
+
+        yield return new WaitForSeconds(TIME);
+        Debug.Log("ADD DEPLOY4");
+
+        scenes.Add(new SceneData()
+        {
+            id = "MyDeploy4",
+            isDeployed = true,
+            name = "MyDeploy4"
+        });
+        scenesViewController.SetScenes(scenes);
+
+        yield return new WaitForSeconds(TIME);
+        Debug.Log("REMOVE DEPLOY3");
+
+        scenes = scenes.FindAll((data) => data.id != "MyDeploy3");
+        scenesViewController.SetScenes(scenes);
+
+        yield return new WaitForSeconds(TIME);
+        Debug.Log("REMOVE DEPLOY4");
+
+        scenes = scenes.FindAll((data) => data.id != "MyDeploy4");
+        scenesViewController.SetScenes(scenes);
+
+        yield return new WaitForSeconds(TIME);
+        Debug.Log("REMOVE DEPLOY2");
+
+        scenes = scenes.FindAll((data) => data.id != "MyDeploy2");
+        scenesViewController.SetScenes(scenes);
+
+        yield return new WaitForSeconds(TIME);
+        Debug.Log("REMOVE DEPLOY1");
+
+        scenes = scenes.FindAll((data) => data.id != "MyDeploy1");
+        scenesViewController.SetScenes(scenes);
+
+        yield return new WaitForSeconds(TIME);
+        Debug.Log("REMOVE ALL");
+        scenesViewController.SetScenes(new List<ISceneData>());
+    }
+}
