4a221ac20098bd43cb7b66d3c8b81bfe712565fc
Pato <pato@decentraland.org>
2021-02-02 15:49:24 -0300

    add tests for deployed and project menu section

---
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/BuilderProjectsPanel/Tests/SectionDeployedScenesViewShould.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class SectionDeployedScenesViewShould
+    {
+        private SectionDeployedScenesView view;
+
+        [SetUp]
+        public void SetUp()
+        {
+            const string prefabAssetPath = "BuilderProjectsPanelMenuSections/SectionDeployedScenesView";
+            var prefab = Resources.Load<SectionDeployedScenesView>(prefabAssetPath);
+            view = Object.Instantiate(prefab);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.Destroy(view.gameObject);
+        }
+
+        [Test]
+        public void HaveScenesContainerEmptyAtInstantiation()
+        {
+            Assert.AreEqual(0, view.scenesCardContainer.childCount);
+        }
+    }
+}
---
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/BuilderProjectsPanel/Tests/SectionProjectScenesViewShould.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class SectionProjectScenesViewShould
+    {
+        private SectionProjectScenesView view;
+
+        [SetUp]
+        public void SetUp()
+        {
+            const string prefabAssetPath = "BuilderProjectsPanelMenuSections/SectionProjectScenesView";
+            var prefab = Resources.Load<SectionProjectScenesView>(prefabAssetPath);
+            view = Object.Instantiate(prefab);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.Destroy(view.gameObject);
+        }
+
+        [Test]
+        public void HaveScenesContainerEmptyAtInstantiation()
+        {
+            Assert.AreEqual(0, view.scenesCardContainer.childCount);
+        }
+    }
+}
