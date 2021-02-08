2fa81f4a40727e8cf94609451c160ba227398707
Pablo de Haro <pablitar@gmail.com>
2021-02-08 11:22:43 -0300

    Extracted mappings to another file

---
+++ kernel/packages/shared/quests/mappings.ts
@@ -0,0 +1,63 @@
+import { PlayerQuestDetails, PlayerTaskDetails, ProgressStatus } from 'dcl-quests-client'
+import { QuestForRenderer, SectionForRenderer, TaskForRenderer } from './types'
+
+export function toRendererQuest(details: PlayerQuestDetails): QuestForRenderer {
+  return {
+    id: details.id,
+    name: details.name,
+    description: details.description,
+    icon: details.icon,
+    thumbnail_banner: details.thumbnailBanner,
+    thumbnail_entry: details.thumbnailEntry,
+    sections: toRendererSections(details.tasks)
+  }
+}
+
+export function toRendererSections(tasks: PlayerTaskDetails[]): SectionForRenderer[] {
+  const sectionsMap = tasks.reduce<Record<string, SectionForRenderer>>((currentMap, task) => {
+    const sectionName = task.section ?? ''
+    const section = currentMap[sectionName] ?? { id: sectionName, name: sectionName, progress: 0, tasks: [] }
+
+    section.tasks.push(toRendererTask(task))
+
+    currentMap[sectionName] = section
+    return currentMap
+  }, {})
+
+  const sections = Object.values(sectionsMap)
+
+  sections.forEach((it) => {
+    it.progress = it.tasks.length > 0 ? it.tasks.reduce((a, b) => a + b.progress, 0) / it.tasks.length : 0
+  })
+
+  return sections
+}
+
+export function toRendererTask(task: PlayerTaskDetails): TaskForRenderer {
+  return {
+    id: task.id,
+    progress: task.progressPercentage,
+    name: task.description,
+    coordinates: task.coordinates,
+    payload: JSON.stringify(getProgressPayload(task)),
+    type: task.progressMode.type
+  }
+}
+
+export function getProgressPayload(task: PlayerTaskDetails) {
+  const progressMode = task.progressMode
+  switch (progressMode.type) {
+    case 'single':
+      return { isDone: task.progressStatus === ProgressStatus.COMPLETED }
+    case 'numeric':
+      return {
+        type: 'numeric',
+        start: progressMode.start,
+        end: progressMode.end,
+        current: task.progressSummary?.current ?? progressMode.start
+      }
+    case 'step-based':
+      // TODO
+      return {}
+  }
+}
--- kernel/packages/shared/quests/sagas.ts
+++ kernel/packages/shared/quests/sagas.ts
@@ -1,9 +1,9 @@
-import { ClientResponse, PlayerQuestDetails, PlayerTaskDetails, ProgressStatus } from 'dcl-quests-client'
+import { ClientResponse, PlayerQuestDetails } from 'dcl-quests-client'
 import { takeEvery } from 'redux-saga/effects'
 import { USER_AUTHENTIFIED } from 'shared/session/actions'
 import { questsRequest } from './client'
-import { QuestForRenderer, SectionForRenderer, TaskForRenderer } from './types'
 import { unityInterface } from 'unity-interface/UnityInterface'
+import { toRendererQuest } from './mappings'
 
 export function* questsSaga(): any {
   yield takeEvery(USER_AUTHENTIFIED, fetchQuests)
@@ -20,65 +20,4 @@ function updateQuestsLogData(quests: PlayerQuestDetails[]) {
   const rendererQuests = quests.map((it) => toRendererQuest(it))
 
   unityInterface.InitQuestsInfo(rendererQuests)
-}
-
-function toRendererQuest(details: PlayerQuestDetails): QuestForRenderer {
-  return {
-    id: details.id,
-    name: details.name,
-    description: details.description,
-    icon: details.icon,
-    thumbnail_banner: details.thumbnailBanner,
-    thumbnail_entry: details.thumbnailEntry,
-    sections: toRendererSections(details.tasks)
-  }
-}
-
-function toRendererSections(tasks: PlayerTaskDetails[]): SectionForRenderer[] {
-  const sectionsMap = tasks.reduce<Record<string, SectionForRenderer>>((currentMap, task) => {
-    const sectionName = task.section ?? ''
-    const section = currentMap[sectionName] ?? { id: sectionName, name: sectionName, progress: 0, tasks: [] }
-
-    section.tasks.push(toRendererTask(task))
-
-    currentMap[sectionName] = section
-    return currentMap
-  }, {})
-
-  const sections = Object.values(sectionsMap)
-
-  sections.forEach((it) => {
-    it.progress = it.tasks.length > 0 ? it.tasks.reduce((a, b) => a + b.progress, 0) / it.tasks.length : 0
-  })
-
-  return sections
-}
-
-function toRendererTask(task: PlayerTaskDetails): TaskForRenderer {
-  return {
-    id: task.id,
-    progress: task.progressPercentage,
-    name: task.description,
-    coordinates: task.coordinates,
-    payload: JSON.stringify(getProgressPayload(task)),
-    type: task.progressMode.type
-  }
-}
-
-function getProgressPayload(task: PlayerTaskDetails) {
-  const progressMode = task.progressMode
-  switch (progressMode.type) {
-    case 'single':
-      return { isDone: task.progressStatus === ProgressStatus.COMPLETED }
-    case 'numeric':
-      return {
-        type: 'numeric',
-        start: progressMode.start,
-        end: progressMode.end,
-        current: task.progressSummary?.current ?? progressMode.start
-      }
-    case 'step-based':
-      // TODO
-      return {}
-  }
 }
