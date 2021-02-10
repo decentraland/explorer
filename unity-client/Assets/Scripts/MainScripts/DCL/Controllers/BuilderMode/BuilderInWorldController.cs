04a3491fdb2ca95a3de59c2bd9d843bdb6e6706b
Suduck <adrian@suduck.com>
2021-02-10 17:45:09 +0100

    fix: webbrorswer log enabled

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/BuilderMode/BuilderInWorldController.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/BuilderMode/BuilderInWorldController.cs
@@ -886,7 +886,9 @@ public class BuilderInWorldController : MonoBehaviour
         UserProfile userProfile = UserProfile.GetOwnUserProfile();
         foreach (UserProfileModel.ParcelsWithAccess parcelWithAccess in userProfile.parcelsWithAccess)
         {
+            Debug.unityLogger.logEnabled = true;
             Debug.Log("Access to X:" + parcelWithAccess.x + "   Y:" + parcelWithAccess.y + "     ROLE: " + parcelWithAccess.landRole);
+            Debug.unityLogger.logEnabled = false;
             foreach (Vector2Int parcel in scene.sceneData.parcels)
             {
                 if (parcel.x == parcelWithAccess.x && parcel.y == parcelWithAccess.y) return true;
