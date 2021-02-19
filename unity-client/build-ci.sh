bc2127a04ecc376afadb615e5a66658083cf3870
Brian Amadori <brian@decentraland.org>
2021-02-19 16:29:38 -0300

    update with new license

--- unity-client/build-ci.sh
+++ unity-client/build-ci.sh
@@ -12,10 +12,10 @@ cd $BUILD_PATH
 export BUILD_PATH=$PWD
 cd ../..
 
-${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' $UNITY_PATH/Editor/Unity} \
-  -projectPath $(pwd) \
+xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' $UNITY_PATH/Editor/Unity \
   -quit \
   -batchmode \
+  -projectPath $(pwd) \
   -buildTarget $BUILD_TARGET \
   -customBuildTarget $BUILD_TARGET \
   -customBuildName $BUILD_NAME \
