69a730298856e4569673b1a299e250e85f60bd3f
Brian Amadori <brian@decentraland.org>
2021-02-19 15:34:47 -0300

    unity executable path fix

--- unity-client/build-ci.sh
+++ unity-client/build-ci.sh
@@ -12,7 +12,7 @@ cd $BUILD_PATH
 export BUILD_PATH=$PWD
 cd ../..
 
-${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity} \
+${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' $UNITY_PATH/Editor/Unity} \
   -projectPath $(pwd) \
   -quit \
   -batchmode \
