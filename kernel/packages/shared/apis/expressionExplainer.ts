18e955fcb308d4e699fef66d430280cb99b8298e
Alejandro Jimenez <ajimenez@decentraland.org>
2021-02-19 16:18:39 +0100

    Anniversary animations

--- kernel/packages/shared/apis/expressionExplainer.ts
+++ kernel/packages/shared/apis/expressionExplainer.ts
@@ -1,4 +1,4 @@
-export const expressionExplainer = { robot: 'the robot dance!', wave: 'waving', fistpump: 'fist-pumping' }
+export const expressionExplainer = { robot: 'the robot dance!', wave: 'waving', fistpump: 'fist-pumping', tik: "tik", hammer: "hammer", tektonik: "tektonik" }
 export const validExpressions = Object.keys(expressionExplainer)
 
 export type validExpression = keyof typeof validExpressions
