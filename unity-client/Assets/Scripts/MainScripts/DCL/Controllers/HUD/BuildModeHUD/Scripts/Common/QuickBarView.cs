eb605b6f83e4972b02c37b87db9068cd4b9da9e9
Santi Andrade <sandrade@decentraland.org>
2021-03-02 11:43:12 +0100

    Refactorize serialized references in QuickBarView

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/BuildModeHUD/Scripts/Common/QuickBarView.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/BuildModeHUD/Scripts/Common/QuickBarView.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class QuickBarView : MonoBehaviour
 {
@@ -9,6 +11,8 @@ public class QuickBarView : MonoBehaviour
     public event System.Action<int> OnQuickBarInputTriggered;
 
     [SerializeField] internal QuickBarSlot[] shortcutsImgs;
+    [SerializeField] internal Button[] shortcutsButtons;
+    [SerializeField] internal EventTrigger[] shortcutsEventTriggers;
     [SerializeField] internal InputAction_Trigger quickBar1InputAction;
     [SerializeField] internal InputAction_Trigger quickBar2InputAction;
     [SerializeField] internal InputAction_Trigger quickBar3InputAction;
@@ -19,8 +23,24 @@ public class QuickBarView : MonoBehaviour
     [SerializeField] internal InputAction_Trigger quickBar8InputAction;
     [SerializeField] internal InputAction_Trigger quickBar9InputAction;
 
-    private void Start()
+    private void Awake()
     {
+        for (int i = 0; i < shortcutsButtons.Length; i++)
+        {
+            int buttonIndex = i;
+            shortcutsButtons[buttonIndex].onClick.AddListener(() => QuickBarObjectSelected(buttonIndex));
+        }
+
+        for (int i = 0; i < shortcutsEventTriggers.Length; i++)
+        {
+            int triggerIndex = i;
+            ConfigureEventTrigger(triggerIndex, EventTriggerType.Drop, (eventData) =>
+            {
+                SetIndexToDrop(triggerIndex);
+                SceneObjectDropped(eventData);
+            });
+        }
+
         quickBar1InputAction.OnTriggered += OnQuickBar1InputTriggedered;
         quickBar2InputAction.OnTriggered += OnQuickBar2InputTriggedered;
         quickBar3InputAction.OnTriggered += OnQuickBar3InputTriggedered;
@@ -34,6 +54,18 @@ public class QuickBarView : MonoBehaviour
 
     private void OnDestroy()
     {
+        for (int i = 0; i < shortcutsButtons.Length; i++)
+        {
+            int buttonIndex = i;
+            shortcutsButtons[buttonIndex].onClick.RemoveListener(() => QuickBarObjectSelected(buttonIndex));
+        }
+
+        for (int i = 0; i < shortcutsEventTriggers.Length; i++)
+        {
+            int triggerIndex = i;
+            RemoveEventTrigger(triggerIndex, EventTriggerType.Drop);
+        }
+
         quickBar1InputAction.OnTriggered -= OnQuickBar1InputTriggedered;
         quickBar2InputAction.OnTriggered -= OnQuickBar2InputTriggedered;
         quickBar3InputAction.OnTriggered -= OnQuickBar3InputTriggedered;
@@ -45,19 +77,29 @@ public class QuickBarView : MonoBehaviour
         quickBar9InputAction.OnTriggered -= OnQuickBar9InputTriggedered;
     }
 
-    // TODO (Santi): Called from imspector!
+    private void ConfigureEventTrigger(int index, EventTriggerType eventType, UnityAction<BaseEventData> call)
+    {
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = eventType;
+        entry.callback.AddListener(call);
+        shortcutsEventTriggers[index].triggers.Add(entry);
+    }
+
+    private void RemoveEventTrigger(int index, EventTriggerType eventType)
+    {
+        shortcutsEventTriggers[index].triggers.RemoveAll(x => x.eventID == eventType);
+    }
+
     public void QuickBarObjectSelected(int index)
     {
         OnQuickBarObjectSelected?.Invoke(index);
     }
 
-    // TODO (Santi): Called from imspector!
     public void SetIndexToDrop(int index)
     {
         OnSetIndexToDrop?.Invoke(index);
     }
 
-    // TODO (Santi): Called from imspector!
     public void SceneObjectDropped(BaseEventData data)
     {
         OnSceneObjectDropped?.Invoke(data);
