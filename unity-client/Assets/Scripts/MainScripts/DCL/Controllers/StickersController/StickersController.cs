770411e912863cfb75eccce08bef32d077df2be5
Alejandro Jimenez <ajimenez@decentraland.org>
2021-03-15 12:56:09 +0100

    Stickers WIP

---
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/StickersController/StickersController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class StickersController : MonoBehaviour
+{
+    [SerializeField] private StickersFactory stickersFactory;
+
+    public void PlayEmote(string id)
+    {
+        if (!stickersFactory.TryGet(id, out GameObject prefab))
+            return;
+
+        var emoteGameObject = Instantiate(prefab);
+        emoteGameObject.AddComponent<>()
+    }
+}
