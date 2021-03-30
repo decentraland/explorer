a4dfff4e14fa8a209c6cf59d9e5eb4760acf6ccf
Alejandro Jimenez <ajimenez@decentraland.org>
2021-03-30 17:12:50 +0200

    NewQuest tooltip in the taskbar

---
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/HUD/TaskbarHUD/TaskbarNewQuestTooltip.cs
@@ -0,0 +1,12 @@
+﻿using DCL;
+using UnityEngine;
+
+public class TaskbarNewQuestTooltip : MonoBehaviour
+{
+    private static readonly int ANIM_STATE_TRIGGER = Animator.StringToHash("ShowDisabledTooltip");
+    [SerializeField] private Animator animator;
+
+    private void Awake() { DataStore.i.Quests.quests.OnAdded += OnQuestsAdded; }
+
+    private void OnQuestsAdded(string s, QuestModel model) { animator?.SetTrigger(ANIM_STATE_TRIGGER); }
+}
