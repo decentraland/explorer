3ff025c17cd3b00d5bd9cd6db24617637217bf79
Brian Amadori <brian@decentraland.org>
2021-04-12 14:12:33 -0300

    add gentler limits to GetSafeBounds

--- unity-client/Assets/Scripts/MainScripts/DCL/Helpers/Utils/Utils.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Helpers/Utils/Utils.cs
@@ -662,8 +662,8 @@ namespace DCL.Helpers
         /// <returns>The bounds value if the value is correct, or a mocked bounds object with clamped values if its too far away.</returns>
         public static Bounds GetSafeBounds( this Renderer renderer )
         {
-            if ( Vector3.Distance(renderer.transform.position, Vector3.zero) > 10000 )
-                return new Bounds( Vector3.one * 10000, Vector3.one * 0.1f );
+            if ( Vector3.Distance(renderer.transform.position, Vector3.zero) > 1000000 )
+                return new Bounds( Vector3.one * 1000000, Vector3.one * 0.1f );
 
             return renderer.bounds;
         }
