cecf5b4421b0db3183fb8e2ed19e8087b7c64c02
Brian Amadori <brian@decentraland.org>
2021-04-12 18:22:30 -0300

    improve GetSafeBounds() algorithm

--- unity-client/Assets/Scripts/MainScripts/DCL/Helpers/Utils/Utils.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Helpers/Utils/Utils.cs
@@ -662,8 +662,12 @@ namespace DCL.Helpers
         /// <returns>The bounds value if the value is correct, or a mocked bounds object with clamped values if its too far away.</returns>
         public static Bounds GetSafeBounds( this Renderer renderer )
         {
-            if ( Vector3.Distance(renderer.transform.position, Vector3.zero) > 1000000 )
-                return new Bounds( Vector3.one * 1000000, Vector3.one * 0.1f );
+            // World extents are of 4800 world mts, so this limit far exceeds the world size.
+            const float POSITION_OVERFLOW_LIMIT = 10000;
+            const float POSITION_OVERFLOW_LIMIT_SQR = POSITION_OVERFLOW_LIMIT * POSITION_OVERFLOW_LIMIT;
+
+            if ( renderer.transform.position.sqrMagnitude > POSITION_OVERFLOW_LIMIT_SQR )
+                return new Bounds( Vector3.one * POSITION_OVERFLOW_LIMIT, Vector3.one * 0.1f );
 
             return renderer.bounds;
         }
