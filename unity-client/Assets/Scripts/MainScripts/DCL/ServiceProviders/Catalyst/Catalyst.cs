6bba6afcb51ea524e3ce309cc0878989fb528fbd
Pato <pato@decentraland.org>
2021-04-19 13:07:51 -0300

    debug log for webgl testing

--- unity-client/Assets/Scripts/MainScripts/DCL/ServiceProviders/Catalyst/Catalyst.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/ServiceProviders/Catalyst/Catalyst.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using DCL;
 using DCL.Helpers;
+using UnityEngine;
 using Variables.RealmsInfo;
 
 public interface ICatalyst : IDisposable
@@ -119,10 +120,12 @@ public class Catalyst : ICatalyst
         cache[url] = result;
 
         // NOTE: remove from cache after CACHE_TIME_MSECS time passed
+        Debug.Log("PATO: CACHE ADDED");
         Task.Delay(CACHE_TIME_MSECS)
             .ContinueWith((task) =>
             {
                 cache.Remove(url);
+                Debug.Log("PATO: CACHE REMOVED");
             }, cancellationTokenSource.Token);
     }
 }
