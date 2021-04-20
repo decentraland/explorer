bd20752538448f6a0adbb4c2f82de8a0dba0a809
Pato <pato@decentraland.org>
2021-04-20 19:43:04 -0300

    fix: catalyst request with too many pointers

--- unity-client/Assets/Scripts/MainScripts/DCL/ServiceProviders/Catalyst/Catalyst.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/ServiceProviders/Catalyst/Catalyst.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using DCL;
 using DCL.Helpers;
@@ -18,6 +19,7 @@ public interface ICatalyst : IDisposable
 public class Catalyst : ICatalyst
 {
     private const float CACHE_TIME = 5 * 60;
+    private const int MAX_POINTERS_PER_REQUEST = 90;
 
     public string contentUrl => realmContentServerUrl;
 
@@ -48,15 +50,33 @@ public class Catalyst : ICatalyst
         GetEntities(CatalystEntitiesType.SCENE, parcels)
             .Then(json =>
             {
-                CatalystSceneEntityPayload[] parsedValue = null;
+                CatalystSceneEntityPayload[] scenes = null;
                 bool hasException = false;
                 try
                 {
-                    parsedValue = Utils.ParseJsonArray<CatalystSceneEntityPayload[]>(json);
+                    CatalystSceneEntityPayload[] parsedValue = Utils.ParseJsonArray<CatalystSceneEntityPayload[]>(json);
+                    
+                    // remove duplicated 
+                    List<CatalystSceneEntityPayload> noDuplicates = new List<CatalystSceneEntityPayload>();
+                    for (int i = 0; i < parsedValue.Length; i++)
+                    {
+                        var sceneToCheck = parsedValue[i];
+                        if (noDuplicates.Any(scene => scene.id == sceneToCheck.id))
+                            continue;
+                        
+                        noDuplicates.Add(sceneToCheck);
+                    }
+                    scenes = noDuplicates.ToArray();
                 }
                 catch (Exception e)
                 {
                     promise.Reject(e.Message);
+                    hasException = true;
+                }
+                finally
+                {
+                    if (!hasException)
+                        promise.Resolve(scenes);
                 }
             })
             .Catch(error => promise.Reject(error));
@@ -66,13 +86,68 @@ public class Catalyst : ICatalyst
     
     public Promise<string> GetEntities(string entityType, string[] pointers)
     {
-        string deploymentsUrl = $"content/entities/{entityType}";
-        string urlParams = "";
-        urlParams = pointers.Aggregate(urlParams, (current, pointer) => current + $"&pointer={pointer}");
+        Promise<string> promise = new Promise<string>();
         
-        string url = $"{realmDomain}/{deploymentsUrl}?{urlParams}";
+        string[][] pointersToFetch;
 
-        return Get(url);
+        if (pointers.Length <= MAX_POINTERS_PER_REQUEST)
+        {
+            pointersToFetch = new [] { pointers };
+        }
+        else
+        {
+            // split pointers array in length of MAX_POINTERS_PER_REQUEST
+            int i = 0;
+            var query = from s in pointers
+                let num = i++
+                group s by num / MAX_POINTERS_PER_REQUEST
+                into g
+                select g.ToArray();
+            pointersToFetch = query.ToArray();
+        }
+
+        if (pointersToFetch.Length == 0)
+        {
+            promise.Reject("error: no pointers to fetch");
+            return promise;
+        }
+
+        Promise<string>[] splittedPromises = new Promise<string>[pointersToFetch.Length];
+        
+        for (int i = 0; i < pointersToFetch.Length; i++)
+        {
+            string urlParams = "";
+            urlParams = pointersToFetch[i].Aggregate(urlParams, (current, pointer) => current + $"&pointer={pointer}");
+            string url = $"{realmDomain}/content/entities/{entityType}?{urlParams}";
+            
+            splittedPromises[i] = Get(url);
+            splittedPromises[i].Then(value =>
+            {
+                for (int j = 0; j < splittedPromises.Length; j++)
+                {
+                    if (splittedPromises[j] == null || splittedPromises[j].keepWaiting || !string.IsNullOrEmpty(splittedPromises[j].error))
+                    {
+                        return;
+                    }
+                }
+
+                // make sure not to continue if promise was already resolved
+                if (!promise.keepWaiting)
+                    return;
+                
+                // build json with all promises result
+                string json = splittedPromises[0].value.Substring(1, splittedPromises[0].value.Length - 2);
+                for (int j = 1; j < splittedPromises.Length; j++)
+                {
+                    string jsonContent = splittedPromises[j].value.Substring(1, splittedPromises[j].value.Length - 2);
+                    json += $",{jsonContent}";
+                }
+                promise.Resolve($"[{json}]");
+            });
+            splittedPromises[i].Catch(error => promise.Reject(error));
+        }
+
+        return promise;
     }
 
     public Promise<string> Get(string url)
