bb9d32445a7b64184b7699f8ccf8ff0fb3d89b1f
Alejandro Jimenez <ajimenez@decentraland.org>
2021-04-20 14:00:29 +0200

    Detect bogus texture

--- unity-client/Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/Texture/AssetPromise_Texture.cs
+++ unity-client/Assets/Scripts/MainScripts/DCL/Controllers/AssetManager/Texture/AssetPromise_Texture.cs
@@ -32,18 +32,11 @@ namespace DCL
             idWithTexSettings = UsesDefaultWrapAndFilterMode() ? idWithDefaultTexSettings : ConstructId(url, wrapMode, filterMode);
         }
 
-        protected override void OnAfterLoadOrReuse()
-        {
-        }
+        protected override void OnAfterLoadOrReuse() { }
 
-        protected override void OnBeforeLoadOrReuse()
-        {
-        }
+        protected override void OnBeforeLoadOrReuse() { }
 
-        protected override object GetLibraryAssetCheckId()
-        {
-            return idWithTexSettings;
-        }
+        protected override object GetLibraryAssetCheckId() { return idWithTexSettings; }
 
         protected override void OnCancelLoading()
         {
@@ -71,7 +64,10 @@ namespace DCL
                     if (success)
                     {
                         asset.texture = DownloadHandlerTexture.GetContent(webRequest);
-                        OnSuccess?.Invoke();
+                        if(IsQuestionMarkPNG(asset.texture))
+                            OnFail?.Invoke();
+                        else
+                            OnSuccess?.Invoke();
                     }
                     else
                     {
@@ -86,7 +82,7 @@ namespace DCL
                 //For Base64 protocols we just take the bytes and create the texture
                 //to avoid Unity's web request issue with large URLs
                 byte[] decodedTexture = Convert.FromBase64String(url.Substring(PLAIN_BASE64_PROTOCOL.Length));
-                asset.texture = new Texture2D(1,1);
+                asset.texture = new Texture2D(1, 1);
                 asset.texture.LoadImage(decodedTexture);
                 OnSuccess?.Invoke();
             }
@@ -130,10 +126,7 @@ namespace DCL
             return true;
         }
 
-        string ConstructId(string textureUrl, TextureWrapMode textureWrapMode, FilterMode textureFilterMode)
-        {
-            return ((int)textureWrapMode).ToString() + ((int)textureFilterMode).ToString() + textureUrl;
-        }
+        string ConstructId(string textureUrl, TextureWrapMode textureWrapMode, FilterMode textureFilterMode) { return ((int)textureWrapMode).ToString() + ((int)textureFilterMode).ToString() + textureUrl; }
 
         public override object GetId()
         {
@@ -141,9 +134,22 @@ namespace DCL
             return idWithDefaultTexSettings;
         }
 
-        public bool UsesDefaultWrapAndFilterMode()
+        public bool UsesDefaultWrapAndFilterMode() { return wrapMode == DEFAULT_WRAP_MODE && filterMode == DEFAULT_FILTER_MODE; }
+
+        static byte[] questionMarkPNG = { 137, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 0, 8, 0, 0, 0, 8, 8, 2, 0, 0, 0, 75, 109, 41, 220, 0, 0, 0, 65, 73, 68, 65, 84, 8, 29, 85, 142, 81, 10, 0, 48, 8, 66, 107, 236, 254, 87, 110, 106, 35, 172, 143, 74, 243, 65, 89, 85, 129, 202, 100, 239, 146, 115, 184, 183, 11, 109, 33, 29, 126, 114, 141, 75, 213, 65, 44, 131, 70, 24, 97, 46, 50, 34, 72, 25, 39, 181, 9, 251, 205, 14, 10, 78, 123, 43, 35, 17, 17, 228, 109, 164, 219, 0, 0, 0, 0, 73, 69, 78, 68, 174, 66, 96, 130, };
+        internal bool IsQuestionMarkPNG(Texture tex)
         {
-            return wrapMode == DEFAULT_WRAP_MODE && filterMode == DEFAULT_FILTER_MODE;
+            if (!tex)
+                return true;
+
+            byte[] png1 = (tex as Texture2D).EncodeToPNG();
+
+            if (png1.Length != questionMarkPNG.Length)
+                return false;
+            for (int i = 0; i < questionMarkPNG.Length; i++)
+                if (!png1[i].Equals(questionMarkPNG[i]))
+                    return false;
+            return true;
         }
     }
 }
