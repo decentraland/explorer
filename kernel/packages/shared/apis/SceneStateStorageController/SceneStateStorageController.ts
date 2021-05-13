38bf8107915f7477fbfe3be0950966fdd519c93d
Nicolas Chamo <nicolas@chamo.com.ar>
2021-05-13 11:36:14 -0300

    First commit

--- kernel/packages/shared/apis/SceneStateStorageController/SceneStateStorageController.ts
+++ kernel/packages/shared/apis/SceneStateStorageController/SceneStateStorageController.ts
@@ -30,6 +30,9 @@ import { ISceneStateStorageController } from './ISceneStateStorageController'
 declare const globalThis: any
 
 export class SceneStateStorageController extends ExposableAPI implements ISceneStateStorageController {
+  // @ts-ignore
+  static name = 'SceneStateStorageController'
+
   private readonly builderApiManager = new BuilderServerAPIManager()
   private parcelIdentity = this.options.getAPIInstance(ParcelIdentity)
   private builderManifest!: BuilderManifest
