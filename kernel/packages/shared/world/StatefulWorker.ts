ba76cfa93ed5fadd26b8de9d357d62773e430ca9
Nicolas Chamo <nicolas@chamo.com.ar>
2021-05-13 15:11:30 -0300

    Another try

--- kernel/packages/shared/world/StatefulWorker.ts
+++ kernel/packages/shared/world/StatefulWorker.ts
@@ -4,6 +4,7 @@ import { SceneWorker } from './SceneWorker'
 import { CustomWebWorkerTransport } from './CustomWebWorkerTransport'
 import { SceneStateStorageController } from 'shared/apis/SceneStateStorageController/SceneStateStorageController'
 import { defaultLogger } from 'shared/logger'
+import { setAPIName } from 'decentraland-rpc/lib/host'
 
 const gamekitWorkerRaw = require('raw-loader!../../../static/systems/stateful.scene.system.js')
 const gamekitWorkerBLOB = new Blob([gamekitWorkerRaw])
@@ -13,6 +14,7 @@ export class StatefulWorker extends SceneWorker {
   constructor(parcelScene: ParcelSceneAPI) {
     super(parcelScene, StatefulWorker.buildWebWorkerTransport(parcelScene))
 
+    setAPIName('SceneStateStorageController', SceneStateStorageController)
     this.getAPIInstance(SceneStateStorageController).catch((error) =>
       defaultLogger.error('Failed to load the SceneStateStorageController', error)
     )
