8887182f7e015d43cb44db25e78a97b1a4c17967
pablitar <pablo@decentraland.org>
2021-05-18 16:28:38 -0300

    feat: Added API to expose explorer configuration to scenes (#2372)

--- kernel/packages/shared/apis/EnvironmentAPI.ts
+++ kernel/packages/shared/apis/EnvironmentAPI.ts
@@ -4,13 +4,18 @@ import { EnvironmentData } from 'shared/types'
 import { Store } from 'redux'
 import { RootState } from 'shared/store/rootTypes'
 import { getRealm } from 'shared/dao/selectors'
-import { PREVIEW } from 'config'
+import { getServerConfigurations, PREVIEW } from 'config'
 
 type EnvironmentRealm = {
   domain: string
   layer: string
   serverName: string
   displayName: string
+}
+
+type ExplorerConfiguration = {
+  clientUri: string
+  configurations: Record<string, string | number | boolean>
 }
 
 declare const window: any
@@ -51,6 +56,19 @@ export class EnvironmentAPI extends ExposableAPI {
       layer,
       serverName,
       displayName: `${serverName}-${layer}`
+    }
+  }
+
+  /**
+   * Returns explorer configuration and environment information
+   */
+  @exposeMethod
+  async getExplorerConfiguration(): Promise<ExplorerConfiguration> {
+    return {
+      clientUri: location.href,
+      configurations: {
+        questsServerUrl: getServerConfigurations().questsUrl
+      }
     }
   }
 }
