57eddb4b00274e5cf57517e72b8d4a63ec4996e3
kuruk-mm <mateomiccino@gmail.com>
2021-05-19 16:52:44 -0300

    feat: add player expression, position change and rotation change even… (#2360)
    * feat: add player expression, position change and rotation change events to the sdk

--- kernel/packages/decentraland-ecs/src/decentraland/Types.ts
+++ kernel/packages/decentraland-ecs/src/decentraland/Types.ts
@@ -187,6 +187,10 @@ export interface IEvents {
     isIdle: boolean
   }
 
+  playerExpression: {
+    expressionId: string
+  }
+
   /**
    * `pointerUp` is triggered when the user releases an input pointer.
    * It could be a VR controller, a touch screen or the mouse.
