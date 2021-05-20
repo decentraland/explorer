1914a5bf72562b0fd95252a843a5d5d3011dca2c
Mateo Miccino <mateomiccino@gmail.com>
2021-05-20 10:33:11 -0300

    fix: removed observable events of position and rotation to take over those in the future with more consistency names

--- kernel/public/test-scenes/1.14.playerEvents/game.ts
+++ kernel/public/test-scenes/1.14.playerEvents/game.ts
@@ -4,22 +4,12 @@ import {
   Transform,
   Vector3,
   onPlayerExpressionObservable,
-  onPositionChangedObservable,
-  onRotationChangedObservable,
-  TextShape,
-  onEnterSceneObservable,
-  BoxShape,
-  Quaternion
+  TextShape
 } from 'decentraland-ecs/src'
-
-const box = new Entity()
-box.addComponent(new BoxShape())
-box.addComponent(new Transform({ position: new Vector3(8, 2, 8), scale: new Vector3(0.25, 0.25, 1) }))
-engine.addEntity(box)
 
 //Create entity and assign shape
 const text = new Entity()
-const shape = new TextShape('Null...')
+const shape = new TextShape('Make an expression!')
 text.addComponent(shape)
 
 text.addComponent(
@@ -31,22 +21,6 @@ text.addComponent(
 
 engine.addEntity(text)
 
-onEnterSceneObservable.add(({ userId }) => {
-  shape.value = 'Enter: ' + userId
-})
-
 onPlayerExpressionObservable.add(({ expressionId }) => {
   shape.value = 'Expression: ' + expressionId
-})
-
-onPositionChangedObservable.add((eventData) => {
-  text.getComponent(Transform).lookAt(new Vector3(eventData.position.x, eventData.position.y, eventData.position.z))
-})
-onRotationChangedObservable.add((eventData) => {
-  box.getComponent(Transform).rotation = new Quaternion(
-    eventData.quaternion.x,
-    eventData.quaternion.y,
-    eventData.quaternion.z,
-    eventData.quaternion.w
-  )
 })
