69739b6be490a3d6bb5b318b620f5da3258d6dfe
Santi Andrade <sandrade@decentraland.org>
2021-08-03 17:47:03 +0200

    Create isRecommendedBrowser function

--- kernel/packages/shared/comms/browser.ts
+++ kernel/packages/shared/comms/browser.ts
@@ -1,6 +1,21 @@
+export const recommendedBrowsers: string[] = [
+  "Chrome",
+  "Firefox"
+]
+
 export function isWebGLCompatible() {
   // Create canvas element. The canvas is not added to the document itself, so it is never displayed in the browser window.
   var canvas = <HTMLCanvasElement>document.createElement("canvas");
   var gl = canvas.getContext("webgl2")
   return gl && gl instanceof WebGL2RenderingContext
+}
+
+export function isRecommendedBrowser() {
+  for (let i = 0; i < recommendedBrowsers.length; i++) {
+    if (navigator.userAgent.indexOf(recommendedBrowsers[i]) != -1) {
+      return true
+    }
+  }
+
+  return false
 }
